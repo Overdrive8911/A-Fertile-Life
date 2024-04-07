03cf1b0b871511166c74937a0b4fc0af1f61914e
Overdrive8911 <wuchidiscord@gmail.com>
2024-04-07 21:55:06 +0100

    Created a container to hold the side bar toggle button and the newly added bare-bones map toggle button instead of only having the former like before
    A chunk of it is just renaming

--- src/js/project_js/ui/ui_side_section.ts
+++ src/js/project_js/ui/ui_side_section.ts
@@ -9,7 +9,9 @@ const uiSideBarToggleHandler = () => {
   $("html").css("--ui-side-bar-width", settings.uiSideBarToggle ? "" : "1rem");
 
   // Change the stubbed arrow's facing direction depending on the state of the sidebar
-  $("#ui-side-bar-toggle-button").text(settings.uiSideBarToggle ? "◄" : "►");
+  $("#ui-side-bar-toggle-state-button").text(
+    settings.uiSideBarToggle ? "◄" : "►"
+  );
 
   // Toggle the visibility all the contents of the sidebar
   for (const child of $("#ui-side-bar").children()) {
@@ -222,7 +224,7 @@ $(document).on(":passageend", () => {
   // To make sure the changes stick around when loading the game
   uiSideBarToggleHandler();
 
-  $("#ui-side-bar-toggle-button").ariaClick(() => {
+  $("#ui-side-bar-toggle-state-button").ariaClick(() => {
     // Open or stow the side bar
     settings.uiSideBarToggle = !settings.uiSideBarToggle;
     uiSideBarToggleHandler();
