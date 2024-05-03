6683d83046e4efb38c1c5b3c88e6ddebd5c0503f
Overdrive8911 <wuchidiscord@gmail.com>
2024-05-03 20:06:50 +0100

    Update:
    - Renamed "displayAndUpdateGameTime()" to "updateGameTimeDisplay()" since its more appropriate.
    - Moved "getDistanceToTravelFromLocation()" to "location_data.ts" since its more appropriate.
    - Update readme.
    - Added a function to update the variable storing the game time and another one that changes the time after a passage is navigated to.
    - "getDistanceToTravelFromLocation()" now accepts 2 parameters; the 2 passage titles to work with.
    - Fixed typos.
    - Updated the coords for some sub-locations and added new ones.

--- src/js/project_js/declarations/location_declarations.ts
+++ src/js/project_js/declarations/location_declarations.ts
@@ -1,68 +1,8 @@
-// const invalidLocation: number = -999;
-// const invalidSubLocation: number = invalidLocation;
 const distanceToMetresConversionRange: [min: number, max: number] = [
   0.85, 1.15,
 ];
-
-// // This checks whether a location exists and returns its array index location else returns -999
-// const getLocationDataIndex = (locationName: string) => {
-//   // Create setup.locations if it doesn't exist. Although this would never really happen, just to appease the typescript gods
-//   if (setup.locations === undefined) {
-//     setup.locations = new Map();
-//   }
-
-//   for (let i = 0; i < setup.locations?.size; i++) {
-//     const location = setup.locations[i];
-
-//     if (location.name === locationName) {
-//       // console.log(`index and element: ${i} and ${location.name}`);
-//       // The location exists so exit this function
-//       return i;
-//     }
-//   }
-
-//   for (const keyAndValue of setup.locations) {
-//     const locationKey = keyAndValue[0];
-//     const locationData = keyAndValue[1];
-
-//     if (locationData.name === locationName) {
-//       // console.log(`index and element: ${i} and ${location.name}`);
-//       // The location exists so exit this function
-//       return i;
-//     }
-//   }
-
-//   return invalidLocation;
-// };
-
-// const getSubLocationDataIndex = (
-//   subLocationName: string,
-//   locationName: string
-// ) => {
-//   let locationId = getLocationDataIndex(locationName);
-
-//   // Create setup.locations and the subLocations if it doesn't exist. Although this would never really happen, just to appease the typescript gods
-//   if (setup.locations === undefined) {
-//     setup.locations = [];
-//   }
-//   if (setup.locations[locationId].subLocations === undefined) {
-//     setup.locations[locationId].subLocations = [];
-//   }
-
-//   if (locationId != invalidLocation) {
-//     // location exists
-//     for (
-//       let i = 0;
-//       i < setup.locations[locationId]?.subLocations?.length;
-//       i++
-//     ) {
-//       const subLocation = setup.locations[locationId]?.subLocations[i];
-
-//       if (subLocation.name === subLocationName) {
-//         return i;
-//       }
-//     }
-//   }
-
-//   return invalidSubLocation;
-// };
+const averageWalkingSpeed: [
+  value: number,
+  distanceUnit: string,
+  timeUnit: string
+] = [1.42, "metres", "second"];
--- src/js/project_js/location/location_data.ts
+++ src/js/project_js/location/location_data.ts
@@ -94,14 +94,17 @@ setup.initializeLocationDataArray = function () {
             switch (location[0]) {
               case "location_playerHouse":
                 switch (subLocation[0]) {
+                  case "subLocation_livingRoom":
+                    subLocation_coords = [1, 30];
+                    break;
                   case "subLocation_bathroom":
-                    subLocation_coords = [3, 2];
+                    subLocation_coords = [15, 20];
                     break;
                   case "subLocation_porch":
-                    subLocation_coords = [5, 3];
+                    subLocation_coords = [0, -23];
                     break;
                   case "subLocation_bedroom":
-                    subLocation_coords = [2, 3];
+                    subLocation_coords = [21, 36];
                     break;
                   default:
                     subLocation_coords = [0, 0];
@@ -146,4 +149,213 @@ setup.initializeLocationDataArray = function () {
       }
     }
   }
+};
+
+// Determines the distance between the location/subLocation of the the previous and current passage using their tags
+setup.getDistanceToTravelFromLocation = (
+  prevPassageTitle: string,
+  currPassageTitle: string
+) => {
+  // const prevPassageTitle = previous();
+  // const currPassageTitle = State.active.title;
+
+  // Get the prev passage's location and sub location if any
+  let prevPassageLocation: string = "",
+    prevPassageSubLocation: string = "",
+    currPassageLocation: string = "",
+    currPassageSubLocation: string = "";
+
+  for (let i = 0; i < tags(prevPassageTitle).length; i++) {
+    let locationRegexArrResult =
+      tags(prevPassageTitle)[i].match(/location_[^\s]*/);
+    if (locationRegexArrResult) {
+      prevPassageLocation = locationRegexArrResult[0];
+    }
+
+    let subLocationRegexArrResult =
+      tags(prevPassageTitle)[i].match(/subLocation_[^\s]*/);
+    if (subLocationRegexArrResult) {
+      prevPassageSubLocation = subLocationRegexArrResult[0];
+    }
+  }
+
+  // Get the curr passage's location and sub location if any
+  for (let i = 0; i < tags(currPassageTitle).length; i++) {
+    let locationRegexArrResult =
+      tags(currPassageTitle)[i].match(/location_[^\s]*/);
+    if (locationRegexArrResult) {
+      currPassageLocation = locationRegexArrResult[0];
+    }
+
+    let subLocationRegexArrResult =
+      tags(currPassageTitle)[i].match(/subLocation_[^\s]*/);
+    if (subLocationRegexArrResult) {
+      currPassageSubLocation = subLocationRegexArrResult[0];
+    }
+  }
+
+  // console.log(`${prevPassageLocation} and ${prevPassageSubLocation}`);
+  // console.log(`${currPassageLocation} and ${currPassageSubLocation}`);
+
+  if (!prevPassageLocation || !currPassageLocation) {
+    // Either the prev or curr passage isn't a location so no need to calculate any distance
+    return 0;
+  }
+
+  // // Get location indexes in setup.locations
+  // const prevPassageLocationIndex = getLocationDataIndex(prevPassageLocation);
+  // const prevPassageSubLocationIndex = getSubLocationDataIndex(
+  //   prevPassageSubLocation,
+  //   prevPassageLocation
+  // );
+  // const currPassageLocationIndex = getLocationDataIndex(currPassageLocation);
+  // const currPassageSubLocationIndex = getSubLocationDataIndex(
+  //   currPassageSubLocation,
+  //   currPassageLocation
+  // );
+
+  // Appease typescript. This should never happen
+  if (setup.locations === undefined) {
+    setup.locations = new Map();
+  }
+
+  // Access the coords using the indexes
+  let prevPassageLocationCoords: [x: number, y: number, z?: number];
+  let prevPassageSubLocationCoords: [x: number, y: number, z?: number];
+  let currPassageLocationCoords: [x: number, y: number, z?: number];
+  let currPassageSubLocationCoords: [x: number, y: number, z?: number];
+
+  prevPassageLocationCoords = setup.locations.get(prevPassageLocation)!.coords;
+
+  if (
+    setup.locations.get(prevPassageLocation)!.subLocations !== undefined &&
+    prevPassageSubLocation
+  ) {
+    prevPassageSubLocationCoords = setup.locations
+      .get(prevPassageLocation)
+      .subLocations.get(prevPassageSubLocation).coords;
+  } else {
+    // no sub location exists so default the coords to [0, 0, 0]
+    prevPassageSubLocationCoords = [0, 0, 0];
+  }
+
+  currPassageLocationCoords = setup.locations.get(currPassageLocation).coords;
+
+  if (
+    setup.locations.get(currPassageLocation)!.subLocations !== undefined &&
+    currPassageSubLocation
+  ) {
+    currPassageSubLocationCoords = setup.locations
+      .get(currPassageLocation)
+      .subLocations.get(currPassageSubLocation).coords;
+  } else {
+    // no sub location exists so default the coords to [0, 0, 0]
+    currPassageSubLocationCoords = [0, 0, 0];
+  }
+
+  // Set the z-coordinate as 0 if it doesn't exist
+  const x_coord = 0,
+    y_coord = 1,
+    z_coord = 2;
+  if (!prevPassageLocationCoords[z_coord]) {
+    prevPassageLocationCoords[z_coord] = 0;
+  }
+  if (!prevPassageSubLocationCoords[z_coord]) {
+    prevPassageSubLocationCoords[z_coord] = 0;
+  }
+  if (!currPassageLocationCoords[z_coord]) {
+    currPassageLocationCoords[z_coord] = 0;
+  }
+  if (!currPassageSubLocationCoords[z_coord]) {
+    currPassageSubLocationCoords[z_coord] = 0;
+  }
+
+  // Assuming we are looking for the shortest distance between location A & subLocation A and location B & subLocation B, it would be the combined sum of the shortest distance between location A & subLocation A, location B & subLocation B, and location A & Location B. If location A & location B are the same, then find the shortest distance between subLocation A and subLocation B
+  // Assume that when comparing a subLocation to its own location, the coordinates on the location are [0, 0, 0]
+  const defaultLocationCoords: [x: number, y: number, z: number] = [0, 0, 0];
+
+  let distBetweenPrevPassageLocationAndSubLocation: number;
+  let distBetweenCurrPassageLocationAndSubLocation: number;
+  let distBetweenPrevPassageLocationAndCurrPassageLocation: number;
+  let distBetweenPrevPassageSubLocationAndCurrPassageSubLocation: number;
+  let totalDistanceBetweenLocationsInMetres: number;
+
+  const getDistanceBetweenTwoPoints = (
+    x1: number,
+    y1: number,
+    z1: number = 0,
+    x2: number,
+    y2: number,
+    z2: number = 0
+  ) => {
+    return Math.sqrt(
+      Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) + Math.pow(z1 - z2, 2)
+    );
+  };
+  const getRandomNumberFromRangeInclusive = (min: number, max: number) => {
+    return Math.random() * (max - min) + min;
+  };
+
+  distBetweenPrevPassageLocationAndSubLocation = getDistanceBetweenTwoPoints(
+    defaultLocationCoords[x_coord],
+    defaultLocationCoords[y_coord],
+    defaultLocationCoords[z_coord],
+    prevPassageSubLocationCoords[x_coord],
+    prevPassageSubLocationCoords[y_coord],
+    prevPassageSubLocationCoords[z_coord]
+  );
+
+  distBetweenCurrPassageLocationAndSubLocation = getDistanceBetweenTwoPoints(
+    defaultLocationCoords[x_coord],
+    defaultLocationCoords[y_coord],
+    defaultLocationCoords[z_coord],
+    currPassageSubLocationCoords[x_coord],
+    currPassageSubLocationCoords[y_coord],
+    currPassageSubLocationCoords[z_coord]
+  );
+
+  distBetweenPrevPassageLocationAndCurrPassageLocation =
+    getDistanceBetweenTwoPoints(
+      prevPassageLocationCoords[x_coord],
+      prevPassageLocationCoords[y_coord],
+      prevPassageLocationCoords[z_coord],
+      currPassageLocationCoords[x_coord],
+      currPassageLocationCoords[y_coord],
+      currPassageLocationCoords[z_coord]
+    );
+
+  distBetweenPrevPassageSubLocationAndCurrPassageSubLocation =
+    getDistanceBetweenTwoPoints(
+      prevPassageSubLocationCoords[x_coord],
+      prevPassageSubLocationCoords[y_coord],
+      prevPassageSubLocationCoords[z_coord],
+      currPassageSubLocationCoords[x_coord],
+      currPassageSubLocationCoords[y_coord],
+      currPassageSubLocationCoords[z_coord]
+    );
+
+  const min = 0,
+    max = 1;
+
+  if (prevPassageLocation !== currPassageLocation) {
+    totalDistanceBetweenLocationsInMetres =
+      (distBetweenPrevPassageLocationAndSubLocation +
+        distBetweenCurrPassageLocationAndSubLocation +
+        distBetweenPrevPassageLocationAndCurrPassageLocation) *
+      getRandomNumberFromRangeInclusive(
+        distanceToMetresConversionRange[min],
+        distanceToMetresConversionRange[max]
+      );
+  } else {
+    // Just use the distance between subLocations
+    totalDistanceBetweenLocationsInMetres =
+      distBetweenPrevPassageSubLocationAndCurrPassageSubLocation *
+      getRandomNumberFromRangeInclusive(
+        distanceToMetresConversionRange[min],
+        distanceToMetresConversionRange[max]
+      );
+  }
+
+  // Return a floating point number with 2 decimal places
+  return parseFloat(totalDistanceBetweenLocationsInMetres.toFixed(2));
 };
