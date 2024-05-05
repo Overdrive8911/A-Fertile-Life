a1edadf8df51c0ddccf75cd5a343d593c7dc08f1
Overdrive8911 <wuchidiscord@gmail.com>
2024-05-05 22:20:52 +0100

    Update batch script

--- build_and_watch_for_changes.bat
+++ build_and_watch_for_changes.bat
@@ -26,16 +26,17 @@ if exist src/css/dist/ui/mobile set arg4=src/css/dist/ui/mobile
 if exist src/css/dist/game set arg5=src/css/dist/game
 
 if exist src/js/dist/declarations set arg6=src/js/dist/declarations
-if exist src/js/dist/date_and_time set arg7=src/js/dist/date_and_time
-if exist src/js/dist/location set arg8=src/js/dist/location
-if exist src/js/dist/ui set arg9=src/js/dist/ui
+if exist src/js/dist/variable_initialization set arg7=src/js/dist/variable_initialization
+if exist src/js/dist/date_and_time set arg8=src/js/dist/date_and_time
+if exist src/js/dist/location set arg9=src/js/dist/location
+if exist src/js/dist/ui set arg10=src/js/dist/ui
 
-if exist src/js/extensions set arg10=src/js/extensions
-if exist src/css/extensions set arg11=src/css/extensions
+if exist src/js/extensions set arg11=src/js/extensions
+if exist src/css/extensions set arg12=src/css/extensions
 
-if exist src/passages set arg12=src/passages
+if exist src/passages set arg13=src/passages
 
-if exist src/variables set arg13=src/variables
+if exist src/variables set arg14=src/variables
 
 
 call tweego -l -w -t -o "export\A Fertile Life.html" %arg1% %arg2% %arg3% %arg4% %arg5% %arg6% %arg7% %arg8% %arg9% %arg10% %arg11% %arg12% %arg13% %arg14% %arg15% %arg16% %arg17% %arg18% %arg19% %arg20% 
