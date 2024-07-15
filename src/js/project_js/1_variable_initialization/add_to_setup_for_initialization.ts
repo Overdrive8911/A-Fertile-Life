00401bfdaec5bf81de04904e0d1681657d1004fd
Overdrive8911 <wuchidiscord@gmail.com>
2024-07-16 00:22:38 +0100

    Getting rid of some messy save stuff (I know I shouldn't do it in this branch but I'm tired)
    - Clearing other unnecessary stuff

---
+++ src/js/project_js/1_variable_initialization/add_to_setup_for_initialization.ts
@@ -0,0 +1,8 @@
+setup.initializeSaveVariables = () => {
+  // SECTION - STATE DATA
+  variables().gameDateAndTime = saveVar_gameDateAndTime;
+  variables().player = saveVar_player;
+  variables().lastPregUpdateFunctionCall = saveVar_lastPregUpdateFunctionCall;
+
+  // SECTION - STATIC DATA
+};
