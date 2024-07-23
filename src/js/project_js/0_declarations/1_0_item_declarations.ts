283a71ab151a6f3f43cdfcd9f8322927f9a0ad01
Overdrive8911 <wuchidiscord@gmail.com>
2024-07-23 17:18:14 +0100

    Rename

--- src/js/project_js/0_declarations/1_0_item_declarations.ts
+++ src/js/project_js/0_declarations/1_0_item_declarations.ts
@@ -1,12 +1,12 @@
-enum InventoryItemIds {
+enum ItemIds {
   DUMMY,
 }
 
 // This will store ALL the available info for every item. All the PC will keep in their inventory is the ID of the item so the required data can be linked back here. If an item has dynamic data, then that would be stored with the PC
-const gInGameItems: Item[] = [
-  {
-    itemId: InventoryItemIds.DUMMY,
+const gInGameItems: { [key in ItemIds]: Item } = {
+  [ItemIds.DUMMY]: {
+    itemId: ItemIds.DUMMY,
     price: 0,
     weight: 0,
   },
-];
+};
