df99bb176addd40cab1b322a2242fcccce71ee9e
Overdrive8911 <wuchidiscord@gmail.com>
2024-07-23 19:30:23 +0100

    Added descriptions, a bunch of placeholder items and cheese.

--- src/js/project_js/0_declarations/1_0_item_declarations.ts
+++ src/js/project_js/0_declarations/1_0_item_declarations.ts
@@ -1,12 +1,33 @@
 enum ItemIds {
   DUMMY,
+
+  // Food
+  FOOD_CHEAP_CHEESE,
+  FOOD_CHEESE,
+
+  // Player House
+  PLAYER_HOUSE_KEY,
+
+  // Drugs
+  DRUG_CONTRACEPTIVE,
+  DRUG_FERTILITY_BOOST_1,
+  DRUG_FERTILITY_BOOST_2,
+  DRUG_FERTILITY_BOOST_3,
 }
 
 // This will store ALL the available info for every item. All the PC will keep in their inventory is the ID of the item so the required data can be linked back here. If an item has dynamic data, then that would be stored with the PC
-const gInGameItems: { [key in ItemIds]: Item } = {
+const gInGameItems: { [key in ItemIds]?: Item } = {
   [ItemIds.DUMMY]: {
     itemId: ItemIds.DUMMY,
     price: 0,
     weight: 0,
+    description: "",
+    handler: function () {},
+  },
+  [ItemIds.FOOD_CHEESE]: {
+    itemId: ItemIds.FOOD_CHEESE,
+    price: 100,
+    weight: 30,
+    description: `A piece of soft yellow goodness from heaven.`,
   },
 };
--- src/js/project_js/0_declarations/1_inventory_declarations.ts
+++ src/js/project_js/0_declarations/1_inventory_declarations.ts
@@ -1,6 +1,7 @@
 // Will probably be extended to more specific types (especially those with dynamic data like ammunition)
 interface Item {
   itemId: number; // Entry in `ItemIds`. Also used to get the name of the items
+  description: string; // Make it gud
   price: number; // For the player to obtain it. The selling price is 45% of this value :p
   weight: number; // In grams
   handler?: () => void; // A handler function called when the item is used. Unusable items don't need this
@@ -9,7 +10,7 @@ interface Item {
 // Only the ID and location obtained is needed for static data since the required info can be fetched from `gInGameItems`. A regular `Item` is converted to this in `storeItem()`
 interface InventoryItem {
   itemId: number;
-  locationObtained: string; // It'll just store the name of the location
+  locationObtained?: string; // It'll just store the name of the location. If it doesn't exist, the item was gotten from "???"
   price?: number;
   weight?: number;
   dynamicData?: {}; // In case an object has dynamicData, just put the required data here and read it as necessary
