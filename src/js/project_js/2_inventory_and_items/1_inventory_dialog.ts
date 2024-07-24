53a3e35e7153b6303dfc0aeb17f54f1c351bb1c8
Overdrive8911 <wuchidiscord@gmail.com>
2024-07-24 13:53:34 +0100

    Added a hoverable tooltip to the inventory item's image

--- src/js/project_js/2_inventory_and_items/1_inventory_dialog.ts
+++ src/js/project_js/2_inventory_and_items/1_inventory_dialog.ts
@@ -14,6 +14,15 @@ setup.openInventoryDialog = () => {
     const nameOfItem = getItem(itemId).name;
     const itemImageUrl = getItem(itemId).imageUrl;
 
+    const itemSellingPrice = `$${getItem(itemId).price * 0.45}`;
+    const itemWeight =
+      getItem(itemId).weight < 1000
+        ? `${getItem(itemId).weight}g`
+        : `${(getItem(itemId).weight / 1000).toFixed(2)}kg`;
+    const itemDescription = getItem(itemId).description;
+
+    // TODO - Add at most 3 different locations the item was found and finally a Use button
+
     // Append the name, amount and image of the item
     inventoryRow.append(
       `<div class="inventory-item">
@@ -21,6 +30,18 @@ setup.openInventoryDialog = () => {
 
         <div class="inventory-item-image pixel-art">
           <img src=${itemImageUrl}>
+
+          <div class="inventory-tooltip">
+            <p>
+              <i>${itemDescription}</i>
+            </p>
+            
+            <p>
+                Selling Price = <span class="playerStatNeutral">${itemSellingPrice}</span>
+                <br>
+                Weight = <span class="playerStatNeutral">${itemWeight}</span>
+            </p>
+          </div>
         </div>
 
         <div class="inventory-item-footer">x${numOfDuplicates}&nbsp</div>
