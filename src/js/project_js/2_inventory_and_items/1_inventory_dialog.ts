088eaf8d51acde126e11347e1a61586fb444aa69
Overdrive8911 <wuchidiscord@gmail.com>
2024-07-24 22:48:07 +0100

    Added a sorting system to the inventory

--- src/js/project_js/2_inventory_and_items/1_inventory_dialog.ts
+++ src/js/project_js/2_inventory_and_items/1_inventory_dialog.ts
@@ -1,13 +1,63 @@
 setup.openInventoryDialog = () => {
+  // For stuff like All, Food, Key Items, Drugs, etc
+  let inventoryTabs = $('<div class="inventory-tabs"></div>');
   let inventoryRow = $('<div class="inventory-row"></div>');
 
-  // let arr: ItemId[] = [];
+  // SECTION - Populate inventoryTabs
+  let arrayOfTabStrings = Object.values(ItemTag).filter((value) => {
+    return typeof value == "string" && value != "DUMMY";
+  }) as string[];
 
-  // variables().player.inventory.forEach((value) => {
-  //   arr.push(value.itemId);
-  // });
+  // Leave the old array untouched
+  let editedArr = [...arrayOfTabStrings];
 
-  const noDupeItemArr = returnNoDuplicateArrayOfInventoryIds();
+  // To turn stuff like "DUMMY" to "Dummy"
+  // TODO - Make "KEY_ITEMS" turn to "Key Items" not "Key_items"
+  editedArr.forEach((tabString, index) => {
+    editedArr[index] = tabString.toLocaleLowerCase();
+    editedArr[index] =
+      editedArr[index].charAt(0).toLocaleUpperCase() +
+      editedArr[index].slice(1);
+  });
+  // I'll just hardcode it for now
+  console.log(editedArr);
+
+  for (let i = 0; i < editedArr.length; i++) {
+    const tabString = editedArr[i];
+    // The real ItemTag is stored as the `sorting-tag`
+    let tab = $(
+      `<button sorting-tag=${arrayOfTabStrings[i]}>${tabString}</button>`
+    );
+    tab.ariaClick(() => {
+      inventoryTabButtonHandler(tab, inventoryRow);
+    });
+
+    inventoryTabs.append(tab);
+  }
+
+  // SECTION - Populate inventoryRow
+  populateInventoryRowItems(inventoryRow);
+
+  Dialog.setup("Inventory", "inventory-dialog");
+  Dialog.append(inventoryTabs).append(inventoryRow);
+  Dialog.open();
+};
+
+function populateInventoryRowItems(
+  inventoryRow: JQuery<HTMLElement>,
+  sortingTag?: ItemTag
+) {
+  if (sortingTag == undefined) sortingTag = ItemTag.ALL;
+
+  let noDupeItemArr = returnNoDuplicateArrayOfInventoryIds();
+
+  // Sort using the sortingTag (except if its `ItemTag.ALL`)
+  if (sortingTag != ItemTag.ALL) {
+    noDupeItemArr = noDupeItemArr.filter((id) => {
+      return doesItemHaveTag(id, sortingTag);
+    });
+  }
+
   for (let i = 0; i < noDupeItemArr.length; i++) {
     const itemId = noDupeItemArr[i];
     const numOfDuplicates = getNumberOfItemDuplicates(itemId);
@@ -48,8 +98,61 @@ setup.openInventoryDialog = () => {
       </div>`
     );
   }
+}
 
-  Dialog.setup("Inventory", "inventory-dialog");
-  Dialog.append(inventoryRow);
-  Dialog.open();
-};
+function inventoryTabButtonHandler(
+  currentButton: JQuery<HTMLElement>,
+  inventoryRow: JQuery<HTMLElement>
+) {
+  // Check the current selected button (the selected button will have a "selected" class)
+  const currentSelectedBtn = getCurrentSelectedButton();
+
+  // Set the selected button to the one from the argument
+  if (currentSelectedBtn != currentButton) currentButton.addClass("selected");
+
+  // If not undefined, remove the selected class from currentSelectedBtn
+  if (currentSelectedBtn) currentSelectedBtn.removeClass("selected");
+
+  // Resort the items shown in the inventory
+  // but first, get the value we'll be sorting with
+  const sortingTag = getSortingValueFromSelectedBtn(currentButton);
+
+  // Empty the element containing our displayed items and rebuild it with the new sorting order
+  inventoryRow.empty();
+  populateInventoryRowItems(inventoryRow, sortingTag);
+}
+
+function getCurrentSelectedButton(): JQuery<HTMLElement> | undefined {
+  const invTabs = $(".inventory-tabs");
+
+  // Loop through its children (the buttons) and check for any with the "selected" class
+  for (let i = 0; i < invTabs.children().length; i++) {
+    const tab = invTabs.children()[i];
+
+    if ($(tab).hasClass("selected")) {
+      return $(tab);
+    }
+  }
+
+  // Didn't find any so return undefined
+  return undefined;
+}
+
+function getSortingValueFromSelectedBtn(button: JQuery<HTMLElement>): ItemTag {
+  // Get the `sorting-tag` of the button that was created with the latter
+  const sortingTag = button.attr("sorting-tag");
+
+  // Get all the string tags from `ItemTag`
+  const tagArray = Object.values(ItemTag).filter((value) => {
+    return typeof value == "string";
+  }) as string[];
+
+  for (let i = 0; i < tagArray.length; i++) {
+    const tag = tagArray[i];
+
+    if (sortingTag == tag) return i;
+  }
+
+  // Shouldn't happen if this function is called correctly
+  return undefined;
+}
