205a75e2075038077046e1f3a9252a90c39fe582
Overdrive8911 <wuchidiscord@gmail.com>
2024-08-04 13:57:14 +0100

    Split up the declaration file for clarity

---
+++ src/js/project_js/0_declarations/1_location_declarations/types_and_interfaces.ts
@@ -0,0 +1,40 @@
+interface GameLocation {
+  name: string;
+  coords: LocationCoords;
+  subLocations?: {
+    [nameOfSubLocation in MapSubLocation]?: GameSubLocation;
+  };
+  subLocationMap?: GameMapForSubLocations<typeof gGameMapSubLocationArraySize>;
+}
+
+interface GameSubLocation {
+  name?: string;
+  coords: LocationCoords;
+}
+
+type LocationCoords = [x: number, y: number, z?: number];
+
+// The game map would be a 2d/3d array to store all 3 possible coords for locations or sub locations. The id's for the respective location/sub locations are stored in the spot that their coords point to
+// NOTE - Any locations/sub locations that can be navigated to via north, east, south or west directions must share the same axis, i.e the x axis in east/ west and y axis in north/south.
+// NOTE - The first array represents the x axis, the second represents the y axis and the third (if present) represents the z axis
+type GameMapForLocations<Size extends number> = GameMapTuple<
+  GameMapTuple<GameMapTuple<number, Size>, Size>,
+  Size
+>; // 3d array
+type GameMapForSubLocations<Size extends number> = GameMapTuple<
+  GameMapTuple<number, Size>,
+  Size
+>; // 2d array
+type GameMapTuple<Coord, AxisLength extends number> = [Coord, ...Coord[]] & {
+  length: AxisLength;
+};
+// type GameMapTuple<Coord> = GameMapTupleType<Coord, 100>
+
+// For ease of use later. Add the first and last related MapLocation in the enum
+type RelatedMapLocations = {
+  [key in MapLocationContainer]: {
+    name: string;
+    generalCoords: LocationCoords;
+    relatedLocations: MapLocation[];
+  };
+};
--- src/js/project_js/0_declarations/1_location_declarations/variables.ts
+++ src/js/project_js/0_declarations/1_location_declarations/variables.ts
@@ -1,80 +1,19 @@
-interface GameLocation {
-  name: string;
-  coords: LocationCoords;
-  subLocations?: {
-    [nameOfSubLocation in MapSubLocation]?: GameSubLocation;
-  };
-  subLocationMap?: GameMapForSubLocations<typeof gGameMapSubLocationArraySize>;
-}
+const distanceToMetresConversionRange: [min: number, max: number] = [
+  0.85, 1.15,
+];
 
-interface GameSubLocation {
-  name?: string;
-  coords: LocationCoords;
-}
-
-type LocationCoords = [x: number, y: number, z?: number];
-
-enum LocationCoordIndex {
-  X,
-  Y,
-  Z,
-}
-
-// The game map would be a 2d/3d array to store all 3 possible coords for locations or sub locations. The id's for the respective location/sub locations are stored in the spot that their coords point to
-// NOTE - Any locations/sub locations that can be navigated to via north, east, south or west directions must share the same axis, i.e the x axis in east/ west and y axis in north/south.
-// NOTE - The first array represents the x axis, the second represents the y axis and the third (if present) represents the z axis
-type GameMapForLocations<Size extends number> = GameMapTuple<
-  GameMapTuple<GameMapTuple<number, Size>, Size>,
-  Size
->; // 3d array
-type GameMapForSubLocations<Size extends number> = GameMapTuple<
-  GameMapTuple<number, Size>,
-  Size
->; // 2d array
-type GameMapTuple<Coord, AxisLength extends number> = [Coord, ...Coord[]] & {
-  length: AxisLength;
-};
-// type GameMapTuple<Coord> = GameMapTupleType<Coord, 100>
+// I used 3.02 mph as the average and scaled it down to metres per second
+// NOTE - It should take, on average, 10 seconds to leave a room for the immediate one close by (e.g a room with coords [2,6] to another with coords [2,7]) so every single value difference in coordinates (or the result of `getDistanceBetweenTwoPoints` times 10) is worth 10 seconds at the `averageWalkingSpeed` i.e (1/ averageWalkingSpeed * (INSERT EXTRA VALUES HERE) = 10 on average).
+const averageWalkingSpeed: [
+  value: number,
+  distanceUnit: string,
+  timeUnit: string
+] = [1.35, "metres", "second"];
 
 // To create 100x100 or 100x100x100 arrays
 const gGameMapSubLocationArraySize = 100;
 
-// In the 2d/3d game map array, each numeric member has a value to determine properties, like if it is passable/empty/blocked/etc
-enum GameMapCoordinate {
-  EMPTY = (1 << 0) * gGameMapSubLocationArraySize,
-}
-
-// An enum of all locations. Locations are basically just containers of related areas. They are also the only areas that may be displayed on the world map
-// NOTE - Ensure that the name of a member (e.g FERTILO_INC) can be converted into a subLocation string (e.g location_fertiloInc)
-// NOTE - Arrange related locations right after each other for clarity
-enum MapLocation {
-  //
-  FERTILO_INC_FIRST_FLOOR_UNDERGROUND,
-  FERTILO_INC_GROUND_FLOOR,
-  FERTILO_INC_FIRST_FLOOR,
-  FERTILO_INC_SECOND_FLOOR,
-  FERTILO_INC_THIRD_FLOOR,
-  FERTILO_INC_TOP_FLOOR,
-
-  PLAYER_HOUSE,
-  BUS,
-  DREAM,
-  UNKNOWN,
-}
-
-// For ease of use later. Add the first and last related MapLocation in the enum
-type RelatedMapLocations = {
-  [key in MapLocationContainer]: {
-    name: string;
-    generalCoords: LocationCoords;
-    relatedLocations: MapLocation[];
-  };
-};
-
-// Stores the general name of a group of multiple locations. Is used in `gRelatedLocations`
-enum MapLocationContainer {
-  FERTILO_INC,
-}
+// A bunch of locations represent single floors of a total structure/building. This should help with grouping them
 const gRelatedLocations: RelatedMapLocations = {
   [MapLocationContainer.FERTILO_INC]: {
     name: "Fertilo Inc",
@@ -89,95 +28,6 @@ const gRelatedLocations: RelatedMapLocations = {
     ],
   },
 };
-
-// An enum of all sub locations. Multiple sub locations can share the same name as long as they're in different locations
-// Some sub locations can occur multiple times in a single location and as such have a number appended to them.
-// NOTE - Ensure that the name of a member (e.g RECEPTION) can be converted into a subLocation string (e.g subLocation_reception) as well as be used to get the appropriate image (e.g assets/img/map/sub_location/reception.webp)
-// NOTE - Assign the duplicates to the map from left to right
-enum MapSubLocation {
-  RECEPTION,
-  CLOSET,
-  MEASUREMENT_CLOSET,
-  CEO_OFFICE,
-  CONSULTATION,
-  LAB,
-  OFFICE_WORK,
-
-  PHARMACY_1,
-  PHARMACY_2,
-
-  ROOM,
-  PLAYER_ROOM,
-  LIVING_ROOM,
-  BATHROOM,
-  BEDROOM,
-  PORCH,
-
-  // Hallways
-  HALLWAY_1,
-  HALLWAY_2,
-  HALLWAY_3,
-  HALLWAY_4,
-  HALLWAY_5,
-  HALLWAY_6,
-  HALLWAY_7,
-
-  // Corridors
-  CORRIDOR_1,
-  CORRIDOR_2,
-  CORRIDOR_3,
-
-  // Staircases
-  STAIRCASE_1,
-  STAIRCASE_2,
-  STAIRCASE_3,
-
-  // Elevators
-  ELEVATOR_1,
-  ELEVATOR_2,
-  ELEVATOR_3,
-}
-
-// Only here to avoid repetition since it's used in `setup.locationData`
-function getSubLocationCoords(
-  locationId: MapLocation,
-  subLocationId: MapSubLocation
-): LocationCoords {
-  const coords =
-    setup.locationData[locationId].subLocations[subLocationId].coords;
-  return coords != undefined ? coords : [0, 0, 0];
-}
-
-// Same case with the above function. Use zeroes for x/y/z if you're not using them
-function getCoordsRelativeToOtherSubLocation(
-  locationId: MapLocation,
-  subLocationId: MapSubLocation,
-  xIncrement: number,
-  yIncrement: number,
-  zIncrement: number
-): LocationCoords {
-  const otherSubLocationCoords = getSubLocationCoords(
-    locationId,
-    subLocationId
-  );
-
-  // Prevent undefined values
-  if (otherSubLocationCoords[LocationCoordIndex.Z] == undefined)
-    otherSubLocationCoords[LocationCoordIndex.Z] = 0;
-
-  let newCoords: LocationCoords = [
-    otherSubLocationCoords[LocationCoordIndex.X] + xIncrement,
-    otherSubLocationCoords[LocationCoordIndex.Y] + yIncrement,
-    otherSubLocationCoords[LocationCoordIndex.Z] + zIncrement,
-  ];
-
-  if (newCoords[LocationCoordIndex.Z] == 0) {
-    // No use of keeping it
-    delete newCoords[LocationCoordIndex.Z];
-  }
-
-  return newCoords;
-}
 
 // NOTE - This stores EVERY possible location. Keep in mind that moving from coords [2,6] to [2,7] or [5,3] to [4,3] takes 10 seconds on average. Note that the `entry` sub location would have its distance calculated from [0,0]
 // NOTE - The first entry in `subLocations` is where the player will enter if they move into that particular location without a set destination (aka another sub location)
@@ -506,26 +356,3 @@ setup.locationData = {
   [MapLocation.DREAM]: { name: "???", coords: [0, 0] },
   [MapLocation.UNKNOWN]: { name: "???", coords: [0, 0] },
 };
-
-let a = {
-  b: 2,
-  get c() {
-    return this.b * 2;
-  },
-
-  get d() {
-    return this.c * 4;
-  },
-};
-
-const distanceToMetresConversionRange: [min: number, max: number] = [
-  0.85, 1.15,
-];
-
-// I used 3.02 mph as the average and scaled it down to metres per second
-// NOTE - It should take, on average, 10 seconds to leave a room for the immediate one close by (e.g a room with coords [2,6] to another with coords [2,7]) so every single value difference in coordinates (or the result of `getDistanceBetweenTwoPoints` times 10) is worth 10 seconds at the `averageWalkingSpeed` i.e (1/ averageWalkingSpeed * (INSERT EXTRA VALUES HERE) = 10 on average).
-const averageWalkingSpeed: [
-  value: number,
-  distanceUnit: string,
-  timeUnit: string
-] = [1.35, "metres", "second"];
