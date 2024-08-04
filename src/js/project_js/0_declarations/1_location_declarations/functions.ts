4faa45c22c00e678ad20b34312d7dddbe60f26e2
Overdrive8911 <wuchidiscord@gmail.com>
2024-08-04 13:57:14 +0100

    Split up the declaration file for clarity

---
+++ src/js/project_js/0_declarations/1_location_declarations/functions.ts
@@ -0,0 +1,40 @@
+// Only here to avoid repetition since it's used in `setup.locationData`
+function getSubLocationCoords(
+  locationId: MapLocation,
+  subLocationId: MapSubLocation
+): LocationCoords {
+  const coords =
+    setup.locationData[locationId].subLocations[subLocationId].coords;
+  return coords != undefined ? coords : [0, 0, 0];
+}
+
+// Same case with the above function. Use zeroes for x/y/z if you're not using them
+function getCoordsRelativeToOtherSubLocation(
+  locationId: MapLocation,
+  subLocationId: MapSubLocation,
+  xIncrement: number,
+  yIncrement: number,
+  zIncrement: number
+): LocationCoords {
+  const otherSubLocationCoords = getSubLocationCoords(
+    locationId,
+    subLocationId
+  );
+
+  // Prevent undefined values
+  if (otherSubLocationCoords[LocationCoordIndex.Z] == undefined)
+    otherSubLocationCoords[LocationCoordIndex.Z] = 0;
+
+  let newCoords: LocationCoords = [
+    otherSubLocationCoords[LocationCoordIndex.X] + xIncrement,
+    otherSubLocationCoords[LocationCoordIndex.Y] + yIncrement,
+    otherSubLocationCoords[LocationCoordIndex.Z] + zIncrement,
+  ];
+
+  if (newCoords[LocationCoordIndex.Z] == 0) {
+    // No use of keeping it
+    delete newCoords[LocationCoordIndex.Z];
+  }
+
+  return newCoords;
+}
