abe0c1e850f61f8bea9eec7b979db591144dff29
Overdrive8911 <wuchidiscord@gmail.com>
2024-08-14 09:15:06 +0100

    Added a larger view that can also be zoomed in/out

--- src/js/project_js/2_location/999_event_handlers.ts
+++ src/js/project_js/2_location/999_event_handlers.ts
@@ -13,33 +13,66 @@ $(document).one(":passageend", () => {
     );
   });
 
-  const getZoomRatio = () => {
+  const getZoomRatio = (element: JQuery<HTMLElement>) => {
     // Check the transform value on the svg (It should be a scaled value i.e "matrix(2, 0, 0, 2, 0, 0)" corresponds with scale(2) ). If the property doesn't exist or if it's less than 1, default to 1
     // NOTE - Index 3 in "matrix(2, 0, 0, 2, 0, 0)" will be 2, which is the number that is currently being scaled by
-    return $(".ui-side-bar-popout-map > svg").css("transform") != "none"
-      ? parseFloat(
-          $(".ui-side-bar-popout-map > svg").css("transform").split(",")[3]
-        ) >= 1
-        ? parseFloat(
-            $(".ui-side-bar-popout-map > svg").css("transform").split(",")[3]
-          )
+    return $(element).css("transform") != "none"
+      ? parseFloat($(element).css("transform").split(",")[3]) >= 1
+        ? parseFloat($(element).css("transform").split(",")[3])
         : 1
       : 1;
   };
 
   // Handlers for the zooming functionality of the map popout
   $(".ui-side-bar-popout-map-button-bar > .button-zoom-in").ariaClick(() => {
-    // Increment the zoom ratio by 0.5
-    $(".ui-side-bar-popout-map > svg").css(
-      "transform",
-      `scale(${getZoomRatio() + 0.5})`
-    );
+    // Check if the dialog for "Large View" is open
+    if (!Dialog.isOpen("map-large-view")) {
+      // Increment the zoom ratio by 0.5
+      $(".ui-side-bar-popout-map > svg").css(
+        "transform",
+        `scale(${getZoomRatio($(".ui-side-bar-popout-map > svg")) + 0.5})`
+      );
+    } else {
+      // Do the same but for the large view of the map
+      $(".map-large-view > svg").css(
+        "transform",
+        `scale(${getZoomRatio($(".map-large-view > svg")) + 0.5})`
+      );
+    }
   });
   $(".ui-side-bar-popout-map-button-bar > .button-zoom-out").ariaClick(() => {
-    // Decrement the zoom ratio by 0.5 but don't go lower than 1
-    $(".ui-side-bar-popout-map > svg").css(
-      "transform",
-      `scale(${getZoomRatio() - 0.5 > 1 ? getZoomRatio() - 0.5 : 1})`
-    );
+    // Check if the dialog for "Large View" is open
+    if (!Dialog.isOpen("map-large-view")) {
+      // Decrement the zoom ratio by 0.5 but don't go lower than 1
+      $(".ui-side-bar-popout-map > svg").css(
+        "transform",
+        `scale(${
+          getZoomRatio($(".ui-side-bar-popout-map > svg")) - 0.5 > 1
+            ? getZoomRatio($(".ui-side-bar-popout-map > svg")) - 0.5
+            : 1
+        })`
+      );
+    } else {
+      // Do the same for the large view of the map
+      $(".map-large-view > svg").css(
+        "transform",
+        `scale(${
+          getZoomRatio($(".map-large-view > svg")) - 0.5 > 1
+            ? getZoomRatio($(".map-large-view > svg")) - 0.5
+            : 1
+        })`
+      );
+    }
+  });
+
+  // For handling the "Large View" functionality (it just displays the map in a large dialog)
+  $(".ui-side-bar-popout-map-button-bar > .button-large-view").ariaClick(() => {
+    Dialog.setup("Large View", "map-large-view");
+    // Add dummy data
+    Dialog.append("");
+    Dialog.open();
+
+    // Load the map into here
+    loadGameMap(variables().player.locationData.location, $(".map-large-view"));
   });
 });
