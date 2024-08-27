7d0afc887a64a1253a6bd614d8365065941580f9
Overdrive8911 <wuchidiscord@gmail.com>
2024-08-27 16:52:41 +0100

    Got rid of `setup.initializeExtraLocationData()` and used a namespace/IIFE to replace it

--- src/js/project_js/2_location/2_location_data_object_intializer.ts
+++ src/js/project_js/2_location/2_location_data_object_intializer.ts
@@ -1,32 +1,36 @@
 namespace NSLocation {
   // NOTE - The default name of the sub locations are the capital forms of the indexes of their names, e.g MapSubLocation.HALLWAY_4 has a default name of "Hallway", MapSubLocation.CORRIDOR has a default name of "Corridor". Use this for unimportant areas with duplicates
-  setup.initializeExtraLocationData = () => {
-    const mapLocations = Object.values(MapLocation).filter((value) => {
-      return typeof value == "number";
-    }) as MapLocation[];
 
-    mapLocations.forEach((location) => {
-      // TODO - Populate all Location Maps
+  const mapLocations = Object.values(MapLocation).filter((value) => {
+    return typeof value == "number";
+  }) as MapLocation[];
 
-      // Proceed if an entry in `gLocationData` exists
-      if (setup.locationData[location]) {
-        // Populate all Sub Location Maps
-        populateSubLocationMap(location);
+  mapLocations.forEach((location) => {
+    // TODO - Populate all Location Maps
 
-        // Set the names of all sub locations (since some rely on their defaults)
-        if (setup.locationData[location].subLocations) {
-          Object.entries(setup.locationData[location].subLocations).forEach(
-            ([subLocationId]) => {
-              setup.locationData[location].subLocations[
-                parseInt(subLocationId) as MapSubLocation
-              ].name = getDefaultNameOfSubLocation(
-                location,
-                parseInt(subLocationId) as MapSubLocation
-              );
-            }
-          );
-        }
+    // Proceed if an entry in `gLocationData` exists without a sub location map
+    if (
+      setup.locationData[location] &&
+      !setup.locationData[location].subLocationMap
+    ) {
+      // Populate all Sub Location Maps
+      populateSubLocationMap(location);
+
+      // Set the names of all sub locations (since some rely on their defaults)
+      if (setup.locationData[location].subLocations) {
+        Object.entries(setup.locationData[location].subLocations).forEach(
+          ([subLocationId]) => {
+            setup.locationData[location].subLocations[
+              parseInt(subLocationId) as MapSubLocation
+            ].name = getDefaultNameOfSubLocation(
+              location,
+              parseInt(subLocationId) as MapSubLocation
+            );
+          }
+        );
       }
-    });
-  };
+    }
+  });
 }
+
+NSLocation;
