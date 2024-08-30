0b2728c218bc9bc064d6e1e8166f342bb60c25fa
Overdrive8911 <wuchidiscord@gmail.com>
2024-08-30 21:50:26 +0100

    Moved an event handler function to a more suitable file
    - Fixed a light bug in `inventoryTabButtonHandler()`

--- src/js/project_js/2_inventory_and_items/999_event_handlers.ts
+++ src/js/project_js/2_inventory_and_items/999_event_handlers.ts
@@ -1,4 +1,5 @@
 namespace NSInventoryAndItem {
+  // SECTION - For adding functionality to the inventory icon in the top bar
   $(document).on(":passageend", () => {
     const inventoryBtn = $(".ui-settings-button-inventory");
 
@@ -6,4 +7,41 @@ namespace NSInventoryAndItem {
       openInventoryDialog();
     });
   });
+  // !SECTION
+
+  // SECTION - Deal with the inventory tooltip and its quirks
+  export function inventoryTooltipHandler() {
+    $(".inventory-item-image").on("mouseover", () => {
+      let tooltipContainer: JQuery<HTMLElement>;
+      let inventoryImageContainer: JQuery<HTMLElement>;
+
+      for (let i of $(".inventory-tooltip")) {
+        if ($(i).css("display") != "none") {
+          // The active tooltip
+          tooltipContainer = $(i);
+
+          // The image container that is the parent to the active tooltip
+          inventoryImageContainer = tooltipContainer.parent();
+        }
+      }
+
+      const inventoryImageContainerBottom =
+        inventoryImageContainer.offset().top + inventoryImageContainer.height();
+
+      // Ensure that the tooltip container stays right beneath the inventory item's image
+      tooltipContainer.offset({ top: inventoryImageContainerBottom });
+
+      // Make sure that the tool tip doesn't cut out of the view port. Instead, shift it above the image
+      const tooltipContainerBottom =
+        tooltipContainer.offset().top + tooltipContainer.height();
+      const viewportBottom = $(window).height();
+
+      if (tooltipContainerBottom > viewportBottom) {
+        tooltipContainer.offset({
+          top: inventoryImageContainer.offset().top - tooltipContainer.height(),
+        });
+      }
+    });
+  }
+  // !SECTION
 }
