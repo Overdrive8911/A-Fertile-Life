7b85703f741a7b7570b3457bebb6c4b5e76816d9
Overdrive8911 <wuchidiscord@gmail.com>
2024-08-30 20:13:23 +0100

    Update

--- src/js/project_js/0_declarations/1_inventory_declarations.ts
+++ src/js/project_js/0_declarations/1_inventory_declarations.ts
@@ -14,7 +14,7 @@ interface Item {
 // Only the ID and location obtained is needed for static data since the required info can be fetched from `gInGameItems`. A regular `Item` is converted to this in `storeItem()`
 interface InventoryItem {
   itemId: number;
-  locationObtained?: string; // It'll just store the name of the location. If it doesn't exist, the item was gotten from "???"
+  locationObtained?: string; // NOTE - It's actually meant to be a number, so make sure to convert it appropriately when merging. It'll just store the name of the location. If it doesn't exist, the item was gotten from "???"
   price?: number;
   weight?: number;
   dynamicData?: {}; // In case an object has dynamicData, just put the required data here and read it as necessary
