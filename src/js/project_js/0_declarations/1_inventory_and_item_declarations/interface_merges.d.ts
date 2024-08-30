8e4fdc4a11459f1479fa4283b39c53595911e806
Overdrive8911 <wuchidiscord@gmail.com>
2024-08-30 21:25:45 +0100

    Added setup types

---
+++ src/js/project_js/0_declarations/1_inventory_and_item_declarations/interface_merges.d.ts
@@ -0,0 +1,10 @@
+declare module "twine-sugarcube" {
+  export interface SugarCubeSetupObject {
+    openInventoryDialog: () => void;
+    addAllItems: () => void;
+  }
+}
+
+namespace NSInventoryAndItem {}
+
+export {};
