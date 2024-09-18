f1478bf6f7e22c8a090411110c38c71b03154ff6
Overdrive8911 <wuchidiscord@gmail.com>
2024-09-18 10:21:11 +0100

    Comment

--- src/js/project_js/1_variable_initialization/add_to_setup_function_for_initialization.ts
+++ src/js/project_js/1_variable_initialization/add_to_setup_function_for_initialization.ts
@@ -2,6 +2,7 @@ setup.initializeSaveVariables = () => {
   // SECTION - STATE DATA
   variables().gameDateAndTime = saveVar_gameDateAndTime;
   variables().player = saveVar_player;
+  // TODO - lastPregUpdateFunctionCall seems redundant
   variables().lastPregUpdateFunctionCall = saveVar_lastPregUpdateFunctionCall;
 
   // SECTION - STATIC DATA
