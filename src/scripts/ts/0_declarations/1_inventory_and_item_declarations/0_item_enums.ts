1b69e309b202c7be0bc7b743bfdb6efbf8d8c9ab
Overdrive8911 <wuchidiscord@gmail.com>
2024-12-24 18:49:02 +0100

    Improved `get imageUrl()` in the `Item` class

--- src/scripts/ts/0_declarations/1_inventory_and_item_declarations/0_item_enums.ts
+++ src/scripts/ts/0_declarations/1_inventory_and_item_declarations/0_item_enums.ts
@@ -3,25 +3,25 @@ namespace NSInventoryAndItem {
     DUMMY,
 
     // Food
-    FOOD_CHEESE,
-    FOOD_MOULDY_CHEESE,
+    CHEESE,
+    MOULDY_CHEESE,
 
     // Key Cards
     //
-    KEY_CARD_LVL_1, // Basic rooms like cleaning supplies and the PC office. PC gets handed this at the beginning
-    KEY_CARD_LVL_2, // Better prices at the shops and better food (just an upgrade)
-    KEY_CARD_LVL_3, // Allowed into major places like the Nursery
-    KEY_CARD_LVL_4, // Access to all drugs and items as well as the Underground
-    KEY_CARD_LVL_5, // Dunno. Will be unobtainable irl for a while
+    KEYCARD_LVL_1, // Basic rooms like cleaning supplies and the PC office. PC gets handed this at the beginning
+    KEYCARD_LVL_2, // Better prices at the shops and better food (just an upgrade)
+    KEYCARD_LVL_3, // Allowed into major places like the Nursery
+    KEYCARD_LVL_4, // Access to all drugs and items as well as the Underground
+    KEYCARD_LVL_5, // Dunno. Will be unobtainable irl for a while
 
     // Player House
     PLAYER_HOUSE_KEY,
 
     // Drugs
-    DRUG_CONTRACEPTIVE,
-    DRUG_FERTILITY_BOOST_1,
-    DRUG_FERTILITY_BOOST_2,
-    DRUG_FERTILITY_BOOST_3,
+    CONTRACEPTIVE,
+    FERTILITY_BOOST_1,
+    FERTILITY_BOOST_2,
+    FERTILITY_BOOST_3,
   }
 
   export enum ItemTag {
