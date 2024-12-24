2412094556ca5a30e2fba2f18d19ef625450f5d9
Overdrive8911 <wuchidiscord@gmail.com>
2024-12-24 18:41:48 +0100

    Converted te Item objects into proper classes

--- src/scripts/ts/0_declarations/1_inventory_and_item_declarations/5_game_item_declarations.ts
+++ src/scripts/ts/0_declarations/1_inventory_and_item_declarations/5_game_item_declarations.ts
@@ -1,73 +1,19 @@
 namespace NSInventoryAndItem {
-  export enum ItemId {
-    DUMMY,
-
-    // Food
-    FOOD_CHEESE,
-    FOOD_MOULDY_CHEESE,
-
-    // Key Cards
-    //
-    KEY_CARD_LVL_1, // Basic rooms like cleaning supplies and the PC office. PC gets handed this at the beginning
-    KEY_CARD_LVL_2, // Better prices at the shops and better food (just an upgrade)
-    KEY_CARD_LVL_3, // Allowed into major places like the Nursery
-    KEY_CARD_LVL_4, // Access to all drugs and items as well as the Underground
-    KEY_CARD_LVL_5, // Dunno. Will be unobtainable irl for a while
-
-    // Player House
-    PLAYER_HOUSE_KEY,
-
-    // Drugs
-    DRUG_CONTRACEPTIVE,
-    DRUG_FERTILITY_BOOST_1,
-    DRUG_FERTILITY_BOOST_2,
-    DRUG_FERTILITY_BOOST_3,
-  }
-
-  export const enum ItemTag {
-    DUMMY,
-    ALL, // Don't give this tag to anything. It applies to every item already
-    KEY_ITEMS,
-    FOOD,
-    CLOTHING,
-    MISCELLANEOUS,
-    DRUGS,
-    TRASH,
-  }
-
-  export const enum ItemProperties {
-    PRICE_CANNOT_BE_BOUGHT = -1,
-  }
-
   // This will store ALL the available info for every item. All the PC will keep in their inventory is the ID of the item so the required data can be linked back here. If an item has dynamic data, then that would be stored with the PC
   export const gInGameItems: { [key in ItemId]?: Item } = {
-    [ItemId.DUMMY]: {
-      itemId: ItemId.DUMMY,
-      name: "Dummy",
-      price: 0,
-      weight: 0,
-      description: "Dummy",
-      // TODO - use a lazy getter to get the url
-      imageUrl: "assets/img/items/dummy.webp",
-      tags: [ItemTag.DUMMY],
-      handler: function (...args) {
-        let dummyVar = args[0] as number | string;
+    [ItemId.DUMMY]: new Item(),
 
-        return !dummyVar ? random(25) : `Dummy is ${dummyVar}`;
-      },
-    },
-
-    [ItemId.FOOD_CHEESE]: {
+    [ItemId.FOOD_CHEESE]: new Item({
       itemId: ItemId.FOOD_CHEESE,
       name: "Cheese",
       price: 100,
       weight: 300,
-      description: `A piece of soft yellow divine goodness from heaven itself.`,
+      description: "A piece of soft yellow divine goodness from heaven itself.",
       imageUrl: "assets/img/items/cheese.webp",
       tags: [ItemTag.FOOD],
-    },
+    }),
 
-    [ItemId.FOOD_MOULDY_CHEESE]: {
+    [ItemId.FOOD_MOULDY_CHEESE]: new Item({
       itemId: ItemId.FOOD_MOULDY_CHEESE,
       name: "Mouldy Cheese",
       price: 10000,
@@ -75,9 +21,9 @@ namespace NSInventoryAndItem {
       description: `An antiquated piece of "food" that should've been discarded long ago.`,
       imageUrl: "assets/img/items/mouldy_cheese.webp",
       tags: [ItemTag.FOOD],
-    },
+    }),
 
-    [ItemId.KEY_CARD_LVL_1]: {
+    [ItemId.KEY_CARD_LVL_1]: new Item({
       itemId: ItemId.KEY_CARD_LVL_1,
       name: "Key Card",
       price: ItemProperties.PRICE_CANNOT_BE_BOUGHT,
@@ -85,9 +31,9 @@ namespace NSInventoryAndItem {
       description: `It looks like lowest level out of its set. It probably has no use beyond accessing closets…`,
       imageUrl: "assets/img/items/keycard_lvl_1.webp",
       tags: [ItemTag.KEY_ITEMS],
-    },
+    }),
 
-    [ItemId.KEY_CARD_LVL_2]: {
+    [ItemId.KEY_CARD_LVL_2]: new Item({
       itemId: ItemId.KEY_CARD_LVL_2,
       name: "Key Card",
       price: ItemProperties.PRICE_CANNOT_BE_BOUGHT,
@@ -95,9 +41,9 @@ namespace NSInventoryAndItem {
       description: `The defacto card for employees. You have access to everything your co-workers have. Looks like you're fitting in just nicely.`,
       imageUrl: "assets/img/items/keycard_lvl_2.webp",
       tags: [ItemTag.KEY_ITEMS],
-    },
+    }),
 
-    [ItemId.KEY_CARD_LVL_3]: {
+    [ItemId.KEY_CARD_LVL_3]: new Item({
       itemId: ItemId.KEY_CARD_LVL_3,
       name: "Key Card",
       price: ItemProperties.PRICE_CANNOT_BE_BOUGHT,
@@ -105,9 +51,9 @@ namespace NSInventoryAndItem {
       description: `Seems like you've been given much elevated permissions. You now have proper access to places like <b>The Nursery</b>.`,
       imageUrl: "assets/img/items/keycard_lvl_3.webp",
       tags: [ItemTag.KEY_ITEMS],
-    },
+    }),
 
-    [ItemId.KEY_CARD_LVL_4]: {
+    [ItemId.KEY_CARD_LVL_4]: new Item({
       itemId: ItemId.KEY_CARD_LVL_4,
       name: "Key Card",
       price: ItemProperties.PRICE_CANNOT_BE_BOUGHT,
@@ -115,9 +61,9 @@ namespace NSInventoryAndItem {
       description: `With the authority of a top-ranking researcher, you now have access to all items sold, and locations in the hospital. Including <b>The Underground</b>`,
       imageUrl: "assets/img/items/keycard_lvl_4.webp",
       tags: [ItemTag.KEY_ITEMS],
-    },
+    }),
 
-    [ItemId.KEY_CARD_LVL_5]: {
+    [ItemId.KEY_CARD_LVL_5]: new Item({
       itemId: ItemId.KEY_CARD_LVL_5,
       name: "Key Card",
       price: ItemProperties.PRICE_CANNOT_BE_BOUGHT,
@@ -125,6 +71,6 @@ namespace NSInventoryAndItem {
       description: `You really shouldn't have this. I don't know what to do with this item :p`,
       imageUrl: "assets/img/items/keycard_lvl_5.webp",
       tags: [ItemTag.KEY_ITEMS],
-    },
+    }),
   };
 }
