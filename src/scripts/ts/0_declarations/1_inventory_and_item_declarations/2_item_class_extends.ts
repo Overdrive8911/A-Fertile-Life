7036ebff1406945776645e551f0f0c9b27967fb1
Overdrive8911 <wuchidiscord@gmail.com>
2024-12-26 11:16:41 +0100

    Started implementing the child classes of a generic item

---
+++ src/scripts/ts/0_declarations/1_inventory_and_item_declarations/2_item_class_extends.ts
@@ -0,0 +1,34 @@
+namespace NSInventoryAndItem {
+  // REVIEW - Types of food that reduce hunger and may give certain buffs or nerf?
+  export class Food extends Item {
+    constructor(data?: Partial<Item>) {
+      super(data);
+    }
+  }
+
+  export class Clothing extends Item {
+    bodyArea: ClothingArea;
+
+    constructor(data?: Partial<Item>) {
+      super(data);
+    }
+  }
+
+  export class Drug extends Item {
+    constructor(data?: Partial<Item>) {
+      super(data);
+    }
+  }
+
+  // export class Trash extends Item {
+  //   constructor(data?: Partial<Item>) {
+  //     super(data);
+  //   }
+  // }
+
+  // export class Miscellaneous extends Item {
+  //   constructor(data?: Partial<Item>) {
+  //     super(data);
+  //   }
+  // }
+}
