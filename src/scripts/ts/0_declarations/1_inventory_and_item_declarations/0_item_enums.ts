fcd111f2f4e99f01cf6d1bf3bc37656a7dd6fbae
Overdrive8911 <wuchidiscord@gmail.com>
2024-12-30 09:21:33 +0100

    Added a utility enum

--- src/scripts/ts/0_declarations/1_inventory_and_item_declarations/0_item_enums.ts
+++ src/scripts/ts/0_declarations/1_inventory_and_item_declarations/0_item_enums.ts
@@ -194,5 +194,9 @@ namespace NSInventoryAndItem {
 
       DEFAULT = DURABILITY_EXCELLENT | NOT_IN_USE,
     }
+    export const enum InventoryItemOrStaticItem {
+      INVENTORY_ITEM = 1,
+      STATIC_ITEM,
+    }
   }
 }
