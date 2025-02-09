eaf0f2559bd2b13c7caa2321df980fcdc1d34f5b
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-09 18:59:09 +0100

    Added a way to unconditionally generate data in `generateMapOfConnectionsForChildData()`

--- src/scripts/ts/0_declarations/1_location_declarations/0_classes.ts
+++ src/scripts/ts/0_declarations/1_location_declarations/0_classes.ts
@@ -153,10 +153,22 @@ namespace NSLocation {
       return this;
     }
 
-    protected async generateMapOfConnectionsForChildData() {
+    /**
+     *
+     * @param forceGenerate - Default: `false`. If this is `true`, the data is always regenerated.
+     * @returns
+     */
+    protected async generateMapOfConnectionsForChildData(
+      forceGenerate = false
+    ) {
+     * 
+     * @param forceGenerate - Default: `false`. If this is `true`, the data is always regenerated.
+     * @returns 
+     */
+    protected async generateMapOfConnectionsForChildData(forceGenerate = false) {
       const sessionData = await this.getSessionMapData();
       // There's no data for this map entity's children so generate one
-      if (!sessionData.size && this.children.size > 1) {
+      if (forceGenerate || (!sessionData.size && this.children.size > 1)) {
         let finalMapOfConnections: MapChildConnectionDataType = new Map();
 
         const getMapChildConnectionPairData = async (
