e207c755b1ff86cd9b8439c728f03201ddd4ac75
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-14 13:36:26 +0100

    Changes to fix bugs and ts errors

--- src/assets/app/scripts/ts/2_date_and_time/2_event_handlers.ts
+++ src/assets/app/scripts/ts/2_date_and_time/2_event_handlers.ts
@@ -1,21 +1,24 @@
+import { averageWalkingSpeed } from '../0_declarations/1_location_declarations/variables/0_other_data'
+import { updateGameTimeAfterChangingPassage } from './1_game_date_and_time_updater'
+
 const gameTimeUpdateAfterPassageNavigationEvent = new Event(
-  "gameTimeUpdatedAfterPassageNavigation"
-);
+  'gameTimeUpdatedAfterPassageNavigation'
+)
 
 // Update the game time after changing location but not when the browser window is restarted/refreshed
-$(document).one(":passageinit", () => {
-  $(document).on(":passageinit", (incomingPassage) => {
-    const currentPassageTitle = State.active.title;
+$(document).one(':passageinit', () => {
+  $(document).on(':passageinit', incomingPassage => {
+    const currentPassageTitle = State.active.title
     updateGameTimeAfterChangingPassage(
       currentPassageTitle,
       incomingPassage.passage.title,
-      NSLocation.averageWalkingSpeed[0]
-    );
+      averageWalkingSpeed[0]
+    )
 
     // Dispatch the event
-    document.dispatchEvent(gameTimeUpdateAfterPassageNavigationEvent);
+    document.dispatchEvent(gameTimeUpdateAfterPassageNavigationEvent)
 
-    return 0;
-  });
+    return 0
+  })
   // ev.preventDefault();
-});
+})
