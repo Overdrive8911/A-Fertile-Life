039a7fafe9d2283fcaed389d3e0312bf8eda0dcd
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-15 17:26:47 +0100

    Update SWC and Thyweaver configurations for improved build settings and debugging

--- .build/swc_config.ts
+++ .build/swc_config.ts
@@ -9,25 +9,40 @@ const swcOptions: Options = {
   jsc: {
     parser: {
       syntax: 'typescript',
+      // tsx: false,
+      // decorators: true,
     },
+    // target: 'es2024',
     minify:
       mode === 'production'
         ? {
-            mangle: false,
+            mangle: true,
             format: {
-              comments: 'all',
+              comments: false,
             },
             compress: {
               defaults: true,
               evaluate: true,
               inline: 3,
+              booleans: true,
+              booleans_as_integers: true,
+              drop_debugger: true,
+              dead_code: true,
+              if_return: true,
+              join_vars: true,
+              passes: 2,
+              // ecma: 'es2024',
             },
           }
         : undefined,
+    transform: {
+      treatConstEnumAsEnum: false,
+    },
   },
   env: {
     targets: config.builder!.compilation_target,
   },
+  module: { type: 'es6' },
 }
 
 export default swcOptions
