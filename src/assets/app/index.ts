761370bee3f74b94d8123f86ee6dd888f2ec7ac5
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-20 14:32:08 +0100

    Remove deprecated configuration files and update build process with Bun!

--- src/assets/app/index.ts
+++ src/assets/app/index.ts
@@ -1,5 +1,5 @@
-import './styles/main.scss'
-import * as D from './scripts/ts/0_declarations/1_location_declarations/0_classes'
+// import './styles/main.scss'
+import { GlobalMap } from './scripts/ts/0_declarations/1_location_declarations/0_classes'
 
-console.log('Hello from ThyWeaver!')
-const a = new D.GlobalMap(0, 'Test')
+// console.log('Hello from ThyWeaver!')
+new GlobalMap(0, 'Test')
