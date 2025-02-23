91bca747dac48d54d2655899f0e0d976c2b29702
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-23 18:32:05 +0100

    Add Fertilo Inc sub-region and update region identifiers in global map

--- src/assets/app/scripts/location/game_locations/regions.ts
+++ src/assets/app/scripts/location/game_locations/regions.ts
@@ -1,55 +1,55 @@
 import { Region } from '../classes'
 import { RegionId, Direction, Distance } from '../enums'
 
-export const northHirtheford = new Region(
+export const region_northHirtheford = new Region(
   RegionId.NORTH_HIRTHEFORD,
   'North Hirtheford'
 )
-export const eastHirtheford = new Region(
+export const region_eastHirtheford = new Region(
   RegionId.EAST_HIRTHEFORD,
   'East Hirtheford'
 )
-export const southHirtheford = new Region(
+export const region_southHirtheford = new Region(
   RegionId.SOUTH_HIRTHEFORD,
   'South Hirtheford'
 )
-export const westHirtheford = new Region(
+export const region_westHirtheford = new Region(
   RegionId.WEST_HIRTHEFORD,
   'West Hirtheford'
 )
-export const centralHirtheford = new Region(
+export const region_centralHirtheford = new Region(
   RegionId.CENTRAL_HIRTHEFORD,
   'West Hirtheford'
 )
 
-centralHirtheford.connectTo(
-  { area: northHirtheford, dir: Direction.NORTH, dist: Distance.LONG },
-  { area: eastHirtheford, dir: Direction.EAST, dist: Distance.LONG },
-  { area: southHirtheford, dir: Direction.SOUTH, dist: Distance.LONG },
-  { area: westHirtheford, dir: Direction.WEST, dist: Distance.LONG }
+region_centralHirtheford.connectTo(
+  { area: region_northHirtheford, dir: Direction.NORTH, dist: Distance.LONG },
+  { area: region_eastHirtheford, dir: Direction.EAST, dist: Distance.LONG },
+  { area: region_southHirtheford, dir: Direction.SOUTH, dist: Distance.LONG },
+  { area: region_westHirtheford, dir: Direction.WEST, dist: Distance.LONG }
 )
 
 // REVIEW: Would stuff like `Direction.NORTH_EAST` be better?
-northHirtheford.connectTo(
+region_northHirtheford.connectTo(
   {
-    area: eastHirtheford,
+    area: region_eastHirtheford,
     dir: Direction.EAST,
     dist: Distance.VERY_LONG,
   },
   {
-    area: westHirtheford,
+    area: region_westHirtheford,
     dir: Direction.WEST,
     dist: Distance.VERY_LONG,
   }
 )
-southHirtheford.connectTo(
+region_southHirtheford.connectTo(
   {
-    area: eastHirtheford,
+    area: region_eastHirtheford,
     dir: Direction.EAST,
     dist: Distance.VERY_LONG,
   },
   {
-    area: westHirtheford,
+    area: region_westHirtheford,
     dir: Direction.WEST,
     dist: Distance.VERY_LONG,
   }
--- src/assets/app/scripts/location/game_locations/sub_locations.ts
+++ src/assets/app/scripts/location/game_locations/sub_locations.ts
@@ -8,136 +8,140 @@
 import { SubLocation } from '../classes'
 import { SubLocationId, Direction } from '../enums'
 
-export const fertiloIncPorchFloor1 = new SubLocation(
+export const subLocation_fertiloIncPorch = new SubLocation(
   SubLocationId.PORCH,
   'Porch'
 )
-export const fertiloIncReceptionFloor1 = new SubLocation(
+export const subLocation_fertiloIncReception = new SubLocation(
   SubLocationId.RECEPTION,
   'Reception',
   'The Entry Point of Fertilo Inc'
 )
-export const fertiloIncMeasurementClosetFloor1 = new SubLocation(
+export const subLocation_fertiloIncMeasurementCloset = new SubLocation(
   SubLocationId.MEASUREMENT_CLOSET,
   'Measurement Closet'
 )
-export const fertiloIncPharmacy1Floor1 = new SubLocation(
+export const subLocation_fertiloIncPharmacy1 = new SubLocation(
   SubLocationId.PHARMACY_1,
   'Pharmacy 1'
 )
-export const fertiloIncPharmacy2Floor1 = new SubLocation(
+export const subLocation_fertiloIncPharmacy2 = new SubLocation(
   SubLocationId.PHARMACY_2,
   'Pharmacy 2'
 )
-export const fertiloIncCorridor1Floor1 = new SubLocation(
+export const subLocation_fertiloIncCorridor1 = new SubLocation(
   SubLocationId.CORRIDOR_1,
   'Corridor 1'
 )
-export const fertiloIncHallway1Floor1 = new SubLocation(
+export const subLocation_fertiloIncHallway1 = new SubLocation(
   SubLocationId.HALLWAY_1,
   'Hallway 1'
 )
-export const fertiloIncHallway2Floor1 = new SubLocation(
+export const subLocation_fertiloIncHallway2 = new SubLocation(
   SubLocationId.HALLWAY_2,
   'Hallway 2'
 )
-export const fertiloIncHallway3Floor1 = new SubLocation(
+export const subLocation_fertiloIncHallway3 = new SubLocation(
   SubLocationId.HALLWAY_3,
   'Hallway 3'
 )
-export const fertiloIncHallway4Floor1 = new SubLocation(
+export const subLocation_fertiloIncHallway4 = new SubLocation(
   SubLocationId.HALLWAY_4,
   'Hallway 4'
 )
-export const fertiloIncHallway5Floor1 = new SubLocation(
+export const subLocation_fertiloIncHallway5 = new SubLocation(
   SubLocationId.HALLWAY_5,
   'Hallway 5'
 )
-export const fertiloIncHallway6Floor1 = new SubLocation(
+export const subLocation_fertiloIncHallway6 = new SubLocation(
   SubLocationId.HALLWAY_6,
   'Hallway 6'
 )
-export const fertiloIncHallway7Floor1 = new SubLocation(
+export const subLocation_fertiloIncHallway7 = new SubLocation(
   SubLocationId.HALLWAY_7,
   'Hallway 7'
 )
-export const fertiloIncLabFloor1 = new SubLocation(
+export const subLocation_fertiloIncLab = new SubLocation(
   SubLocationId.LAB,
   'Laboratory'
 )
-export const fertiloIncConsultationFloor1 = new SubLocation(
+export const subLocation_fertiloIncConsultation = new SubLocation(
   SubLocationId.CONSULTATION,
   'Consultation Office'
 )
-export const fertiloIncOfficeWorkFloor1 = new SubLocation(
+export const subLocation_fertiloIncOfficeWork = new SubLocation(
   SubLocationId.OFFICE_WORK,
   'Office'
 )
 
-fertiloIncPorchFloor1.connectTo({
-  area: fertiloIncReceptionFloor1,
+subLocation_fertiloIncPorch.connectTo({
+  area: subLocation_fertiloIncReception,
   dir: Direction.NORTH,
   dist: 2,
 })
 
-fertiloIncReceptionFloor1.connectTo(
-  { area: fertiloIncMeasurementClosetFloor1, dir: Direction.EAST, dist: 1 },
-  { area: fertiloIncPharmacy1Floor1, dir: Direction.WEST, dist: 1 },
-  { area: fertiloIncCorridor1Floor1, dir: Direction.NORTH, dist: 2 }
+subLocation_fertiloIncReception.connectTo(
+  {
+    area: subLocation_fertiloIncMeasurementCloset,
+    dir: Direction.EAST,
+    dist: 1,
+  },
+  { area: subLocation_fertiloIncPharmacy1, dir: Direction.WEST, dist: 1 },
+  { area: subLocation_fertiloIncCorridor1, dir: Direction.NORTH, dist: 2 }
 )
 
-fertiloIncCorridor1Floor1.connectTo({
-  area: fertiloIncHallway4Floor1,
+subLocation_fertiloIncCorridor1.connectTo({
+  area: subLocation_fertiloIncHallway4,
   dir: Direction.NORTH,
   dist: 3,
 })
 
-fertiloIncHallway4Floor1.connectTo(
+subLocation_fertiloIncHallway4.connectTo(
   // { area: fertiloIncHallway1Floor1, dir: Direction.WEST, dist: 6 },
   // { area: fertiloIncHallway2Floor1, dir: Direction.WEST, dist: 4 },
-  { area: fertiloIncHallway3Floor1, dir: Direction.WEST, dist: 2 },
-  { area: fertiloIncHallway5Floor1, dir: Direction.EAST, dist: 2 }
+  { area: subLocation_fertiloIncHallway3, dir: Direction.WEST, dist: 2 },
+  { area: subLocation_fertiloIncHallway5, dir: Direction.EAST, dist: 2 }
   // { area: fertiloIncHallway6Floor1, dir: Direction.EAST, dist: 4 },
   // { area: fertiloIncHallway7Floor1, dir: Direction.EAST, dist: 6 }
 )
 
-fertiloIncHallway3Floor1.connectTo(
+subLocation_fertiloIncHallway3.connectTo(
   {
-    area: fertiloIncLabFloor1,
+    area: subLocation_fertiloIncLab,
     dir: Direction.SOUTH,
     dist: 2,
   },
-  { area: fertiloIncHallway2Floor1, dir: Direction.WEST, dist: 2 }
+  { area: subLocation_fertiloIncHallway2, dir: Direction.WEST, dist: 2 }
 )
 
-fertiloIncHallway2Floor1.connectTo({
-  area: fertiloIncHallway1Floor1,
+subLocation_fertiloIncHallway2.connectTo({
+  area: subLocation_fertiloIncHallway1,
   dir: Direction.WEST,
   dist: 2,
 })
 
-fertiloIncHallway5Floor1.connectTo(
+subLocation_fertiloIncHallway5.connectTo(
   {
-    area: fertiloIncPharmacy2Floor1,
+    area: subLocation_fertiloIncPharmacy2,
     dir: Direction.SOUTH,
     dist: 2,
   },
   {
-    area: fertiloIncConsultationFloor1,
+    area: subLocation_fertiloIncConsultation,
     dir: Direction.NORTH,
     dist: 2,
   },
-  { area: fertiloIncHallway6Floor1, dir: Direction.EAST, dist: 2 }
+  { area: subLocation_fertiloIncHallway6, dir: Direction.EAST, dist: 2 }
 )
 
-fertiloIncHallway6Floor1.connectTo({
-  area: fertiloIncHallway7Floor1,
+subLocation_fertiloIncHallway6.connectTo({
+  area: subLocation_fertiloIncHallway7,
   dir: Direction.EAST,
   dist: 2,
 })
 
-fertiloIncHallway7Floor1.connectTo({
-  area: fertiloIncOfficeWorkFloor1,
+subLocation_fertiloIncHallway7.connectTo({
+  area: subLocation_fertiloIncOfficeWork,
   dir: Direction.NORTH,
   dist: 2,
 })
