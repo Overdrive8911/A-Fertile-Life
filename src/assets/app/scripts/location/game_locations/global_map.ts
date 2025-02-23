e721846c66c8007b3653aed7bf97f691f9d3e642
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-23 21:16:54 +0100

    Add Fertilo Inc location and sub-locations; connect areas in global map

--- src/assets/app/scripts/location/game_locations/global_map.ts
+++ src/assets/app/scripts/location/game_locations/global_map.ts
@@ -1,5 +1,5 @@
 import { GlobalMap } from '../classes'
-import { GlobalMapId } from '../enums'
+import { Direction, Distance, GlobalMapId } from '../enums'
 import {
   region_northHirtheford,
   region_eastHirtheford,
@@ -9,10 +9,66 @@ import {
 } from './regions'
 
 const globalMap = new GlobalMap(GlobalMapId.GLOBAL, 'Global Map')
-globalMap.addArea(
-  region_northHirtheford,
-  region_eastHirtheford,
-  region_southHirtheford,
-  region_westHirtheford,
-  region_centralHirtheford
-)
+
+globalMap
+  .addArea(
+    region_northHirtheford,
+    region_eastHirtheford,
+    region_southHirtheford,
+    region_westHirtheford,
+    region_centralHirtheford
+  )
+  .connect(
+    {
+      from: region_centralHirtheford,
+      to: region_northHirtheford,
+      dir: Direction.NORTH,
+      dist: Distance.LONG,
+    },
+    {
+      from: region_centralHirtheford,
+      to: region_eastHirtheford,
+      dir: Direction.EAST,
+      dist: Distance.LONG,
+    },
+    {
+      from: region_centralHirtheford,
+      to: region_southHirtheford,
+      dir: Direction.SOUTH,
+      dist: Distance.LONG,
+    },
+    {
+      from: region_centralHirtheford,
+      to: region_westHirtheford,
+      dir: Direction.WEST,
+      dist: Distance.LONG,
+    }
+    // REVIEW: Would stuff like `Direction.NORTH_EAST` be better?
+    // {
+    //   from: region_northHirtheford,
+    //   to: region_eastHirtheford,
+    //   dir: Direction.EAST,
+    //   dist: Distance.VERY_LONG,
+    // },
+    // {
+    //   from: region_northHirtheford,
+    //   to: region_westHirtheford,
+    //   dir: Direction.WEST,
+    //   dist: Distance.VERY_LONG,
+    // },
+    // {
+    //   from: region_southHirtheford,
+    //   to: region_eastHirtheford,
+    //   dir: Direction.EAST,
+    //   dist: Distance.VERY_LONG,
+    // },
+    // {
+    //   from: region_southHirtheford,
+    //   to: region_westHirtheford,
+    //   dir: Direction.WEST,
+    //   dist: Distance.VERY_LONG,
+    // }
+  )
+
+//@ts-ignore
+window.tt = globalMap
