2cfa279a09d2754040ea595e4551b091a9d6b252
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-23 18:32:05 +0100

    Add Fertilo Inc sub-region and update region identifiers in global map

--- src/assets/app/scripts/location/game_locations/global_map.ts
+++ src/assets/app/scripts/location/game_locations/global_map.ts
@@ -1,18 +1,18 @@
 import { GlobalMap } from '../classes'
 import { GlobalMapId } from '../enums'
 import {
-  northHirtheford,
-  eastHirtheford,
-  southHirtheford,
-  westHirtheford,
-  centralHirtheford,
+  region_northHirtheford,
+  region_eastHirtheford,
+  region_southHirtheford,
+  region_westHirtheford,
+  region_centralHirtheford,
 } from './regions'
 
 const globalMap = new GlobalMap(GlobalMapId.GLOBAL, 'Global Map')
 globalMap.addArea(
-  northHirtheford,
-  eastHirtheford,
-  southHirtheford,
-  westHirtheford,
-  centralHirtheford
+  region_northHirtheford,
+  region_eastHirtheford,
+  region_southHirtheford,
+  region_westHirtheford,
+  region_centralHirtheford
 )
