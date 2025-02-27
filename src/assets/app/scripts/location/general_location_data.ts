0ba2d400795f6e955edc3cb87d01eaddd050f060
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-27 13:30:36 +0100

    Refactor location data to use SubLocationId enum and clean up unused code

--- src/assets/app/scripts/location/general_location_data.ts
+++ src/assets/app/scripts/location/general_location_data.ts
@@ -1,346 +1,50 @@
-// NOTE: (Barring the first) lm EVERY AREA MUST HAVE A CONNECTION TO AT LEAST ONE OTHER LOCATION. At least for now, don't try to make islands disconnected from others
+import { SubLocationId } from './enums'
+// TODO: Rename this file
 
-import { GlobalMap, SubLocation, Location } from './classes'
-import {
-  GlobalMapId,
-  SubLocationId,
-  Direction,
-  LocationId,
-  MapLocation,
-  MapLocationContainer,
-  MapSubLocation,
-  GameMapCoordinate,
-} from './enums'
-import { getCoordsRelativeToOtherSubLocation } from './functions'
-import type { LocationObject, LocationCoords } from './types_and_interfaces'
-import { gRelatedLocations } from './related_location_data'
-
-// NOTE - This stores EVERY possible location. Keep in mind that moving from coords [2,6] to [2,7] or [5,3] to [4,3] takes 10 seconds on average. Note that the `entry` sub location would have its distance calculated from [0,0]
-// NOTE - The first entry in `subLocations` is where the player will enter if they move into that particular location without a set destination (aka another sub location)
-// NOTE - Using getters for the coords of sub locations makes it easier to do edits down the line. It's also easier to understand what is connected to what
-// It's best to have an image to visualize how the coords would work
-export let gLocationData: LocationObject = {
-  // North Hirtheford
-  [MapLocation.FERTILO_INC_GROUND_FLOOR]: {
-    name: 'Fertilo Inc (Ground Floor)',
-    coords: gRelatedLocations[MapLocationContainer.FERTILO_INC].generalCoords,
-    subLocations: {
-      [MapSubLocation.PORCH]: {
-        coords: [0, 0],
-      },
-      [MapSubLocation.RECEPTION]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.PORCH,
-            0,
-            2,
-            0
-          )
-        },
-      },
-      // get [MapSubLocation.RECEPTION](): GameSubLocation {
-      //   const porchCoords = this[MapSubLocation.PORCH].coords;
-      //   return {
-      //     name: "Reception",
-      //     coords: [
-      //       porchCoords[LocationCoordIndex.X],
-      //       porchCoords[LocationCoordIndex.Y] + 2,
-      //     ],
-      //   };
-      // },
-      [MapSubLocation.MEASUREMENT_CLOSET]: {
-        name: 'Measurement Closet',
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.RECEPTION,
-            1,
-            0,
-            0
-          )
-        },
-      },
-
-      [MapSubLocation.PHARMACY_1]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.RECEPTION,
-            -1,
-            0,
-            0
-          )
-        },
-      },
-      [MapSubLocation.PHARMACY_2]: {
-        extraDirectionInfo: { west: GameMapCoordinate.BLOCKED },
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_5,
-            0,
-            -2,
-            0
-          )
-        },
-      },
-
-      [MapSubLocation.CORRIDOR_1]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.RECEPTION,
-            0,
-            2,
-            0
-          )
-        },
-        extraDirectionInfo: {
-          west: GameMapCoordinate.BLOCKED,
-          east: GameMapCoordinate.BLOCKED,
-        },
-      },
-      // [MapSubLocation.CORRIDOR_2]: {
-      //   coords: [2, 10],
-      // },
-      // [MapSubLocation.CORRIDOR_3]: {
-      //   coords: [0, 0],
-      // },
-
-      [MapSubLocation.HALLWAY_1]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_4,
-            -6,
-            0,
-            0
-          )
-        },
-      },
-      [MapSubLocation.HALLWAY_2]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_4,
-            -4,
-            0,
-            0
-          )
-        },
-      },
-      [MapSubLocation.HALLWAY_3]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_4,
-            -2,
-            0,
-            0
-          )
-        },
-      },
-      [MapSubLocation.HALLWAY_4]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.CORRIDOR_1,
-            0,
-            3,
-            0
-          )
-        },
-      },
-      [MapSubLocation.HALLWAY_5]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_4,
-            2,
-            0,
-            0
-          )
-        },
-      },
-      [MapSubLocation.HALLWAY_6]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_4,
-            4,
-            0,
-            0
-          )
-        },
-      },
-      [MapSubLocation.HALLWAY_7]: {
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_4,
-            6,
-            0,
-            0
-          )
-        },
-      },
-
-      [MapSubLocation.LAB]: {
-        name: 'Laboratory',
-        extraDirectionInfo: { east: GameMapCoordinate.BLOCKED },
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_3,
-            0,
-            -2,
-            0
-          )
-        },
-      },
-      [MapSubLocation.CONSULTATION]: {
-        name: 'Consultation Office',
-        extraDirectionInfo: { east: GameMapCoordinate.BLOCKED },
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_5,
-            0,
-            2,
-            0
-          )
-        },
-      },
-
-      [MapSubLocation.OFFICE_WORK]: {
-        name: 'Office',
-        extraDirectionInfo: { west: GameMapCoordinate.BLOCKED },
-        get [`coords`](): LocationCoords {
-          return getCoordsRelativeToOtherSubLocation(
-            MapLocation.FERTILO_INC_GROUND_FLOOR,
-            MapSubLocation.HALLWAY_7,
-            0,
-            2,
-            0
-          )
-        },
-      },
-    },
-  },
-  [MapLocation.FERTILO_INC_FIRST_FLOOR_UNDERGROUND]: {
-    name: 'Fertilo Inc',
-    coords: [
-      ...(gRelatedLocations[MapLocationContainer.FERTILO_INC].generalCoords as [
-        number,
-        number
-      ]),
-      -5,
-    ],
-    subLocations: {
-      [MapSubLocation.PLAYER_ROOM]: {
-        name: 'Your Room',
-        coords: [5, 5],
-      },
-    },
-  },
-
-  [MapLocation.FERTILO_INC_TOP_FLOOR]: {
-    name: 'Top Floor',
-    coords: [
-      gRelatedLocations[MapLocationContainer.FERTILO_INC].generalCoords[0],
-      gRelatedLocations[MapLocationContainer.FERTILO_INC].generalCoords[1],
-      5,
-    ],
-    subLocations: {
-      [MapSubLocation.CEO_OFFICE]: {
-        name: "Mr Fertilo's Office",
-        coords: [8, 6],
-      },
-    },
-  },
-
-  // East Hirtheford
-
-  // South Hirtheford
-
-  // West Hirtheford
-  [MapLocation.PLAYER_HOUSE]: {
-    name: 'Your Old House',
-    coords: [500, 500],
-    subLocations: {
-      [MapSubLocation.LIVING_ROOM]: {
-        name: 'Your Old Living Room',
-        coords: [1, 3],
-      },
-      [MapSubLocation.BATHROOM]: {
-        name: 'Your Old Bathroom',
-        coords: [5, 2],
-      },
-      [MapSubLocation.PORCH]: { name: 'Your Old Porch', coords: [0, -3] },
-      [MapSubLocation.BEDROOM]: {
-        name: 'Your Old Bedroom',
-        coords: [1, 6],
-      },
-    },
-  },
-
-  // Central Hirtheford
-
-  // Others
-  [MapLocation.BUS]: { name: 'Bus', coords: [0, 0] },
-  [MapLocation.DREAM]: { name: '???', coords: [0, 0] },
-  [MapLocation.UNKNOWN]: { name: '???', coords: [0, 0] },
-}
-// Attach it to setup for use in the browser console
-if (typeof setup == 'undefined') {
-  const setup = {} as any
-  setup.locationData = gLocationData
+function getUrl(subLocation: string) {
+  return `/media/img/map/icons/sub_location/${subLocation}.webp`
 }
 
 // Stores relative urls to the icons for sub locations
 // NOTE - Add the urls of sub locations with mini icons here. Use lowercase
-export const gSubLocationIcons24x24: { [key in MapSubLocation]?: string } = {
-  [MapSubLocation.DUMMY]: getUrl('dummy'),
+export const gSubLocationIcons24x24: { [key in SubLocationId]?: string } = {
+  [SubLocationId.DUMMY]: getUrl('dummy'),
 
-  [MapSubLocation.RECEPTION]: getUrl('reception'),
+  [SubLocationId.RECEPTION]: getUrl('reception'),
 
-  [MapSubLocation.HALLWAY_1]: getUrl('hallway'),
-  [MapSubLocation.HALLWAY_2]: getUrl('hallway'),
-  [MapSubLocation.HALLWAY_3]: getUrl('hallway'),
-  [MapSubLocation.HALLWAY_4]: getUrl('hallway'),
-  [MapSubLocation.HALLWAY_5]: getUrl('hallway'),
-  [MapSubLocation.HALLWAY_6]: getUrl('hallway'),
-  [MapSubLocation.HALLWAY_7]: getUrl('hallway'),
+  [SubLocationId.HALLWAY_1]: getUrl('hallway'),
+  [SubLocationId.HALLWAY_2]: getUrl('hallway'),
+  [SubLocationId.HALLWAY_3]: getUrl('hallway'),
+  [SubLocationId.HALLWAY_4]: getUrl('hallway'),
+  [SubLocationId.HALLWAY_5]: getUrl('hallway'),
+  [SubLocationId.HALLWAY_6]: getUrl('hallway'),
+  [SubLocationId.HALLWAY_7]: getUrl('hallway'),
 
-  [MapSubLocation.PHARMACY_1]: getUrl('pharmacy'),
-  [MapSubLocation.PHARMACY_2]: getUrl('pharmacy'),
+  [SubLocationId.PHARMACY_1]: getUrl('pharmacy'),
+  [SubLocationId.PHARMACY_2]: getUrl('pharmacy'),
 
-  [MapSubLocation.PORCH]: getUrl('porch'),
+  [SubLocationId.PORCH]: getUrl('porch'),
 
-  [MapSubLocation.CORRIDOR_1]: getUrl('corridor'),
-  [MapSubLocation.CORRIDOR_2]: getUrl('corridor'),
-  [MapSubLocation.CORRIDOR_3]: getUrl('corridor'),
+  [SubLocationId.CORRIDOR_1]: getUrl('corridor'),
+  [SubLocationId.CORRIDOR_2]: getUrl('corridor'),
+  [SubLocationId.CORRIDOR_3]: getUrl('corridor'),
 
-  [MapSubLocation.ROOM_1]: getUrl('room'),
-  [MapSubLocation.ROOM_2]: getUrl('room'),
-  [MapSubLocation.ROOM_3]: getUrl('room'),
-  [MapSubLocation.ROOM_4]: getUrl('room'),
-  [MapSubLocation.ROOM_5]: getUrl('room'),
+  [SubLocationId.ROOM_1]: getUrl('room'),
+  [SubLocationId.ROOM_2]: getUrl('room'),
+  [SubLocationId.ROOM_3]: getUrl('room'),
+  [SubLocationId.ROOM_4]: getUrl('room'),
+  [SubLocationId.ROOM_5]: getUrl('room'),
 
-  [MapSubLocation.LAB]: getUrl('lab'),
+  [SubLocationId.LAB]: getUrl('lab'),
 
-  [MapSubLocation.CONSULTATION]: getUrl('consultation'),
+  [SubLocationId.CONSULTATION]: getUrl('consultation'),
 
-  [MapSubLocation.OFFICE_WORK]: getUrl('office_work'),
+  [SubLocationId.OFFICE_WORK]: getUrl('office_work'),
 
-  [MapSubLocation.MEASUREMENT_CLOSET]: getUrl('measurement_closet'),
+  [SubLocationId.MEASUREMENT_CLOSET]: getUrl('measurement_closet'),
 
-  [MapSubLocation.PLAYER_ROOM]: getUrl('room'),
-  [MapSubLocation.BEDROOM]: getUrl('bedroom'),
-  [MapSubLocation.BATHROOM]: getUrl('bathroom'),
-  [MapSubLocation.LIVING_ROOM]: getUrl('living_room'),
-}
-function getUrl(subLocation: string) {
-  return `/media/img/map/icons/sub_location/${subLocation}.webp`
+  [SubLocationId.PLAYER_ROOM]: getUrl('room'),
+  [SubLocationId.BEDROOM]: getUrl('bedroom'),
+  [SubLocationId.BATHROOM]: getUrl('bathroom'),
+  [SubLocationId.LIVING_ROOM]: getUrl('living_room'),
 }
