a3fde6b58c24234350d06ea66537a0866e02e26c
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-27 15:44:09 +0100

    Formatting

--- src/assets/app/scripts/location/classes.ts
+++ src/assets/app/scripts/location/classes.ts
@@ -205,7 +205,7 @@ class MapEntity<
     return this
   }
 
-  getArea(areaId: ChildType["id"]) {
+  getArea(areaId: ChildType['id']) {
     let childArea: ChildType | null = null
 
     for (const [child] of this.childrenData) {
