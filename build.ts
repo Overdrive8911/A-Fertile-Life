b30297931e054afbd70247e68e88a4cdb7a51668
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-28 19:34:00 +0100

    Refactor build.ts to streamline setupTweego usage and replace Tweenode processing with terminal command execution

--- build.ts
+++ build.ts
@@ -1,4 +1,4 @@
-import { build, sleep, write } from 'bun'
+import { build, sleep, write, $ } from 'bun'
 import {
   bundleScriptAndStyleExtensions,
   cleanDirectories,
@@ -7,7 +7,7 @@ import {
 } from './.build/plugins'
 import { Directory, mode } from './.build/variables'
 import { watch } from 'fs-extra'
-import { setupTweego, Tweenode } from 'tweenode'
+import { setupTweego } from 'tweenode'
 // NOTE: None of the file watchers detect file deletions so keep that in mind, your best bet would be to rebuild the project
 
 // // Add explicit dependencies to the entry point so bun's build watcher will reload when these change
@@ -47,22 +47,34 @@ if (mode == 'development') {
 }
 
 sleep(2000).finally(async () => {
-  await setupTweego()
-  const tweego = new Tweenode({ writeToLog: true })
+  setupTweego().finally(async () => {
+    await compileStory()
+  })
+  // const tweego = new Tweenode({ writeToLog: true })
 
-  const compileStory = async () => {
-    await tweego.process({
-      input: {
-        storyDir: Directory.STORY,
-        useTwineTestMode: mode == 'development' ? true : false,
-        htmlHead: Directory.HEAD_CONTENT,
-        modules: [Directory.VENDOR],
-        scripts: Directory.BUNDLED_SCRIPTS_DIR,
-        styles: Directory.BUNDLED_STYLES_DIR,
-      },
-      // output: { fileName: Directory.BUNDLED_STORY_NAME, mode: 'file' },
-      output: { mode: 'file', fileName: Directory.BUNDLED_STORY_NAME },
-    })
+  async function compileStory() {
+    //ANCHOR: The only reason I'm manually using a terminal command is because `tweenode` occasionally errors out with no reasonable logs >~<
+    await $`cd .tweenode && ./tweego --head=../${
+      Directory.HEAD_CONTENT
+    } --module=../${Directory.VENDOR} ${
+      mode == 'development' ? '-t' : ''
+    } --output=../${Directory.BUNDLED_STORY_NAME} ../${Directory.STORY} ../${
+      Directory.BUNDLED_SCRIPTS_DIR
+    } ../${Directory.BUNDLED_STYLES_DIR}`
+
+    // write(Directory.BUNDLED_STORY_NAME, blob())
+    // await tweego.process({
+    //   input: {
+    //     storyDir: Directory.STORY,
+    //     useTwineTestMode: mode == 'development' ? true : false,
+    //     htmlHead: Directory.HEAD_CONTENT,
+    //     modules: [Directory.VENDOR],
+    //     scripts: Directory.BUNDLED_SCRIPTS_DIR,
+    //     styles: Directory.BUNDLED_STYLES_DIR,
+    //   },
+    //   // output: { fileName: Directory.BUNDLED_STORY_NAME, mode: 'file' },
+    //   output: { mode: 'file', fileName: Directory.BUNDLED_STORY_NAME },
+    // })
   }
 
   if (mode == 'development') {
