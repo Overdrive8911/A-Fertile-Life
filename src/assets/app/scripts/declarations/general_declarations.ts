7dd6019d2b3e7da54ac51e89d5a5714e3a965e91
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-28 12:52:27 +0100

    Add story flag management functions and update StoryFlags enum

--- src/assets/app/scripts/declarations/general_declarations.ts
+++ src/assets/app/scripts/declarations/general_declarations.ts
@@ -1,6 +1,30 @@
 export const enum StoryFlags {
+  NONE = 0,
   /**
    * Used to determine if a particular story-important scene is active
    */
   IS_EVENT_ACTIVE = 1 << 0,
+}
+
+/**
+ * NOTE: **This only tests if all the bits in the flag match, otherwise it returns false. So be careful with it for flags with multiple set bits. Use `isAnyStoryFlagActive()` instead.**
+ */
+export function isStoryFlagActive(flag: StoryFlags) {
+  return (variables().storyFlags & flag) == StoryFlags.NONE ? false : true
+}
+
+export function isAnyStoryFlagActive(flags: StoryFlags) {
+  return variables().storyFlags & flags ? true : false
+}
+
+export function setStoryFlag(flag: StoryFlags) {
+  variables().storyFlags |= flag
+}
+
+export function clearStoryFlag(flag: StoryFlags) {
+  variables().storyFlags &= ~flag
+}
+
+export function toggleStoryFlag(flag: StoryFlags) {
+  variables().storyFlags ^= flag
 }
