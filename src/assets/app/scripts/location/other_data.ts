81b318343e7762853508be97f9557eeebaaff7a1
Overdrive8911 <wuchidiscord@gmail.com>
2025-02-28 20:35:04 +0100

    Update default warp destination to player house in West Hirtheford region

--- src/assets/app/scripts/location/other_data.ts
+++ src/assets/app/scripts/location/other_data.ts
@@ -30,7 +30,7 @@ export const setMapPopoutZoomLvl = (lvl: number) => {
 
 export const gPlayerMapSpriteSrc = '/media/img/map/icons/player_map_sprite.webp'
 
-export const defaultWarpDestination: AreaUniqueId = `${GlobalMapId.GLOBAL}_${RegionId.DUMMY}_${SubRegionId.DUMMY}_${LocationId.DUMMY}_${SubLocationId.DUMMY}`
+export const defaultWarpDestination: AreaUniqueId = `${GlobalMapId.GLOBAL}_${RegionId.WEST_HIRTHEFORD}_${SubRegionId.TEST_NEIGHBOURHOOD}_${LocationId.PLAYER_HOUSE}_${SubLocationId.PLAYER_ROOM}`
 
 export let lastWarpDestination: AreaUniqueId = defaultWarpDestination // When the function `warpToArea()` is called, this value is updated if the warp is possible or has happened. When called by `navigateInDirectionOnMap()`, it is set to null instead
 export const setLastWarpDestination = (arg: typeof lastWarpDestination) => {
