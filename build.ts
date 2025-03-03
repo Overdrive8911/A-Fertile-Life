d4a02aa275a1fd9cc0e65a8efb5bdec70e1f5eba
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-03 21:49:52 +0100

    Add live reload support for story compilation and update player room sublocation ID

--- build.ts
+++ build.ts
@@ -90,8 +90,18 @@ if (buildResult) {
       )
     })
 
+    const subscription2 = watcher.subscribe(Directory.STORY, async () => {
+      await compileStory()
+      // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
+      await write(
+        `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
+        'Dummy'
+      )
+    })
+
     process.on('SIGINT', async () => {
       await (await subscription).unsubscribe()
+      await (await subscription2).unsubscribe()
       process.exit(0)
     })
   }
