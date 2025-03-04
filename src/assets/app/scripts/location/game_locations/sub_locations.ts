1f0f965c4ff4615f0d001dfa5e3ad47dfe0b1019
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-04 15:51:56 +0100

    Update comments in sub_locations.ts to clarify safe zone handling and passage behavior

--- src/assets/app/scripts/location/game_locations/sub_locations.ts
+++ src/assets/app/scripts/location/game_locations/sub_locations.ts
@@ -2,7 +2,11 @@
 
 As such, ensure that all "safe zones", which are areas where the player's can confidently save without the risk of issues, are defined at the top of this file and should NEVER have their positions altered unless you want to break them :p 
 
-Edit: I've added a safeguard that'll prevent the saved area id from messing up too bad as long as the passage doesn't *also* change. */
+Edit: I've added a safeguard that'll prevent the saved area id from messing up too bad as long as the passage doesn't *also* change. 
+
+TLDR: Don't think too much about anything if only a single area is attached to a passage. Even if multiple are attached to a passage, it shouldn't be much of an issue as long as you don't mess up the order. Even if you do, worst case scenario is that the player get's warped to a similar passage (and I'll probably implement a tool to manually fix that).
+
+If the player stays in a "safe-zone", this is a non-issue. */
 
 //ANCHOR - Assume that any areas with the same passage name are the same
 
