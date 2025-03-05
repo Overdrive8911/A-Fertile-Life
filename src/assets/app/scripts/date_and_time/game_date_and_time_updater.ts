19be640fece9ec6025016ae4d6e706f5940dabaa
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-05 15:39:31 +0100

    Refactor game time update logic by removing unused event handlers and simplifying distance calculations

--- src/assets/app/scripts/date_and_time/game_date_and_time_updater.ts
+++ src/assets/app/scripts/date_and_time/game_date_and_time_updater.ts
@@ -1,64 +1,20 @@
 import { averageWalkingSpeed } from '../location/other_data'
 
-export const updateGameTimeAfterChangingPassage = (
-  passageName1: string,
-  passageName2: string,
+export const updateTimeWithDistance = (
+  dist: number,
   movementSpeed: number /* in metres per second*/
 ) => {
-  // Get the distance in metres
-  const distBetweenLocations = setup.getDistanceToTravelFromLocation(
-    passageName1,
-    passageName2
-  )
-
   // Get the time to travel in seconds
   const timeToTravel = Math.floor(
-    (distBetweenLocations / movementSpeed) *
+    (dist / movementSpeed) *
       randomFloat(
         averageWalkingSpeed[0] * 10 - 1,
         averageWalkingSpeed[0] * 10 + 1
       )
   )
 
-  let extraTimeForRemainingInALocationInSeconds = 0
-  const passage1Location: string | undefined =
-    getLocationFromPassageTitle(passageName1)
-  const passage1SubLocation: string | undefined =
-    getSubLocationFromPassageTitle(passageName1)
-  const passage2Location: string | undefined =
-    getLocationFromPassageTitle(passageName2)
-  const passage2SubLocation: string | undefined =
-    getSubLocationFromPassageTitle(passageName2)
-
-  if (
-    passage1Location &&
-    passage2Location &&
-    passage1Location === passage2Location &&
-    (passage1SubLocation === passage2SubLocation ||
-      !passage1SubLocation ||
-      !passage2SubLocation)
-  ) {
-    // If the first and second location both exist and are the same, as well as their sub-locations regardless if either doesn't exist, then the player is still in the same location so calculate a random amount of time in seconds to spend
-    extraTimeForRemainingInALocationInSeconds = random(15, 65)
-  }
-
   // Change the in-game time
-  setup.updateGameTimeVariable(
-    timeToTravel + extraTimeForRemainingInALocationInSeconds
-  )
-
-  // Update the location data of the player
-  if (passage2Location != undefined) {
-    variables().player.locationData.location =
-      getMapLocationIdFromLocation(passage2Location)
-
-    if (passage2SubLocation != undefined) {
-      variables().player.locationData.subLocation =
-        getMapSubLocationIdFromSubLocation(passage2SubLocation)
-    } else {
-      variables().player.locationData.subLocation = null
-    }
-  }
+  setup.updateGameTimeVariable(timeToTravel)
 }
 
 // Skip forward `day` times to the specified time (in hrs and minutes)
