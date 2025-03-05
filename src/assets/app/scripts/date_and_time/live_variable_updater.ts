37e29d04e193a1d1b91cd30b97ba0b2158e60ff7
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-05 16:25:04 +0100

    Rename updateGameDateAndTimeDisplay to updateTime for clarity and consistency

--- src/assets/app/scripts/date_and_time/live_variable_updater.ts
+++ src/assets/app/scripts/date_and_time/live_variable_updater.ts
@@ -1,46 +1,46 @@
 // setup.locations is found in "location_data.twee"
 
-setup.updateGameDateAndTimeDisplay = () => {
+setup.updateTime = () => {
   // Update game date variable
   if (variables().gameDateAndTime !== undefined) {
-    const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
     const months = [
-      "JAN",
-      "FEB",
-      "MAR",
-      "APR",
-      "MAY",
-      "JUN",
-      "JUL",
-      "AUG",
-      "SEP",
-      "OCT",
-      "NOV",
-      "DEC",
-    ];
+      'JAN',
+      'FEB',
+      'MAR',
+      'APR',
+      'MAY',
+      'JUN',
+      'JUL',
+      'AUG',
+      'SEP',
+      'OCT',
+      'NOV',
+      'DEC',
+    ]
 
     // Get the day (e.g Monday)
-    const gameDateAndTime_Day = days[variables().gameDateAndTime!.getUTCDay()];
+    const gameDateAndTime_Day = days[variables().gameDateAndTime!.getUTCDay()]
 
     // Get the date (e.g 1st)
-    const gameDateAndTime_Date = variables().gameDateAndTime!.getUTCDate();
+    const gameDateAndTime_Date = variables().gameDateAndTime!.getUTCDate()
 
     // Get the month
     const gameDateAndTime_Month =
-      months[variables().gameDateAndTime!.getUTCMonth()];
+      months[variables().gameDateAndTime!.getUTCMonth()]
 
     // Put the string in the variable
-    variables().gameDateDisplay = `${gameDateAndTime_Day}, ${gameDateAndTime_Date} ${gameDateAndTime_Month}`;
+    variables().gameDateDisplay = `${gameDateAndTime_Day}, ${gameDateAndTime_Date} ${gameDateAndTime_Month}`
   }
 
   // Update game Time variable
   if (variables().gameDateAndTime !== undefined) {
     // Get the time in hours for the day
     const gameDateAndTime_DayHours: number =
-      variables().gameDateAndTime!.getUTCHours();
+      variables().gameDateAndTime!.getUTCHours()
     // Get the time in minutes for the hour
     const gameDateAndTime_DayMinutes: number =
-      variables().gameDateAndTime!.getUTCMinutes();
+      variables().gameDateAndTime!.getUTCMinutes()
 
     if (gameDateAndTime_DayHours < 12) {
       // AM
@@ -53,14 +53,14 @@ setup.updateGameDateAndTimeDisplay = () => {
           : gameDateAndTime_DayHours.toLocaleString(undefined, {
               minimumIntegerDigits: 2,
               useGrouping: false,
-            });
+            })
       const gameDateAndTime_DayMinutesFormatted: string =
         gameDateAndTime_DayMinutes.toLocaleString(undefined, {
           minimumIntegerDigits: 2,
           useGrouping: false,
-        });
+        })
 
-      variables().gameTimeDisplay = `${gameDateAndTime_DayHoursFormatted}:${gameDateAndTime_DayMinutesFormatted} AM`;
+      variables().gameTimeDisplay = `${gameDateAndTime_DayHoursFormatted}:${gameDateAndTime_DayMinutesFormatted} AM`
     } else {
       // PM
       const gameDateAndTime_DayHoursFormatted: string =
@@ -72,14 +72,14 @@ setup.updateGameDateAndTimeDisplay = () => {
           : (gameDateAndTime_DayHours - 12).toLocaleString(undefined, {
               minimumIntegerDigits: 2,
               useGrouping: false,
-            });
+            })
       const gameDateAndTime_DayMinutesFormatted: string =
         gameDateAndTime_DayMinutes.toLocaleString(undefined, {
           minimumIntegerDigits: 2,
           useGrouping: false,
-        });
+        })
 
-      variables().gameTimeDisplay = `${gameDateAndTime_DayHoursFormatted}:${gameDateAndTime_DayMinutesFormatted} PM`;
+      variables().gameTimeDisplay = `${gameDateAndTime_DayHoursFormatted}:${gameDateAndTime_DayMinutesFormatted} PM`
     }
 
     // console.log(variables().gameDateAndTime_Display);
@@ -87,15 +87,15 @@ setup.updateGameDateAndTimeDisplay = () => {
   }
 
   // Update the variables since its wrapped in a <<live>> macro
-  $("#ui-settings-button-time-border-and-bg").trigger(":liveupdate");
-};
+  $('#ui-settings-button-time-border-and-bg').trigger(':liveupdate')
+}
 
 // Update the time whenever the player moves to a another passage
-$(document).on(":passagedisplay", () => {
-  setup.updateGameDateAndTimeDisplay();
-});
+$(document).on(':passagedisplay', () => {
+  setup.updateTime()
+})
 
 // Update the time every 30s irl (note that it's still using the game time)
 setInterval(() => {
-  setup.updateGameDateAndTimeDisplay();
-}, 30000);
+  setup.updateTime()
+}, 30000)
