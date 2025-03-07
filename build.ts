cce9b4d8864624593ca2cd873e6efc92e3137627
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-07 22:20:34 +0100

    Refactor build script for improved readability and consistency; add favicon link to head content

--- build.ts
+++ build.ts
@@ -1,58 +1,58 @@
-import { build, write, $ } from 'bun'
+import { build, write, $ } from "bun";
 import {
   bundleScriptAndStyleExtensions,
   cleanDirectories,
   copyOtherAssets,
   processStyles,
-} from './.build/plugins'
-import { Directory, mode } from './.build/variables'
-import watcher from '@parcel/watcher'
-import { setupTweego } from 'tweenode'
+} from "./.build/plugins";
+import { Directory, mode } from "./.build/variables";
+import watcher from "@parcel/watcher";
+import { setupTweego } from "tweenode";
 
 const buildResult = await build({
-  entrypoints: [Directory.SCRIPT_ENTRYPOINT + ''],
-  outdir: Directory.BUNDLED_SCRIPTS_DIR + '',
-  minify: mode === 'production',
+  entrypoints: [Directory.SCRIPT_ENTRYPOINT + ""],
+  outdir: Directory.BUNDLED_SCRIPTS_DIR + "",
+  minify: mode === "production",
   plugins: [
     cleanDirectories,
     processStyles,
     bundleScriptAndStyleExtensions,
     copyOtherAssets,
   ],
-  drop: mode === 'production' ? ['console', 'window'] : [],
-})
+  drop: mode === "production" ? ["console", "window"] : [],
+});
 
-if (mode == 'development') {
+if (mode == "development") {
   const tryBuild = async () => {
     try {
       await build({
-        entrypoints: [Directory.SCRIPT_ENTRYPOINT + ''],
-        outdir: Directory.BUNDLED_SCRIPTS_DIR + '',
-      })
+        entrypoints: [Directory.SCRIPT_ENTRYPOINT + ""],
+        outdir: Directory.BUNDLED_SCRIPTS_DIR + "",
+      });
     } catch (error) {
-      console.log('Build failed. Error:', error)
-      console.log('Retrying')
-      await tryBuild()
+      console.log("Build failed. Error:", error);
+      console.log("Retrying");
+      await tryBuild();
     }
-  }
+  };
   let subscription = watcher.subscribe(Directory.APP, async (_, events) => {
-    events.forEach(async e => {
-      if (e.path.endsWith('.ts')) {
-        await tryBuild()
+    events.forEach(async (e) => {
+      if (e.path.endsWith(".ts")) {
+        await tryBuild();
       }
-    })
-  })
+    });
+  });
 
-  process.on('SIGINT', async () => {
-    await (await subscription).unsubscribe()
-    process.exit(0)
-  })
+  process.on("SIGINT", async () => {
+    await (await subscription).unsubscribe();
+    process.exit(0);
+  });
 }
 
 if (buildResult) {
   setupTweego().finally(async () => {
-    await compileStory()
-  })
+    await compileStory();
+  });
   // const tweego = new Tweenode({ writeToLog: true })
 
   async function compileStory() {
@@ -60,10 +60,10 @@ if (buildResult) {
     await $`cd .tweenode && ./tweego --head=../${
       Directory.HEAD_CONTENT
     } --module=../${Directory.VENDOR} ${
-      mode == 'development' ? '-t' : ''
+      mode == "development" ? "-t" : ""
     } --output=../${Directory.BUNDLED_STORY_NAME} ../${Directory.STORY} ../${
       Directory.BUNDLED_SCRIPTS_DIR
-    } ../${Directory.BUNDLED_STYLES_DIR}`
+    } ../${Directory.BUNDLED_STYLES_DIR}`;
 
     // write(Directory.BUNDLED_STORY_NAME, blob())
     // await tweego.process({
@@ -80,33 +80,33 @@ if (buildResult) {
     // })
   }
 
-  if (mode == 'development') {
+  if (mode == "development") {
     const subscription = watcher.subscribe(Directory.OUTPUT, async () => {
-      await compileStory()
+      await compileStory();
       // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
       await write(
         `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
-        'Dummy'
-      )
-    })
+        "Dummy"
+      );
+    });
 
     const subscription2 = watcher.subscribe(Directory.STORY, async () => {
-      await compileStory()
+      await compileStory();
       // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
       await write(
         `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
-        'Dummy'
-      )
-    })
+        "Dummy"
+      );
+    });
 
-    process.on('SIGINT', async () => {
-      await (await subscription).unsubscribe()
-      await (await subscription2).unsubscribe()
-      process.exit(0)
-    })
+    process.on("SIGINT", async () => {
+      await (await subscription).unsubscribe();
+      await (await subscription2).unsubscribe();
+      process.exit(0);
+    });
   }
 
-  await compileStory()
+  await compileStory();
 }
 
-export {}
+export {};
