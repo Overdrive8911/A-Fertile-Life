4b646e30388ffadaacfcabb458348606db896219
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-07 15:52:05 +0100

    Add event listener to initialize PIXI application on passage end

--- src/assets/app/scripts/location/map_code/map_init.ts
+++ src/assets/app/scripts/location/map_code/map_init.ts
@@ -1,3 +1,18 @@
 import { Application } from 'pixi.js'
 
-const application = new Application()
+$(window).on(':passageend', async () => {
+  const container = document.querySelector('.ui-side-bar-popout-map')
+  console.log(container)
+  if (container) {
+    const app = new Application()
+
+    await app.init({ background: '#1099bb' })
+
+    // Then adding the application's canvas to the DOM body.
+    container.prepend(app.canvas)
+  } else {
+    console.error(
+      'No element with the class "ui-side-bar-popout-map" was found.'
+    )
+  }
+})
