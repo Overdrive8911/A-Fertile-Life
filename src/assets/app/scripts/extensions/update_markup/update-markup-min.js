945ea6d5359a54a92a0c4f36786bb6c94007a218
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-08 18:24:52 +0100

    Fix variable updates to time :3

--- src/assets/app/scripts/extensions/update_markup/update-markup-min.js
+++ src/assets/app/scripts/extensions/update_markup/update-markup-min.js
@@ -1,3 +1,58 @@
-/* Mali's update wrapper markup */
+/* Mali's update markup */
 
-((e=!1)=>{const t=Wikifier.helpers.shadowHandler||Wikifier.helpers.createShadowSetterCallback,a=()=>$('[role="update-wrapper"]').each(((e,t)=>t.update()));Wikifier.Parser.add({name:"updateMarkup",match:"{.*?{.*?}}",handler(e){const[a,r,p]=e.matchText.match(/{(.*?){(.*?)}}/),i=$(`<${r.trim()||"span"} role='update-wrapper'>`).text(stringFrom(State.getVar(p))).get(0),n=t(`State.getVar("${p.replace(/"|'|`/g,(e=>`\\${e}`))}")`);i.update=function(){this.innerText=stringFrom(n())},e.output.append(i)}}),$(document).on("change click drop refreshUpdateContainers",(()=>{e||(a(),e=!0,setTimeout((()=>e=!1),100))})),setup.updateWrappers=a})();
+((isCooldown = false) => {
+  const shadowHandler = Wikifier.helpers.shadowHandler || Wikifier.helpers.createShadowSetterCallback;
+
+  const wrappers = [];
+
+  // Update function
+  const updateWrappers = () => {
+    if (isCooldown) return;
+
+    let i = wrappers.length - 1;
+
+    while (i >= 0) {
+      const { wrapper, shadowGetter } = wrappers[i];
+
+      // out of DOM, remove it
+      if (!wrapper.isConnected) {
+        wrappers.splice(i--, 1);
+        continue;
+      }
+
+      const cnt = stringFrom(shadowGetter());
+      if (wrapper.innerText !== cnt) wrapper.innerText = cnt;
+      i--;
+    }
+
+    isCooldown = true;
+    setTimeout(() => isCooldown = false, 150);
+  };
+
+  Wikifier.Parser.add({
+    name: 'updateMarkup',
+    match: '{.*?{.*?}}',
+    handler(w) {
+
+      const [_, elem, raw] = w.matchText.match(/{(.*?){(.*?)}}/),
+        wrapper = $(`<${elem.trim() || 'span'} aria-live='polite'>`)[0];
+
+      // push reference object
+      wrappers.push({
+        wrapper,
+        shadowGetter: shadowHandler(`State.getVar("${raw.replace(/"|'|`/g, m => `\\${m}`)}")`)
+      });
+
+      wrapper.innerText = stringFrom(State.getVar(raw));
+
+      w.output.appendChild(wrapper);
+    }
+  });
+
+  $(document).on('change click drop keyup', updateWrappers);
+
+  // Exports.
+  setup.updateWrappers = updateWrappers;
+
+  Wikifier.Parser.Profile.compile();
+})();
