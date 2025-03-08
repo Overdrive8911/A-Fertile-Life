32294b77918840a9b5669f35a8304ca20b2506a6
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-08 16:01:40 +0100

    Organize inventory and items module imports, enhance item ID handling, and improve tooltip functionality

--- src/assets/app/scripts/inventory_and_items/inventory_event_handlers.ts
+++ src/assets/app/scripts/inventory_and_items/inventory_event_handlers.ts
@@ -1,47 +1,50 @@
+import { openInventoryDialog } from "./inventory_dialog";
 
-  // SECTION - For adding functionality to the inventory icon in the top bar
-  $(document).on(":passageend", () => {
-    const inventoryBtn = $(".ui-settings-button-inventory");
+// SECTION - For adding functionality to the inventory icon in the top bar
+$(document).on(":passageend", () => {
+  const inventoryBtn = $(".ui-settings-button-inventory");
 
-    inventoryBtn.ariaClick(() => {
-      openInventoryDialog();
-    });
+  inventoryBtn.ariaClick(() => {
+    openInventoryDialog();
   });
-  // !SECTION
+});
+// !SECTION
 
-  // SECTION - Deal with the inventory tooltip and its quirks
-  export function inventoryTooltipHandler() {
-    $(".inventory-item-image").on("mouseover", () => {
-      let tooltipContainer: JQuery<HTMLElement>;
-      let inventoryImageContainer: JQuery<HTMLElement>;
+// SECTION - Deal with the inventory tooltip and its quirks
+export function inventoryTooltipHandler() {
+  $(".inventory-item-image").on("mouseover", () => {
+    let tooltipContainer: JQuery<HTMLElement> = {} as any;
+    let inventoryImageContainer: JQuery<HTMLElement> = {} as any;
 
-      for (let i of $(".inventory-tooltip")) {
-        if ($(i).css("display") != "none") {
-          // The active tooltip
-          tooltipContainer = $(i);
+    for (let i of $(".inventory-tooltip")) {
+      if ($(i).css("display") != "none") {
+        // The active tooltip
+        tooltipContainer = $(i);
 
-          // The image container that is the parent to the active tooltip
-          inventoryImageContainer = tooltipContainer.parent();
-        }
+        // The image container that is the parent to the active tooltip
+        inventoryImageContainer = tooltipContainer.parent();
       }
+    }
 
-      const inventoryImageContainerBottom =
-        inventoryImageContainer.offset().top + inventoryImageContainer.height();
+    const inventoryImageContainerBottom =
+      (inventoryImageContainer.offset()?.top ?? 0) +
+      (inventoryImageContainer.height() ?? 0);
 
-      // Ensure that the tooltip container stays right beneath the inventory item's image
-      tooltipContainer.offset({ top: inventoryImageContainerBottom });
+    // Ensure that the tooltip container stays right beneath the inventory item's image
+    tooltipContainer.offset({ top: inventoryImageContainerBottom });
 
-      // Make sure that the tool tip doesn't cut out of the view port. Instead, shift it above the image
-      const tooltipContainerBottom =
-        tooltipContainer.offset().top + tooltipContainer.height();
-      const viewportBottom = $(window).height();
+    // Make sure that the tool tip doesn't cut out of the view port. Instead, shift it above the image
+    const tooltipContainerBottom =
+      (tooltipContainer.offset()?.top ?? 0) + (tooltipContainer.height() ?? 0);
+    const viewportBottom = $(window).height();
 
-      if (tooltipContainerBottom > viewportBottom) {
-        tooltipContainer.offset({
-          top: inventoryImageContainer.offset().top - tooltipContainer.height(),
-        });
-      }
-    });
-  }
-  // !SECTION
+    if (tooltipContainerBottom > (viewportBottom ?? 0)) {
+      tooltipContainer.offset({
+        top:
+          (inventoryImageContainer.offset()?.top ?? 0) -
+          (tooltipContainer.height() ?? 0),
+      });
+    }
+  });
 }
+// !SECTION
