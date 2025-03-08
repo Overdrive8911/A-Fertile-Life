b17112f99979f79213085d1e489a4b90ecb73051
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-08 08:36:38 +0100

    Add date and time management scripts with macros for time manipulation

--- src/assets/app/scripts/date_and_time/date_and_time_declarations.ts
+++ src/assets/app/scripts/date_and_time/date_and_time_declarations.ts
@@ -1,9 +1,9 @@
-setup.updateGameTimeVariable = (timeInSeconds: number) => {
+export function updateGameTimeVariable(timeInSeconds: number) {
   // copy out the date
-  const oldDate = new Date(variables().gameDateAndTime);
+  const oldDate = new Date(variables().gameDateAndTime)
 
   // Convert to milliseconds and add to the old date
   variables().gameDateAndTime = new Date(
     oldDate.getTime() + 1000 * timeInSeconds
-  );
-};
+  )
+}
