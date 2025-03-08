02c0eed5d7a0640194189b83f5f2073cc9fedf43
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-08 15:22:38 +0100

    Add UI module imports and refactor player variable initialization for improved structure and functionality

--- src/assets/app/scripts/declarations/body_stats/body_stats_declarations.ts
+++ src/assets/app/scripts/declarations/body_stats/body_stats_declarations.ts
@@ -1,95 +1,91 @@
 //ANCHOR - Boobs, Booty, Hips, Waist, General body fat.
 
+export // TODO - Make each breast a class :D with a link to the other. Or not, this is only gonna affect the player anyway... Or not! CLASSES
+// NOTE - Breast size doesn't particularly boost milk production, however the two may increase side by side
+enum CupSize {
+  // Cup size per breast in CCs. I basically just copied the measurements from Free Cites Pregmod :p
+  /* Size per breast in CCs. 0 - 299 -> flat, 300 - 399 -> A-cup, 400 - 499 -> B-cup, 500 - 649 -> C-cup, 650 - 799 -> D-cup, 800 - 999 -> DD-cup, 1000 - 1199 -> F-cup, 1200 - 1399 -> G-cup, 1400 - 1599 -> H-cup, 1600 - 1799 -> I-cup, 1800 - 2049 -> J-cup, 2050 - 2299 -> K-cup, 2300 - 2599 -> L-cup, 2600 - 2899 -> M-cup, 2900 - 3249 -> N-cup, 3250-3599 -> O-cup, 3600 - 3949 -> P-cup, 3950 - 4299 -> Q-cup, 4300 - 4699 -> R-cup, 4700 - 5099 -> S-cup, 5100 - 10499 -> massive */
+  ZERO,
+  AA = 299, // From 299 to ZERO is AA-cup (or is it FLAT?)
+  A = 399, // From 399 to 300 (AA-cup + 1) is an A-cup
+  B = 499, // From 499 to 400 (A-cup + 1) is a B-cup
+  C = 649, // From 649 to 500 (B-cup + 1) is a C-cup
+  D = 799, // I think you get it by now
+  DD = 999,
+  E = 1049,
+  F = 1199,
+  G = 1399,
+  H = 1599,
+  I = 1799,
+  J = 2049,
+  K = 2299,
+  L = 2599,
+  M = 2899,
+  N = 3249,
+  // O = 3599, // Not using any of these rn
+  // P = 3949,
+  // Q = 4299,
+  // R = 4699,
+  // S = 5099,
+}
 
-  export 
-    // TODO - Make each breast a class :D with a link to the other. Or not, this is only gonna affect the player anyway... Or not! CLASSES
-    // NOTE - Breast size doesn't particularly boost milk production, however the two may increase side by side
-    export enum CupSize {
-      // Cup size per breast in CCs. I basically just copied the measurements from Free Cites Pregmod :p
-      /* Size per breast in CCs. 0 - 299 -> flat, 300 - 399 -> A-cup, 400 - 499 -> B-cup, 500 - 649 -> C-cup, 650 - 799 -> D-cup, 800 - 999 -> DD-cup, 1000 - 1199 -> F-cup, 1200 - 1399 -> G-cup, 1400 - 1599 -> H-cup, 1600 - 1799 -> I-cup, 1800 - 2049 -> J-cup, 2050 - 2299 -> K-cup, 2300 - 2599 -> L-cup, 2600 - 2899 -> M-cup, 2900 - 3249 -> N-cup, 3250-3599 -> O-cup, 3600 - 3949 -> P-cup, 3950 - 4299 -> Q-cup, 4300 - 4699 -> R-cup, 4700 - 5099 -> S-cup, 5100 - 10499 -> massive */
-      ZERO,
-      AA = 299, // From 299 to ZERO is AA-cup (or is it FLAT?)
-      A = 399, // From 399 to 300 (AA-cup + 1) is an A-cup
-      B = 499, // From 499 to 400 (A-cup + 1) is a B-cup
-      C = 649, // From 649 to 500 (B-cup + 1) is a C-cup
-      D = 799, // I think you get it by now
-      DD = 999,
-      E = 1049,
-      F = 1199,
-      G = 1399,
-      H = 1599,
-      I = 1799,
-      J = 2049,
-      K = 2299,
-      L = 2599,
-      M = 2899,
-      N = 3249,
-      // O = 3599, // Not using any of these rn
-      // P = 3949,
-      // Q = 4299,
-      // R = 4699,
-      // S = 5099,
-    }
+export interface SingleBreast {
+  size: CupSize | number;
+  milkCapacity: MilkCapacity | number;
+  isLactating: boolean;
+  nipple: NippleDescription;
+  areola: AreolaDescription;
+  // location: Location;
+}
 
-    export interface SingleBreast {
-      size: BreastData.CupSize | number;
-      milkCapacity: BreastData.MilkCapacity | number;
-      isLactating: boolean;
-      nipple: BreastData.NippleDescription;
-      areola: BreastData.AreolaDescription;
-      // location: BreastData.Location;
-    }
+// Milk Capacity (or Breast Storage Capacity) is determined by the number of mammary glands (or lobules / ducts).
+// The human body produces more milk if it is used regularly but slows down as time between milking increases.
+// NOTE - Milking till the breast is empty encourages further milk production
+// Small capacity = ~2.5 oz. Average capacity = ~3.5 oz. Large Capacity = ~5 oz. Unusually large capacity = ~10 oz.
+// Average daily milk intake = ~25 to 30 oz (could range from 16.16 to 48.85 oz with the more typical range between 19.27 to 30.43 oz)
+// A single breastfeeding session could range from 1.82 to 7.91 oz
+// Multiply the ounces by 29.574 to get the value in millimetres / cubic centimetres
+export enum MilkCapacity {
+  // in CC / ml, not ounces.
+  // NOTE - This is simply for gameplay purposes :p
+  // When properly emptied / milked, the milk capacity builds up a bit more. Going on extended periods without properly milking reduces this
+  VERY_LOW = 54, // 54 and below is VERY_LOW
+  LOW = 74, // 74 to 55 is LOW
+  AVERAGE = 104, // 104 to 75 is AVERAGE
+  HIGH = 148, // Ya get it
+  VERY_HIGH = 234,
+  EXTREMELY_HIGH = 296,
+  HUCOW = 400,
+}
 
-    // Milk Capacity (or Breast Storage Capacity) is determined by the number of mammary glands (or lobules / ducts).
-    // The human body produces more milk if it is used regularly but slows down as time between milking increases.
-    // NOTE - Milking till the breast is empty encourages further milk production
-    // Small capacity = ~2.5 oz. Average capacity = ~3.5 oz. Large Capacity = ~5 oz. Unusually large capacity = ~10 oz.
-    // Average daily milk intake = ~25 to 30 oz (could range from 16.16 to 48.85 oz with the more typical range between 19.27 to 30.43 oz)
-    // A single breastfeeding session could range from 1.82 to 7.91 oz
-    // Multiply the ounces by 29.574 to get the value in millimetres / cubic centimetres
-    export enum MilkCapacity {
-      // in CC / ml, not ounces.
-      // NOTE - This is simply for gameplay purposes :p
-      // When properly emptied / milked, the milk capacity builds up a bit more. Going on extended periods without properly milking reduces this
-      VERY_LOW = 54, // 54 and below is VERY_LOW
-      LOW = 74, // 74 to 55 is LOW
-      AVERAGE = 104, // 104 to 75 is AVERAGE
-      HIGH = 148, // Ya get it
-      VERY_HIGH = 234,
-      EXTREMELY_HIGH = 296,
-      HUCOW = 400,
-    }
+export const enum NippleDescription {
+  NONE,
+  FLAT,
+  PUFFY,
+  HAIRY,
+  PROTRUDING,
+  INVERTED,
+  BUMPY,
+}
 
-    export const enum NippleDescription {
-      NONE,
-      FLAT,
-      PUFFY,
-      HAIRY,
-      PROTRUDING,
-      INVERTED,
-      BUMPY,
-    }
+export const enum AreolaDescription {
+  // This is using bit fields since an areola would have 2 different possible properties and I didn't feel like making 2 enums/variables
+  // ANCHOR - Shape
+  ROUND = 1 << 0,
+  OVAL = 1 << 1,
 
-    export const enum AreolaDescription {
-      // This is using bit fields since an areola would have 2 different possible properties and I didn't feel like making 2 enums/variables
-      // ANCHOR - Shape
-      ROUND = 1 << 0,
-      OVAL = 1 << 1,
+  // ANCHOR - Size
+  LARGE = 1 << 2,
+  NORMAL = 1 << 3,
+  SMALL = 1 << 4,
 
-      // ANCHOR - Size
-      LARGE = 1 << 2,
-      NORMAL = 1 << 3,
-      SMALL = 1 << 4,
+  // ANCHOR - Defaults
+  DEFAULT = ROUND | NORMAL,
+}
 
-      // ANCHOR - Defaults
-      DEFAULT = ROUND | NORMAL,
-    }
-
-    export enum Location {
-      // NOTE - The order here is important because this is the same order by which data is added when breasts are initialized in their class. Also, only 3 locations will EVER be available.
-      LEFT,
-      RIGHT,
-      MIDDLE,
-    }
-  }
+export enum Location {
+  // NOTE - The order here is important because this is the same order by which data is added when breasts are initialized in their class. Also, only 3 locations will EVER be available.
+  LEFT,
+  RIGHT,
+  MIDDLE,
 }
--- src/assets/app/scripts/ui/declarations/side_section_declarations.ts
+++ src/assets/app/scripts/ui/declarations/side_section_declarations.ts
@@ -1,179 +1,175 @@
+function copyActionInterfaceContentsToSideBar() {
+  const verySlimMobileWidth = "screen and (max-width: 415px)";
 
-  function copyActionInterfaceContentsToSideBar() {
-    const verySlimMobileWidth = "screen and (max-width: 415px)";
-
-    if (window.matchMedia(verySlimMobileWidth).matches) {
-      const actionInterface = $("[id='ui-side-bar-action-interface']");
-      for (const actionInterfaceChild of actionInterface.children()) {
-        // Copy the each child in the action interface e.g the map popout
-        if (!actionInterface.hasClass("stowed")) {
-          $("#ui-side-bar-backup-container1 > :nth-child(3)").append(
-            $(actionInterfaceChild).clone(true)
-          );
-        } else {
-          // Empty the container
-          $("#ui-side-bar-backup-container1 > :nth-child(3)").empty();
-        }
+  if (window.matchMedia(verySlimMobileWidth).matches) {
+    const actionInterface = $("[id='ui-side-bar-action-interface']");
+    for (const actionInterfaceChild of actionInterface.children()) {
+      // Copy the each child in the action interface e.g the map popout
+      if (!actionInterface.hasClass("stowed")) {
+        $("#ui-side-bar-backup-container1 > :nth-child(3)").append(
+          $(actionInterfaceChild).clone(true)
+        );
+      } else {
+        // Empty the container
+        $("#ui-side-bar-backup-container1 > :nth-child(3)").empty();
       }
     }
   }
+}
 
-  // Define the handler for toggling the sidebar
-  export function uiSideBarToggleHandler() {
-    //SECTION - Deal with the mobile aspect
-    // Copy the hidden icons from the top bar and paste them into the side bar if the screen is too narrow
-    // Also hide the stat bars and display them in the sidebar too
+// Define the handler for toggling the sidebar
+export function uiSideBarToggleHandler() {
+  //SECTION - Deal with the mobile aspect
+  // Copy the hidden icons from the top bar and paste them into the side bar if the screen is too narrow
+  // Also hide the stat bars and display them in the sidebar too
 
-    let prevMobileMaxWidth;
-    const slimMobileWidth = "screen and (max-width: 500px)";
-    const wideMobileWidth = "screen and (max-width: 780px)";
-    const generalMobileUISettingsReset = () => {
-      // TODO - PLEASE REVISE THIS
-      $("#ui-side-bar-backup-container1 > div").empty();
+  let prevMobileMaxWidth;
+  const slimMobileWidth = "screen and (max-width: 500px)";
+  const wideMobileWidth = "screen and (max-width: 780px)";
+  const generalMobileUISettingsReset = () => {
+    // TODO - PLEASE REVISE THIS
+    $("#ui-side-bar-backup-container1 > div").empty();
 
-      $("#ui-side-bar-backup-container2").empty();
+    $("#ui-side-bar-backup-container2").empty();
 
+    $("[id|='ui-navigation-option-button']").removeClass(
+      "ui-navigation-button-small"
+    );
+  };
+
+  // if (settings.uiSideBarToggle) {
+  //   $("#ui-side-bar").addClass("stowed");
+  // } else if (!settings.uiSideBarToggle) {
+  //   $("#ui-side-bar").removeClass("stowed");
+  // }
+  if (window.matchMedia(slimMobileWidth).matches) {
+    //SECTION - For slim portrait modes on mobile
+    // Reset general changes if coming from another mobile width range
+    if (prevMobileMaxWidth !== slimMobileWidth) {
+      generalMobileUISettingsReset();
+    }
+    //Deal with the leftmost icons
+    for (const uiIcon of $("#ui-settings-buttons").children()) {
+      // Copy the data for all the leftmost icons with their event handlers and show them in the side bar
+      if (!$("[id='ui-side-bar']").hasClass("stowed")) {
+        $("#ui-side-bar-backup-container1 > :nth-child(1)").append(
+          $(uiIcon).clone(true)
+        );
+      } else {
+        // Empty the container
+        $("#ui-side-bar-backup-container1 > :nth-child(1)").empty();
+      }
+    }
+
+    //Deal with the rightmost icons (money/rep)
+    for (const uiIcon of $("#ui-stat-others").children()) {
+      // Copy the data for all the rightmost icons with their event handlers and show them in the side bar
+      if (!$("[id='ui-side-bar']").hasClass("stowed")) {
+        $("#ui-side-bar-backup-container1 > :nth-child(2)").append(
+          $(uiIcon).clone(true)
+        );
+      } else {
+        // Empty the container
+        $("#ui-side-bar-backup-container1 > :nth-child(2)").empty();
+      }
+    }
+
+    //Deal with the stat bars
+    // It will copy every stat bar individually and display it as a column in the side bar. To do that, I'll have to go through each stat bar column group and then get the stat bar. I think I could use `.find(".ui-stat-bar-and-icon") but eh
+    for (const statBarColumnGroup of $("#ui-stat-bars").children()) {
+      for (const statBar of $(statBarColumnGroup).children()) {
+        // Copy the each stat bar and paste into the side bar
+        if (!$("[id='ui-side-bar']").hasClass("stowed")) {
+          $("#ui-side-bar-backup-container2").append($(statBar).clone(true));
+        } else {
+          // Empty the container
+          $("#ui-side-bar-backup-container2").empty();
+        }
+      }
+    }
+
+    //Extra
+    if (!$("[id='ui-side-bar']").hasClass("stowed")) {
+      // Change the size of the bottom bar navigation settings
+      $("[id|='ui-navigation-option-button']").addClass(
+        "ui-navigation-button-small"
+      );
+    } else {
       $("[id|='ui-navigation-option-button']").removeClass(
         "ui-navigation-button-small"
       );
-    };
+    }
 
-    // if (settings.uiSideBarToggle) {
-    //   $("#ui-side-bar").addClass("stowed");
-    // } else if (!settings.uiSideBarToggle) {
-    //   $("#ui-side-bar").removeClass("stowed");
-    // }
-    if (window.matchMedia(slimMobileWidth).matches) {
-      //SECTION - For slim portrait modes on mobile
-      // Reset general changes if coming from another mobile width range
-      if (prevMobileMaxWidth !== slimMobileWidth) {
-        generalMobileUISettingsReset();
-      }
-      //Deal with the leftmost icons
-      for (const uiIcon of $("#ui-settings-buttons").children()) {
-        // Copy the data for all the leftmost icons with their event handlers and show them in the side bar
-        if (!$("[id='ui-side-bar']").hasClass("stowed")) {
-          $("#ui-side-bar-backup-container1 > :nth-child(1)").append(
-            $(uiIcon).clone(true)
-          );
-        } else {
-          // Empty the container
-          $("#ui-side-bar-backup-container1 > :nth-child(1)").empty();
-        }
-      }
+    // Deal with the action interface's contents when the screen is even smaller
+    copyActionInterfaceContentsToSideBar();
+  }
+  //SECTION - For wide portrait and relatively narrower landscape modes on mobile
+  else if (window.matchMedia(wideMobileWidth).matches) {
+    // Reset if coming from another mobile screen range
+    if (prevMobileMaxWidth !== wideMobileWidth) {
+      generalMobileUISettingsReset();
+    }
 
-      //Deal with the rightmost icons (money/rep)
-      for (const uiIcon of $("#ui-stat-others").children()) {
-        // Copy the data for all the rightmost icons with their event handlers and show them in the side bar
-        if (!$("[id='ui-side-bar']").hasClass("stowed")) {
-          $("#ui-side-bar-backup-container1 > :nth-child(2)").append(
-            $(uiIcon).clone(true)
-          );
-        } else {
-          // Empty the container
-          $("#ui-side-bar-backup-container1 > :nth-child(2)").empty();
-        }
-      }
-
-      //Deal with the stat bars
-      // It will copy every stat bar individually and display it as a column in the side bar. To do that, I'll have to go through each stat bar column group and then get the stat bar. I think I could use `.find(".ui-stat-bar-and-icon") but eh
-      for (const statBarColumnGroup of $("#ui-stat-bars").children()) {
-        for (const statBar of $(statBarColumnGroup).children()) {
-          // Copy the each stat bar and paste into the side bar
-          if (!$("[id='ui-side-bar']").hasClass("stowed")) {
-            $("#ui-side-bar-backup-container2").append($(statBar).clone(true));
-          } else {
-            // Empty the container
-            $("#ui-side-bar-backup-container2").empty();
-          }
-        }
-      }
-
-      //Extra
+    for (const uiIcon of $("#ui-settings-buttons").children()) {
+      // Copy the data for all the leftmost icons with their event handlers and show them in the side bar
       if (!$("[id='ui-side-bar']").hasClass("stowed")) {
-        // Change the size of the bottom bar navigation settings
-        $("[id|='ui-navigation-option-button']").addClass(
-          "ui-navigation-button-small"
+        $("#ui-side-bar-backup-container1 > :nth-child(1)").append(
+          $(uiIcon).clone(true)
         );
       } else {
-        $("[id|='ui-navigation-option-button']").removeClass(
-          "ui-navigation-button-small"
-        );
-      }
-
-      // Deal with the action interface's contents when the screen is even smaller
-      copyActionInterfaceContentsToSideBar();
-    }
-    //SECTION - For wide portrait and relatively narrower landscape modes on mobile
-    else if (window.matchMedia(wideMobileWidth).matches) {
-      // Reset if coming from another mobile screen range
-      if (prevMobileMaxWidth !== wideMobileWidth) {
-        generalMobileUISettingsReset();
-      }
-
-      for (const uiIcon of $("#ui-settings-buttons").children()) {
-        // Copy the data for all the leftmost icons with their event handlers and show them in the side bar
-        if (!$("[id='ui-side-bar']").hasClass("stowed")) {
-          $("#ui-side-bar-backup-container1 > :nth-child(1)").append(
-            $(uiIcon).clone(true)
-          );
-        } else {
-          // Empty the container
-          $("#ui-side-bar-backup-container1 > :nth-child(1)").empty();
-        }
-      }
-
-      for (const uiIcon of $("#ui-stat-others").children()) {
-        // Copy the data for all the rightmost icons with their event handlers and show them in the side bar
-        if (!$("[id='ui-side-bar']").hasClass("stowed")) {
-          $("#ui-side-bar-backup-container1 > :nth-child(2)").append(
-            $(uiIcon).clone(true)
-          );
-        } else {
-          // Empty the container
-          $("#ui-side-bar-backup-container1 > :nth-child(2)").empty();
-        }
+        // Empty the container
+        $("#ui-side-bar-backup-container1 > :nth-child(1)").empty();
       }
     }
-    //SECTION - For much wider screens like laptops/desktops/i-pads, just reset it
-    else {
+
+    for (const uiIcon of $("#ui-stat-others").children()) {
+      // Copy the data for all the rightmost icons with their event handlers and show them in the side bar
       if (!$("[id='ui-side-bar']").hasClass("stowed")) {
-        // The side bar is open and the user is probably in landscape mode/is on something like an ipad so empty the backup containers and restore the stat bars since there's enough space for them
-        generalMobileUISettingsReset();
+        $("#ui-side-bar-backup-container1 > :nth-child(2)").append(
+          $(uiIcon).clone(true)
+        );
+      } else {
+        // Empty the container
+        $("#ui-side-bar-backup-container1 > :nth-child(2)").empty();
       }
     }
   }
-
-  // SECTION - Define a handler for interacting with the action interface
-  // NOTE - Every new item for the action interface needs some code here
-  export let ui_isActionInterfaceOpen = false;
-  export let ui_isMapInActionInterfaceOpen = false;
-  export const actionInterfaceToggleHandler = (
-    actionInterfaceChild: string
-  ) => {
-    // $("#ui-side-bar-action-interface").toggleClass("stowed");
-    // if ($("#ui-side-bar-action-interface").hasClass("stowed")) ui_isActionInterfaceOpen = false;
-    // else ui_isActionInterfaceOpen = true;
-    ui_isActionInterfaceOpen = !ui_isActionInterfaceOpen;
-    if (ui_isActionInterfaceOpen) {
-      $("#ui-side-bar-action-interface").removeClass("stowed");
-      // $(actionInterfaceChild).addClass("hidden");
-    } else {
-      $("#ui-side-bar-action-interface").addClass("stowed");
-      // $(actionInterfaceChild).removeClass("hidden");
+  //SECTION - For much wider screens like laptops/desktops/i-pads, just reset it
+  else {
+    if (!$("[id='ui-side-bar']").hasClass("stowed")) {
+      // The side bar is open and the user is probably in landscape mode/is on something like an ipad so empty the backup containers and restore the stat bars since there's enough space for them
+      generalMobileUISettingsReset();
     }
-
-    if (actionInterfaceChild == ".ui-side-bar-popout-map") {
-      ui_isMapInActionInterfaceOpen = !ui_isMapInActionInterfaceOpen;
-      if (ui_isMapInActionInterfaceOpen) {
-        $(actionInterfaceChild).removeClass("hidden");
-      } else {
-        $(actionInterfaceChild).addClass("hidden");
-      }
-    } else {
-      ui_isMapInActionInterfaceOpen = false;
-    }
-
-    copyActionInterfaceContentsToSideBar();
-  };
+  }
 }
+
+// SECTION - Define a handler for interacting with the action interface
+// NOTE - Every new item for the action interface needs some code here
+export let ui_isActionInterfaceOpen = false;
+export let ui_isMapInActionInterfaceOpen = false;
+export const actionInterfaceToggleHandler = (actionInterfaceChild: string) => {
+  // $("#ui-side-bar-action-interface").toggleClass("stowed");
+  // if ($("#ui-side-bar-action-interface").hasClass("stowed")) ui_isActionInterfaceOpen = false;
+  // else ui_isActionInterfaceOpen = true;
+  ui_isActionInterfaceOpen = !ui_isActionInterfaceOpen;
+  if (ui_isActionInterfaceOpen) {
+    $("#ui-side-bar-action-interface").removeClass("stowed");
+    // $(actionInterfaceChild).addClass("hidden");
+  } else {
+    $("#ui-side-bar-action-interface").addClass("stowed");
+    // $(actionInterfaceChild).removeClass("hidden");
+  }
+
+  if (actionInterfaceChild == ".ui-side-bar-popout-map") {
+    ui_isMapInActionInterfaceOpen = !ui_isMapInActionInterfaceOpen;
+    if (ui_isMapInActionInterfaceOpen) {
+      $(actionInterfaceChild).removeClass("hidden");
+    } else {
+      $(actionInterfaceChild).addClass("hidden");
+    }
+  } else {
+    ui_isMapInActionInterfaceOpen = false;
+  }
+
+  copyActionInterfaceContentsToSideBar();
+};
