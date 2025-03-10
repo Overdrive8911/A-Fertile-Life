89b48831f3b3d6f3301c54fb91b565e49839f622
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-10 19:24:11 +0100

    Refactor imports to specify Womb class path for clarity and consistency

--- src/assets/app/scripts/variable_initialization/player_variables.ts
+++ src/assets/app/scripts/variable_initialization/player_variables.ts
@@ -2,7 +2,7 @@ import { Breasts } from "../declarations/body_stats/breast_class_definitions";
 import type { Player } from "../declarations/player_declarations";
 import { Inventory } from "../inventory_and_items/classes/inventory";
 import { defaultWarpDestination } from "../location/other_data";
-import { Womb } from "../pregnancy/classes";
+import { Womb } from "../pregnancy/classes/womb";
 import {
   FertilityLevel,
   BellyState,
