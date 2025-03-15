01b7c10150ed6ccee0c19676da990f6e8924fe30
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-15 19:53:43 +0100

    Enhance Food class to support fullness effects and update healing logic

--- src/assets/app/scripts/inventory_and_items/classes/item_extends/food.ts
+++ src/assets/app/scripts/inventory_and_items/classes/item_extends/food.ts
@@ -31,12 +31,12 @@ export const enum FoodEffect {
   HEAL_HP_AND_MOOD_75 = HEAL_HP_75 | HEAL_MOOD_75,
   HEAL_HP_AND_MOOD_100 = HEAL_HP_100 | HEAL_MOOD_100,
 
-  // SECTION - Hunger Effects. If not explicitly provided, the food item will heal hunger by 10%
-  HEAL_HUNGER_10 = 1 << 8,
-  HEAL_HUNGER_25 = 1 << 9,
-  HEAL_HUNGER_50 = 1 << 10,
-  HEAL_HUNGER_75 = 1 << 11,
-  HEAL_HUNGER_100 = HEAL_HUNGER_25 + HEAL_HUNGER_75,
+  // SECTION - Hunger Effects. If not explicitly provided, the food item will heal fullness by 10%
+  HEAL_FULLNESS_10 = 1 << 8,
+  HEAL_FULLNESS_25 = 1 << 9,
+  HEAL_FULLNESS_50 = 1 << 10,
+  HEAL_FULLNESS_75 = 1 << 11,
+  HEAL_FULLNESS_100 = HEAL_FULLNESS_25 + HEAL_FULLNESS_75,
   // !SECTION
 
   // SECTION - EXP Effects. These add a percentage of exp relative to the total amount of exp needed to advance to the next level. If not specific, defaults to 1% exp
@@ -48,7 +48,7 @@ export const enum FoodEffect {
   ADD_EXP_75 = 1 << 17,
   ADD_EXP_100 = ADD_EXP_25 + ADD_EXP_75,
 }
-export // REVIEW - Types of food that reduce hunger and may give certain buffs or nerf?
+export // REVIEW - Types of food that reduce fullness and may give certain buffs or nerf?
 class Food extends Item {
   /**
    * The time in seconds that should pass before the food item expires
@@ -58,6 +58,51 @@ class Food extends Item {
   constructor(data?: ItemConstructorArgs<Food>) {
     super(data);
     this.addTags(ItemTag.FOOD);
+  }
+
+  applyEffect(effect: FoodEffect, user: Player, shouldInvert?: boolean): void {
+    let hpChange = 0,
+      moodChange = 0,
+      fullnessChange = 0,
+      expChange = 0;
+
+    if (effect & FoodEffect.HEAL_HP_10) hpChange += 10;
+    if (effect & FoodEffect.HEAL_HP_25) hpChange += 25;
+    if (effect & FoodEffect.HEAL_HP_50) hpChange += 50;
+    if (effect & FoodEffect.HEAL_HP_75) hpChange += 75;
+    if (effect & FoodEffect.HEAL_HP_100) hpChange += 100;
+
+    if (effect & FoodEffect.HEAL_MOOD_10) moodChange += 10;
+    if (effect & FoodEffect.HEAL_MOOD_25) moodChange += 25;
+    if (effect & FoodEffect.HEAL_MOOD_50) moodChange += 50;
+    if (effect & FoodEffect.HEAL_MOOD_75) moodChange += 75;
+    if (effect & FoodEffect.HEAL_MOOD_100) moodChange += 100;
+
+    if (effect & FoodEffect.HEAL_FULLNESS_10) fullnessChange += 10;
+    if (effect & FoodEffect.HEAL_FULLNESS_25) fullnessChange += 25;
+    if (effect & FoodEffect.HEAL_FULLNESS_50) fullnessChange += 50;
+    if (effect & FoodEffect.HEAL_FULLNESS_75) fullnessChange += 75;
+    if (effect & FoodEffect.HEAL_FULLNESS_100) fullnessChange += 100;
+
+    if (effect & FoodEffect.ADD_EXP_1) expChange += 1;
+    if (effect & FoodEffect.ADD_EXP_5) expChange += 5;
+    if (effect & FoodEffect.ADD_EXP_10) expChange += 10;
+    if (effect & FoodEffect.ADD_EXP_25) expChange += 25;
+    if (effect & FoodEffect.ADD_EXP_50) expChange += 50;
+    if (effect & FoodEffect.ADD_EXP_75) expChange += 75;
+    if (effect & FoodEffect.ADD_EXP_100) expChange += 100;
+
+    if (shouldInvert) {
+      hpChange *= -1;
+      moodChange *= -1;
+      fullnessChange *= -1;
+      expChange *= -1;
+    }
+
+    user.hp += user.maxHp * hpChange;
+    user.womb.addHp((user.womb.maxHp * hpChange) / 2);
+    user.mentalStats.mood += 100 * moodChange;
+    fullnessChange ? (user.fullness += fullnessChange) : (user.fullness += 10);
   }
 }
 
