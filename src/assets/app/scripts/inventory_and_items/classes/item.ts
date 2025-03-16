825e3cbd0ccf96324cd7195003b3873cc462c8d3
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-16 18:08:22 +0100

    Add usability feature to inventory items and update dialog display

--- src/assets/app/scripts/inventory_and_items/classes/item.ts
+++ src/assets/app/scripts/inventory_and_items/classes/item.ts
@@ -38,6 +38,13 @@ export class Item<ItemEffect extends number = number> {
     | ((user: Player) => void)
   )[];
 
+  /**
+   * If the item can be used. If not, it's just a collectible
+   *
+   * //NOTE: **EVERY ITEM IS USABLE UNLESS EXPLICITLY SET OTHERWISE**
+   */
+  usable = true;
+
   // A handler function called when the item is used. Unusable items don't need this. Return data (and parameters) will be an array/iterable/single primitive value and will likely be of the same structure (since the stored data in an inventory item(if any) may be used as arguments). See the getter `callback()`
   customCallBack?: ItemCallback; // NOTE: Add this when initializing a new item and a special "default callback" is required.
   /**
