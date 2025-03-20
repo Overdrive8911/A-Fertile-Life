455e09a39350295d20b8b1b8dc4f6c80e56dbf89
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-20 21:21:38 +0100

    Added width

--- src/assets/app/scripts/ui/widgets/types.ts
+++ src/assets/app/scripts/ui/widgets/types.ts
@@ -1,8 +1,9 @@
 export type Widgets = {
   /**
-   * Can be used as a stat bar or whatever.
+   * Can be used as a stat bar or whatever
    *
    * @param value - A decimal between 0 and 1 inclusively, where 0 is 0% and 1 is 100%.
+   * @param width - Defaults to 100% of the parent container
    * @param lowColor - A valid css color representing the `low` percentage. Can be skipped with `""`. Defaults to `red`
    * @param midColor - A valid css color representing the `middle` percentage. Can be skipped with `""`. Defaults to `yellow`
    * @param highColor - A valid css color representing the `high` percentage. Defaults to `green`
@@ -11,6 +12,7 @@ export type Widgets = {
    */
   meter: (
     value: number,
+    width?: string,
     lowColor?: string,
     midColor?: string,
     highColor?: string,
