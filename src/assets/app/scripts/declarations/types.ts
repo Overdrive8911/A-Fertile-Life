13246a14a92dd096b745149cd12878926a56fec4
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-21 18:50:49 +0100

    Add sugarcube variable type

--- src/assets/app/scripts/declarations/types.ts
+++ src/assets/app/scripts/declarations/types.ts
@@ -5,3 +5,5 @@ export type NonFunctionKeys<T> = {
 export type NumberKeys<T> = {
   [K in keyof T]: T[K] extends number ? K : never;
 }[keyof T];
+
+export type SugarcubeVariable = `$${string}` | `_${string}`;
