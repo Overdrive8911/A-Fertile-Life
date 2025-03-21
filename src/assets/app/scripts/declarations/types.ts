f56a9b075f89350200ea36d0a4abc376487ed440
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-21 21:37:04 +0100

    Refactor getSugarCubeVariableValue to use eval for dynamic variable retrieval and update SugarcubeVariable type documentation

--- src/assets/app/scripts/declarations/types.ts
+++ src/assets/app/scripts/declarations/types.ts
@@ -6,4 +6,7 @@ export type NumberKeys<T> = {
   [K in keyof T]: T[K] extends number ? K : never;
 }[keyof T];
 
+/**
+ * Stuff like `$var`, `$var.foo`, `_var.foo[bar]`
+ */
 export type SugarcubeVariable = `$${string}` | `_${string}`;
