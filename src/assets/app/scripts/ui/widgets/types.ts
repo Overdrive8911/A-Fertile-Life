62c34ca31b868529b36eeb4bdb8d77b26092bf95
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-22 12:35:19 +0100

    Enhance meter widget styling by adding a class for better customization and update documentation to reflect new styling options.

--- src/assets/app/scripts/ui/widgets/types.ts
+++ src/assets/app/scripts/ui/widgets/types.ts
@@ -2,7 +2,7 @@ import type { SugarcubeVariable } from "../../declarations/types";
 
 export type Widgets = {
   /**
-   * Can be used as a stat bar or whatever
+   * Can be used as a stat bar or whatever. It can also be styled using the class `.meter-body`
    *
    * @param value - Either a decimal between 0 and 1 inclusively, where 0 is 0% and 1 is 100% or a string representing a valid sugarcube variable name (with the $ or _) that stores the aforementioned decimal (otherwise it defaults to 1). If a variable string is passed, the meter auto-updates when the variable changes.
    * @param parentElement - The parent element to append the meter to
