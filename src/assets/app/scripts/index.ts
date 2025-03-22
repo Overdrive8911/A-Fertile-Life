4722d0c120e433f8ab02e00583a161ece2811e82
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-22 15:27:15 +0100

    Remove old chapel meter macro stuff

--- src/assets/app/scripts/index.ts
+++ src/assets/app/scripts/index.ts
@@ -7,7 +7,6 @@ import "./ui/index";
 
 import "./extensions/check_vars/checkvars-macro.min";
 import "./extensions/click_to_proceed/click-to-proceed";
-import "./extensions/meter_macro/meter-macro.min";
 import "./extensions/no_space/nosp_macro";
 import "./extensions/number_pool/numberpool_macro_set";
 import "./extensions/update_markup/update-markup-min";
