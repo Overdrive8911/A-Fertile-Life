757fe7b815b74751aaad19533bcff98dbba7bd8c
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-25 20:38:09 +0100

    Refactor game date and time handling to use GameDateAndTime class for improved type safety and functionality

--- src/assets/app/scripts/date_and_time/date_and_time_declarations.ts
+++ src/assets/app/scripts/date_and_time/date_and_time_declarations.ts
@@ -1,15 +1,17 @@
 import { dispatchCustomEvent } from "../declarations/custom_events";
 import { CustomEventName } from "../declarations/enums";
+import { GameDateAndTime } from "./classes";
 
 export function updateGameTimeVariable(timeInSecondsOrNewDate: number | Date) {
   // copy out the date
-  const oldDate = new Date(variables().gameDateAndTime);
+  const oldDate = new GameDateAndTime(variables().gameDateAndTime);
 
   // Convert to milliseconds and add to the old date
-  variables().gameDateAndTime =
+  variables().gameDateAndTime.update(
     timeInSecondsOrNewDate instanceof Date
       ? timeInSecondsOrNewDate
-      : new Date(oldDate.getTime() + 1000 * timeInSecondsOrNewDate);
+      : new Date(oldDate.getTime() + 1000 * timeInSecondsOrNewDate)
+  );
 
   // Dispatch an event for other stuff that rely on time to work
   dispatchCustomEvent(CustomEventName.TIME_UPDATE, {
