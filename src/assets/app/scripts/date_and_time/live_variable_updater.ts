7c2c38d95bd8b9485af030b3300214ad18a6fcf5
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-25 14:09:07 +0100

    Refactor game date and time retrieval to use a single variable for improved readability

--- src/assets/app/scripts/date_and_time/live_variable_updater.ts
+++ src/assets/app/scripts/date_and_time/live_variable_updater.ts
@@ -22,26 +22,24 @@ const updateGameTimeDisplay = () => {
     "DEC",
   ];
 
+  const gameDateAndTime = variables().gameDateAndTime;
   // Get the day (e.g Monday)
-  const gameDateAndTime_Day = days[variables().gameDateAndTime.getUTCDay()];
+  const gameDateAndTime_Day = days[gameDateAndTime.getUTCDay()];
 
   // Get the date (e.g 1st)
-  const gameDateAndTime_Date = variables().gameDateAndTime.getUTCDate();
+  const gameDateAndTime_Date = gameDateAndTime.getUTCDate();
 
   // Get the month
-  const gameDateAndTime_Month =
-    months[variables().gameDateAndTime.getUTCMonth()];
+  const gameDateAndTime_Month = months[gameDateAndTime.getUTCMonth()];
 
   // Put the string in the variable
   variables().gameDateDisplay = `${gameDateAndTime_Day}, ${gameDateAndTime_Date} ${gameDateAndTime_Month}`;
 
   // Update game Time variable
   // Get the time in hours for the day
-  const gameDateAndTime_DayHours: number =
-    variables().gameDateAndTime.getUTCHours();
+  const gameDateAndTime_DayHours: number = gameDateAndTime.getUTCHours();
   // Get the time in minutes for the hour
-  const gameDateAndTime_DayMinutes: number =
-    variables().gameDateAndTime.getUTCMinutes();
+  const gameDateAndTime_DayMinutes: number = gameDateAndTime.getUTCMinutes();
 
   if (gameDateAndTime_DayHours < 12) {
     // AM
