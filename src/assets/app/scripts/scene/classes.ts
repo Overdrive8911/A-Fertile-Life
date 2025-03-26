461eb48e23a87aadd6e301994cadbecc7c8b6bb5
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-26 13:36:15 +0100

    De-privatized `scenes`

--- src/assets/app/scripts/scene/classes.ts
+++ src/assets/app/scripts/scene/classes.ts
@@ -14,7 +14,7 @@ export class SceneData {
   /**
    * This will always have at least **one** element
    */
-  #scenes: Scene[] = [];
+  scenes: Scene[] = [];
 
   constructor(passageName: string, area?: AreaUUID);
   constructor(classOrClassLikeObject: SceneData);
@@ -47,13 +47,13 @@ export class SceneData {
    * Retrieves the first scene in the scenes array, representing the initial scene.
    */
   get initialScene() {
-    return this.#scenes[0];
+    return this.scenes[0];
   }
   /**
    * Retrieves the scene that is currently active or in progress.
    */
   get currentScene() {
-    return this.#scenes[this.#scenes.length - 1];
+    return this.scenes[this.scenes.length - 1];
   }
   /**
    * Retrieves the second-to-last scene in the scenes array, if it exists.
@@ -61,20 +61,18 @@ export class SceneData {
    * Returns `null` if there is only one or no scene in the array.
    */
   get lastScene() {
-    return this.#scenes.length > 1
-      ? this.#scenes[this.#scenes.length - 2]
-      : null;
+    return this.scenes.length > 1 ? this.scenes[this.scenes.length - 2] : null;
   }
 
   /**
    * Gets the scene (if any) that has the same passage name with the given argument
    */
   scene(passageName: string) {
-    return this.#scenes.find((scene) => scene.passage == passageName);
+    return this.scenes.find((scene) => scene.passage == passageName);
   }
 
   addScene(passageName: string, area?: AreaUUID) {
-    this.#scenes.push({ passage: passageName, area: area ?? player().areaId });
+    this.scenes.push({ passage: passageName, area: area ?? player().areaId });
   }
 }
 
