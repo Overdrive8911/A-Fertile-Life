ea2e01770e190369f48d5b22a53ccf7548194be4
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-26 11:37:57 +0100

    Add scene area retrieval to getAreaUUID function

--- src/assets/app/scripts/location/functions.ts
+++ src/assets/app/scripts/location/functions.ts
@@ -1,9 +1,12 @@
+import { SceneEnum } from "../scene/enums";
 import { globalMap } from "./game_locations/global_map";
 import type { AreaUUID } from "./types_and_interfaces";
 
 export function getAreaUUID(uuidOrPassageName: AreaUUID | string) {
   return (
     globalMap.uuidFromPassage(uuidOrPassageName) ??
+    variables()[SceneEnum.STORY_VARIABLE_NAME]?.scene(uuidOrPassageName)
+      ?.area ??
     (uuidOrPassageName as AreaUUID)
   );
 }
