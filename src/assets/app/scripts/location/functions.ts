60a6d87ceed886c49997eb9fa1253cd2533ca3c0
Overdrive8911 <wuchidiscord@gmail.com>
2025-03-28 09:36:30 +0100

    Add validation for AreaUUID in getAreaUUID function

--- src/assets/app/scripts/location/functions.ts
+++ src/assets/app/scripts/location/functions.ts
@@ -3,16 +3,19 @@ import { SceneEnum } from "../scene/enums";
 import { globalMap } from "./game_locations/global_map";
 import type { AreaUUID } from "./types_and_interfaces";
 
+function isValidAreaUUID(possibleUUID: string): boolean {
+  return typeof getAreaFromUUID(possibleUUID) != "undefined" ? true : false;
+}
 /**
  * This doesn't consider if a passage that isn't assigned to an area / not already passed in a scene is given, but that's a user error that'd pretty much only happen in twinescript
  */
 export function getAreaUUID(uuidOrPassageName: AreaUUID | string) {
-  return (
-    globalMap.uuidFromPassage(uuidOrPassageName) ??
+  return globalMap.uuidFromPassage(uuidOrPassageName) ??
     variables()[SceneEnum.STORY_VARIABLE_NAME]?.scene(uuidOrPassageName)
       ?.area ??
-    (uuidOrPassageName as AreaUUID)
-  );
+    isValidAreaUUID(uuidOrPassageName)
+    ? (uuidOrPassageName as AreaUUID)
+    : globalMap.uuid;
 }
 
 export function getAreaFromUUID(uuidOrPassageName: AreaUUID | string) {
