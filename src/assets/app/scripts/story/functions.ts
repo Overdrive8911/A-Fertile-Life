77d68476a93b8ca0dc6182004f33e1bb8b44944b
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-02 21:23:26 +0100

    Enhance passage management by updating addPassage function to accept optional tags and improve type definitions

--- src/assets/app/scripts/story/functions.ts
+++ src/assets/app/scripts/story/functions.ts
@@ -8,7 +8,29 @@ setup.initPassages = () => {
   });
 };
 
+type PassageDescriptorWithOptionalTags = {
+  /**
+   * The tags of the passage.
+   * @since 2.0.0
+   */
+  tags?: string[];
+
+  /**
+   * The name of the passage.
+   * @since 2.37.0
+   */
+  name: string;
+  /**
+   * The raw text of the passage.
+   * @since 2.0.0
+   */
+  text: string;
+};
 /** Use this for any passages you want to add*/
-export function addPassage(passageData: PassageBase) {
-  passagesToAdd.push(passageData);
+export function addPassage(passageData: PassageDescriptorWithOptionalTags) {
+  passagesToAdd.push({
+    name: passageData.name,
+    tags: passageData.tags ?? [""],
+    text: passageData.text,
+  });
 }
--- src/assets/app/scripts/story/passages/index.ts
+++ src/assets/app/scripts/story/passages/index.ts
@@ -1,1 +1,2 @@
 import "./sandbox";
+import "./ui/index";
