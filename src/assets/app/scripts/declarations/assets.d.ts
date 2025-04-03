1314805410daa81cd399428ac0c99b6ae3bf4db0
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-03 21:49:19 +0100

    Update css and scss declarations

--- src/assets/app/scripts/declarations/assets.d.ts
+++ src/assets/app/scripts/declarations/assets.d.ts
@@ -1,3 +1,15 @@
+declare module "*.module.css" {
+  const content: Record<string, string>;
+  export default content;
+}
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+declare module "*.scss" {
+  const content: string;
+  export default content;
+}
 declare module "*.webp" {
   const content: string;
   export default content;
