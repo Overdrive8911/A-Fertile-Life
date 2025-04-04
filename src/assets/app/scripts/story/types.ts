22be24461214358ce3c46d8dc8b7173f60690cc9
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-04 12:27:43 +0100

    Typo fix

--- src/assets/app/scripts/story/types.ts
+++ src/assets/app/scripts/story/types.ts
@@ -19,7 +19,7 @@ export type PassageDescriptorWithOptionalTags = {
 
 export type GenericHtmlElementAttributes = Partial<{
   id: string;
-  classes: string[];
+  class: string[];
   [key: `data-${string}`]: string;
 }>;
 
