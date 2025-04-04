7b33b6f5a68e3417532e580707668f0d537a7133
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-04 18:11:53 +0100

    Add icon styles with responsive dimensions and adjust stat bar positioning

--- src/assets/app/scripts/story/passages/styles/ui/shared.module.css.d.ts
+++ src/assets/app/scripts/story/passages/styles/ui/shared.module.css.d.ts
@@ -1,4 +1,9 @@
 declare const styles: {
+  readonly "icon16X16": string;
+  readonly "icon20X20": string;
+  readonly "icon24X24": string;
+  readonly "icon32X32": string;
+  readonly "iconBase": string;
   readonly "rightmostIconContainer": string;
 };
 export = styles;
