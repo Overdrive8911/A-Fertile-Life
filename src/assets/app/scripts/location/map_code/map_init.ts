c15dfe054e3d8ff41883e7a0735fbbbb1f478c32
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-05 21:54:01 +0100

    Disable unneeded canvas

--- src/assets/app/scripts/location/map_code/map_init.ts
+++ src/assets/app/scripts/location/map_code/map_init.ts
@@ -3,17 +3,15 @@ import { convertToClass } from "../../declarations/general_declarations";
 import { popoutMap } from "../../story/passages/styles/ui/side_section.module.css";
 
 $(window).on(":passageend", async () => {
-  const popoutMapClass = convertToClass(popoutMap);
-  const container = document.querySelector(popoutMapClass);
-  console.log(container);
-  if (container) {
-    const app = new Application();
-
-    await app.init({ background: "#1099bb" });
-
-    // Then adding the application's canvas to the DOM body.
-    container.prepend(app.canvas);
-  } else {
-    console.error(`No element with the class "${popoutMapClass}" was found.`);
-  }
+  // const popoutMapClass = convertToClass(popoutMap);
+  // const container = document.querySelector(popoutMapClass);
+  // console.log(container);
+  // if (container) {
+  //   const app = new Application();
+  //   await app.init({ background: "#1099bb" });
+  //   // Then adding the application's canvas to the DOM body.
+  //   container.prepend(app.canvas);
+  // } else {
+  //   console.error(`No element with the class "${popoutMapClass}" was found.`);
+  // }
 });
