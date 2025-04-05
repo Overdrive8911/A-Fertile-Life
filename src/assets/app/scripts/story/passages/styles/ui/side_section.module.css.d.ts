299c0081ed7e6d124cc471f0371e2853fa687d98
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-05 18:05:53 +0100

    Update d.ts

--- src/assets/app/scripts/story/passages/styles/ui/side_section.module.css.d.ts
+++ src/assets/app/scripts/story/passages/styles/ui/side_section.module.css.d.ts
@@ -1,1 +1,19 @@
-export {};
+declare const styles: {
+  readonly "actionInterface": string;
+  readonly "actionMenu": string;
+  readonly "assistantDevice": string;
+  readonly "backupContainer1": string;
+  readonly "backupContainer2": string;
+  readonly "gameInfoAuthor": string;
+  readonly "gameInfoBox": string;
+  readonly "gameInfoName": string;
+  readonly "gameInfoOthers": string;
+  readonly "playerImage": string;
+  readonly "popoutMap": string;
+  readonly "popoutMapBtnBar": string;
+  readonly "sideBar": string;
+  readonly "stowed": string;
+  readonly "toggleStateBtn": string;
+};
+export = styles;
+
