f1c04028748184ccdad21f522596175e3c17e00e
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-06 20:15:34 +0100

    Added Markup types

--- src/assets/app/scripts/story/types.ts
+++ src/assets/app/scripts/story/types.ts
@@ -1,3 +1,34 @@
+import type { SugarcubeVariable } from "../declarations/types";
+import type { StoryPassageName } from "./enums";
+
+type PassageNameOrPassageNameInStoryVariable =
+  | StoryPassageName
+  | SugarcubeVariable;
+
+/**
+ * `[[Link]]` -> `[[Grocery]]` or `[[$go]]`
+ *
+ * `[[Text|Link]]` -> `[[Go buy milk|Grocery]]` or `[[$show|$go]]`
+ */
+export type LinkMarkup =
+  | `[[${PassageNameOrPassageNameInStoryVariable}]]`
+  | `[[${string}|${PassageNameOrPassageNameInStoryVariable}]]`;
+
+/**
+ * `[img[Image]]` -> `[img[home.png]]` or `[img[$src]]`
+ *
+ * `[img[Image][Link]]` -> `[img[home.png][Home]]` or `[img[$src][$go]]`
+ *
+ * `[img[Title|Image]]` -> `[img[Go home|home.png]]` or `[img[$show|$src]]`
+ *
+ * `[img[Title|Image][Link]]` -> `[img[Go home|home.png][Home]]` or `[img[$show|$src][$go]]`
+ */
+export type ImageMarkup =
+  | `[img[${string}]]`
+  | `[img[${string}][${PassageNameOrPassageNameInStoryVariable}]]`
+  | `[img[${string}|${string}]]`
+  | `[img[${string}|${PassageNameOrPassageNameInStoryVariable}]]`;
+
 export type PassageDescriptorWithOptionalTags = {
   /**
    * The tags of the passage.
