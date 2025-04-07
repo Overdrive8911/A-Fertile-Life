886ac39ea402689954cb9a897b8b51bdc2a9709d
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-07 22:02:46 +0100

    Use enum over raw strings

--- src/assets/app/scripts/story/passages/sandbox.ts
+++ src/assets/app/scripts/story/passages/sandbox.ts
@@ -1,4 +1,5 @@
 import { ItemId } from "../../inventory_and_items/declarations/item_enums";
+import { StoryPassageName } from "../enums";
 import {
   addPassage,
   br,
@@ -66,9 +67,8 @@ addPassage({
     p(macroLink("Rewind 1 day", `<<skipTime -1 0 0>>`)) +
     p(macroLink("Skip 1 week", `<<skipTime 7 0 0>>`)) +
     p(macroLink("Rewind 1 week", `<<skipTime -7 0 0>>`)) +
-    //@ts-expect-error I'll have to fix the types later :p
-    p(macroLink("[[Reload Passage|DebugSandbox]]")) +
+    p(macroLink(`[[Reload Passage|${StoryPassageName.DEBUG_SANDBOX}]]`)) +
     p(macroLink("Test Prologue Scene", `<<startScene "Prologue_Beginning">>`)),
-  name: "DebugSandbox",
+  name: StoryPassageName.DEBUG_SANDBOX,
   tags: [""],
 });
