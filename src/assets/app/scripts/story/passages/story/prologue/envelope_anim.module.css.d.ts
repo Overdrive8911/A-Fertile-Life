7292455f6559bc60474b599ce509b9a83839b83a
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-09 10:21:47 +0100

    Add envelope anim

---
+++ src/assets/app/scripts/story/passages/story/prologue/envelope_anim.module.css.d.ts
@@ -0,0 +1,8 @@
+declare const styles: {
+  readonly "anim": string;
+  readonly "basicEnvelopeAnimation": string;
+  readonly "envelope": string;
+  readonly "playAnim": string;
+};
+export = styles;
+
