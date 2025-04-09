34c68a8a92bf43c56e4f5cb2a096cb0fa862e03b
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-09 18:27:35 +0100

    The starting passage has to be in twee :p

--- src/assets/app/scripts/story/enums.ts
+++ src/assets/app/scripts/story/enums.ts
@@ -1,5 +1,6 @@
 // TODO: Put all the story passages here
 export const enum StoryPassageName {
+  GAME_START = "GameStart",
   DEBUG_SANDBOX = "DebugSandbox",
   PLAYER_HOUSE = "Your House",
   PROLOGUE_BEGINNING = "Prologue_Beginning",
