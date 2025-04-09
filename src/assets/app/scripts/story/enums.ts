d31f9fb7f82ea48993a6a12a73b7a25eb68c4167
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-09 16:10:38 +0100

    Work done

--- src/assets/app/scripts/story/enums.ts
+++ src/assets/app/scripts/story/enums.ts
@@ -3,4 +3,7 @@ export const enum StoryPassageName {
   DEBUG_SANDBOX = "DebugSandbox",
   PLAYER_HOUSE = "Your House",
   PROLOGUE_BEGINNING = "Prologue_Beginning",
+  PROLOGUE_WAKE_UP_AND_BRUSH_TEETH = "Prologue_WakeUpAndBrushTeeth",
+  PROLOGUE_DRESSED_AND_ENTERING_BUS = "Prologue_DressedAndEnteringBus",
+  PLAYER_BATHROOM = "Player_Bathroom",
 }
--- src/assets/app/scripts/story/passages/story/prologue/1_home_event.ts
+++ src/assets/app/scripts/story/passages/story/prologue/1_home_event.ts
@@ -1,7 +1,18 @@
 import { StoryPassageName } from "../../../enums";
-import { em, p, span } from "../../../functions/html_elements";
+import { ctpNoId } from "../../../functions/external_libs/ctp";
+import { br, div, em, p, span, strong } from "../../../functions/html_elements";
+import {
+  macroAddClass,
+  macroImage,
+  macroLink,
+  macroScript,
+  macroTimed,
+} from "../../../functions/macros";
 import { addPassage } from "../../../functions/passage_funcs";
-import { femaleSpeech } from "../../styles/speech.module.css";
+import { pixelArt } from "../../styles/img.module.css";
+import { femaleSpeech, otherSpeech } from "../../styles/speech.module.css";
+import closedEnvelope from "./../../../../../../media/img/general/envelope_closed.webp";
+import { envelope, letterPaper, playAnim } from "./envelope_anim.module.css";
 
 addPassage({
   name: StoryPassageName.PROLOGUE_BEGINNING,
@@ -24,5 +35,107 @@ addPassage({
     ) +
     p(
       "You rise to a sitting position and bring the envelope to your face. It's whitish and looks rather plain. Nothing about it feels suspicious, so you open it and pull out a folded paper."
+    ) +
+    p(
+      "Before you can unfold it, a wave of discomfort hits your midsection hard. You clutch your abdomen and breathe in deeply; this isn't the first time. Lately, you've been feeling some pains right above your pelvis." +
+        span({ class: femaleSpeech }, `"Ugh, this is the worst one yet,"`) +
+        " you grumble. Period pain. You chalked it up to period pain and so bought some medication last night, but it doesn't feel too helpful now."
+    ) +
+    p(
+      "The discomfort eventually subsides in a few minutes; luckily, this happens seldom. Taking in another deep breath, you focus on the envelope and pull out the letter."
+    ) +
+    br +
+    ctpNoId(
+      {
+        content: "",
+        progress(progressText) {
+          // Display an envelope image. The player should click it to open it and then the letter fades in
+          return (
+            macroLink(
+              span({ class: [pixelArt, envelope] }, macroImage(closedEnvelope)),
+              progressText + macroAddClass(envelope, playAnim)
+            ) +
+            br +
+            br
+          );
+        },
+      },
+      {
+        options: { t8n: true },
+        content:
+          br +
+          // Scroll downwards slightly to show the user part of the letter
+          macroTimed([
+            {
+              delay: "0.5s",
+              content: macroScript(
+                "$('html').scrollTop($('html').scrollTop() + 50);"
+              ),
+            },
+          ]) +
+          // TODO - Add the background of a folded letter to this div
+          div(
+            { class: [otherSpeech, pixelArt, letterPaper] },
+            p(
+              `Good day, Miss, I would like to first apologize for all that is happening to you now. Ever since the ${em(
+                "incident"
+              )}, your life has been unexpectedly—well, expectedly—more inconvenient to put it lightly.`
+            ) +
+              p(
+                "Your job security is compromised at this point, but I'm sure you already know this. You probably are scared and worried now; getting another job is not going to be easy, especially since your papers are not in order."
+              ) +
+              p(
+                "By now, you should also have noticed some strange things about your body. They are light symptoms from your exposure and will likely intensify with the passing days. The most obvious of these will be an unusual feeling around your pelvic region."
+              ) +
+              p(
+                "You also need to know that if you have seen some suspicious people dressed in similar attire around places you frequent, your home is not as safe as you think."
+              ) +
+              p(
+                `Now, do not fret yet. I have better news that could resolve most, if not all, your problems. I own a hospital about a hundred or so miles up north from your house, Fertilo Inc, might ring a few bells. To be frank, it's the only place you can have your condition treated without resorting to the government and you ${strong(
+                  "do not"
+                )} want to be their lab rat, trust me. I can also provide you with living accommodations and a well-paying job, as well as a new alias you can go by, during your treatment. `
+              ) +
+              p(
+                "You might be wondering why I sent you this letter or whether this is a scam. For the former, I also benefit here. Your medical condition is a one-in-a-million modern marvel, and the second I have seen in my entire lifetime. This will be the perfect chance for me to study it extensively; I believe that there is something special about it that will be of great benefit to humanity." +
+                  br +
+                  "For the latter, you do not have much of a choice, honestly. None of your living family members live in the country; and I doubt you'd be willing to go back. Your house will probably be jumped by 'agents' next week too."
+              ) +
+              p(
+                "I am not trying to threaten you; just letting you know your options. If you're interested about how I got this information, feel free to come, but please think about your situation properly. I'll be awaiting your response."
+              ) +
+              p(
+                `${strong(
+                  "PS:"
+                )} The directions are on the inner side of the envelope."`
+              )
+          ) +
+          br +
+          p(
+            "The paper falls from your hand in shock. " +
+              span(
+                { class: femaleSpeech },
+                em(
+                  "What the FUCK?! How did this guy know this? How could anyone know this much about me? Are they spying on me now?"
+                )
+              ) +
+              ` you think.
+              Your eyes scan the surrounding room. There aren't any visible cameras or anything similar in sight, but its not like you expected to see them in plain sight.`
+          ) +
+          p(
+            em(
+              { class: femaleSpeech },
+              "…I hate to admit it, but they're right, I don't really have much of a choice now. I honestly didn't have a solid plan before… If I go there tomorrow, even if it's a scam, I'll have a shot at finding out how they got to know this much… God, I hope I'll be able to make sense of this mess."
+            ) +
+              // <!--You've been feeling a dull but constant ache just above your pelvic region for a while now, alongside headaches and a fever. Not to talk of the semi-recent weird feeling of emptiness and occasional 'heat' that's uncomfortable to deal with.-->
+              "You understandably still have your doubts, and hope none of this is true—especially the symptoms part—but that's diminishing by the minute. "
+          ) +
+          p(
+            "You make a mental note to go to the hospital tomorrow." +
+              em({ class: femaleSpeech }, "Better than nothing, I guess.") +
+              `For now, you'll try to get some ${macroLink(
+                `[[sleep|${StoryPassageName.PROLOGUE_WAKE_UP_AND_BRUSH_TEETH}]]`
+              )}`
+          ),
+      }
     ),
 });
