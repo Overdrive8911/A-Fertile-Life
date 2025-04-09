a7c73e57f9a692c969ca9dd12408881c708bdef3
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-09 14:44:47 +0100

    moved `.letter_paper` into a css module

--- src/assets/app/scripts/story/passages/story/prologue/envelope_anim.module.css.d.ts
+++ src/assets/app/scripts/story/passages/story/prologue/envelope_anim.module.css.d.ts
@@ -2,6 +2,7 @@ declare const styles: {
   readonly "anim": string;
   readonly "basicEnvelopeAnimation": string;
   readonly "envelope": string;
+  readonly "letterPaper": string;
   readonly "playAnim": string;
 };
 export = styles;
