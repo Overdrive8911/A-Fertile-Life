894ced82c226a012ffd9fcdd7366c3e29cf57d00
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-10 19:34:48 +0100

    Formatting

--- src/assets/app/scripts/story/functions/others.ts
+++ src/assets/app/scripts/story/functions/others.ts
@@ -1,57 +1,57 @@
 import type { SugarCubeSetupObject } from "twine-sugarcube";
 import type {
-  SugarCubeStoryVariables,
-  SugarCubeTemporaryVariables,
+	SugarCubeStoryVariables,
+	SugarCubeTemporaryVariables,
 } from "twine-sugarcube/userdata";
 
 // SECTION - Variable-specific functions
 // Constructs all valid paths into a nested object T
 type Path<T> = T extends object
-  ? {
-      [K in keyof T]: [K] | [K, ...Path<T[K]>];
-    }[keyof T]
-  : [];
+	? {
+			[K in keyof T]: [K] | [K, ...Path<T[K]>];
+	  }[keyof T]
+	: [];
 function getStoryVar<T, P extends Path<T>>(obj: T, ...path: P) {
-  const identifier =
-    obj == variables() ? "$" : obj == temporary() ? "_" : "setup.";
+	const identifier =
+		obj == variables() ? "$" : obj == temporary() ? "_" : "setup.";
 
-  return `${identifier}${path.join(".")}` as const;
+	return `${identifier}${path.join(".")}` as const;
 }
 export function stateFulVar<P extends Path<SugarCubeStoryVariables>>(
-  //@ts-ignore
-  ...args: P
+	//@ts-ignore
+	...args: P
 ) {
-  //@ts-ignore
-  return getStoryVar(variables(), ...args) as `$${string}`;
+	//@ts-ignore
+	return getStoryVar(variables(), ...args) as `$${string}`;
 }
 export function tempVar<P extends Path<SugarCubeTemporaryVariables>>(
-  ...args: P
+	...args: P
 ) {
-  return getStoryVar(temporary(), ...args) as `_${string}`;
+	return getStoryVar(temporary(), ...args) as `_${string}`;
 }
 /** Utility func for counters in passages */
 export const tempCounterVar = tempVar("counter") as "_counter";
 export function staticVar<P extends Path<SugarCubeSetupObject>>(...args: P) {
-  return getStoryVar(setup, ...args) as `setup.${string}`;
+	return getStoryVar(setup, ...args) as `setup.${string}`;
 }
 // Just to reduce repetition :3
 export function playerVar<P extends Path<SugarCubeStoryVariables["player"]>>(
-  ...args: P
+	...args: P
 ) {
-  //@ts-ignore
-  return stateFulVar("player", ...args);
+	//@ts-ignore
+	return stateFulVar("player", ...args);
 }
 export function playerWombVar<
-  P extends Path<SugarCubeStoryVariables["player"]["womb"]>
+	P extends Path<SugarCubeStoryVariables["player"]["womb"]>
 >(...args: P) {
-  //@ts-ignore
-  return playerVar("womb", ...args);
+	//@ts-ignore
+	return playerVar("womb", ...args);
 }
 export function gameDateAndTimeVar<
-  P extends Path<SugarCubeStoryVariables["gameDateAndTime"]>
+	P extends Path<SugarCubeStoryVariables["gameDateAndTime"]>
 >(...args: P) {
-  //@ts-ignore
-  return stateFulVar("gameDateAndTime", ...args);
+	//@ts-ignore
+	return stateFulVar("gameDateAndTime", ...args);
 }
 
 // SECTION - Others
@@ -59,7 +59,7 @@ export function gameDateAndTimeVar<
  * Use this on a string that evaluates to a variable so that changes made to the variable show up on the passage :D
  */
 export function liveVar(variable: string) {
-  return `{{${variable}}}` as const;
+	return `{{${variable}}}` as const;
 }
 
 export const PC = "<<PC>>";
