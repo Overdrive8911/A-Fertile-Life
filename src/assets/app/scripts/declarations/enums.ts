f0c420f420f2f00fa29c2c4855a1552ed7973b1f
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-14 19:24:59 +0100

    Add Character img enum

--- src/assets/app/scripts/declarations/enums.ts
+++ src/assets/app/scripts/declarations/enums.ts
@@ -1,14 +1,24 @@
 export const enum CustomEventName {
-  TIME_UPDATE = "@timeUpdate",
-  SCENE_START = "@sceneStart",
-  SCENE_PROGRESS = "@sceneProgress",
-  SCENE_END = "@sceneEnd",
+	TIME_UPDATE = "@timeUpdate",
+	SCENE_START = "@sceneStart",
+	SCENE_PROGRESS = "@sceneProgress",
+	SCENE_END = "@sceneEnd",
 }
 
 export const enum CustomMacro {
-  METER = "meter",
+	METER = "meter",
 }
 
 export const enum Default {
-  MAX_STAT = 100,
+	MAX_STAT = 100,
+}
+
+export const enum CharacterImg {
+	NEUTRAL,
+	HAPPY,
+	SAD,
+	ANGRY,
+	SHOCK,
+	BORED,
+	SURPRISE,
 }
