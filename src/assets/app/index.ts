858778cc92a3f20c1a6a12b1d0ef48f21776cbb5
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-15 20:33:48 +0100

    Moved around imports

--- src/assets/app/index.ts
+++ src/assets/app/index.ts
@@ -1,5 +1,5 @@
-import "./scripts/index";
-// import "./styles/main.scss";
 import "./styles/fonts.css";
 import "./styles/variables.css";
 import "./styles/extensions.css";
+import "./scripts/index";
+// import "./styles/main.scss";
