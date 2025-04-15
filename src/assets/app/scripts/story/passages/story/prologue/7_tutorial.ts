aa6b606a4a44fbeec75401f6b56452c4bc366f58
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-15 14:07:55 +0100

    Refactors dialogue implementation
    Replaces character macro calls with a dedicated dialog macro, improving code readability and maintainability.
    
    This change streamlines the dialogue system by introducing a consistent approach for displaying character interactions.
    Additionally, sets min width and height to img element to prevent layout shift.

--- src/assets/app/scripts/story/passages/story/prologue/7_tutorial.ts
+++ src/assets/app/scripts/story/passages/story/prologue/7_tutorial.ts
@@ -27,7 +27,7 @@ addPassage(
 				"an unfamiliar voice answers."
 		) +
 		p({ class: femaleSpeech }, "“Huh?”") +
-		p({ class: roboSpeech }, "“Look at the table.” ") +
+		macroDialog(CharacterName.DUMMY, "Look at the table.", DialogType.AI) +
 		p(
 			"You slowly raise your head towards the large white table in the center of your room. There, your eyes widen as you see a small floating thing with a holographic screen; most likely what issued the command."
 		) +
@@ -35,23 +35,30 @@ addPassage(
 		macroDialog(
 			CharacterName.GIGI,
 			"Morning sleepy head. It's almost time for work!",
+			DialogType.AI
+		) +
+		macroDialog(
+			CharacterName.GIGI,
+			"I am your General Artificial Intelligence for Gynaecological Insights; G.A.I.G.I, but I'd prefer you exclude the 'A'. You're looking at the prototype of one of the greatest achievements in all of medical history! I'll be tracking your vitals and be monitoring your condition for the duration of your stay here. And before you ask about my origins, they're confidential. All that's important for you to know is that the doctor assigned me to you…",
 			DialogType.AI,
 			CharacterEmotion.HAPPY
 		) +
 		p(
-			`<<character "G.I.G.I" "roboSpeech" "happy_2">>“I am your General Artificial Intelligence for Gynaecological Insights; G.A.I.G.I, but I'd prefer you exclude the "A". You're looking at the prototype of one of the greatest achievements in all of medical history! I'll be tracking your vitals and be monitoring your condition for the duration of your stay here. And before you ask about my origins, they're confidential. All that's important for you to know is that the doctor assigned me to you…”<</character>>`
-		) +
-		p(
 			"The introduction, of what you now believe to be a robot, ends with it looking around your room with curiosity. It levitates towards different corners, shelves and wardrobes, opening a few to inspect the contents, before descending to simply floating above the table."
 		) +
-		p(
-			`<<character "G.I.G.I" "roboSpeech" "happy">>“Nice room you got here. Much nicer than the bleak white ones I'm used to.”<</character>>`
+		macroDialog(
+			CharacterName.GIGI,
+			"Nice room you got here. Much nicer than the bleak white ones I'm used to.",
+			DialogType.AI
 		) +
 		p(
 			"After the robot stops talking, it stares silently at you for almost a minute, seemingly waiting for something."
 		) +
-		p(
-			`<<character "G.I.G.I" "roboSpeech" "hmm">>“…Uh, this is the part where you ask some questions. We're kinda pressed for time.”<</character>>`
+		macroDialog(
+			CharacterName.GIGI,
+			"…Uh, this is the part where you ask some questions. We're kinda pressed for time.",
+			DialogType.AI,
+			CharacterEmotion.BORED
 		) +
 		p({ class: femaleSpeech }, `“Oh! I'm ${PC}. So, G.I.G.I, um…”`) +
 		/* This is where the info links start */
