7e046cb6e1e7c76a7ced8a9b4a7a86c018fe304f
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-15 20:24:33 +0100

    Fixed some issues with race conditions on the live server

--- build.ts
+++ build.ts
@@ -1,10 +1,10 @@
 import { build, write, $, sleep, type BuildConfig } from "bun";
 import {
-  bundleScriptAndStyleExtensions,
-  cleanDirectories,
-  copyAssets,
-  copyOtherAssets,
-  scssToCss,
+	bundleScriptAndStyleExtensions,
+	cleanDirectories,
+	copyAssets,
+	copyOtherAssets,
+	scssToCss,
 } from "./.build/plugins";
 import { mode } from "./.build/variables";
 import { Directory } from "./.build/enums";
@@ -12,113 +12,115 @@ import watcher from "@parcel/watcher";
 import { setupTweego } from "tweenode";
 
 const buildConfig: BuildConfig = {
-  entrypoints: [Directory.SCRIPT_ENTRYPOINT + ""],
-  outdir: Directory.BUNDLED_SCRIPTS_DIR + "",
-  minify: mode === "production",
-  plugins: [
-    cleanDirectories,
-    scssToCss,
-    // bundleScriptAndStyleExtensions,
-    // copyOtherAssets,
-  ],
-  drop: mode === "production" ? ["console", "window"] : [],
-  naming: {
-    asset: "assets/[name]-[hash].[ext]",
-  },
+	entrypoints: [Directory.SCRIPT_ENTRYPOINT + ""],
+	outdir: Directory.BUNDLED_SCRIPTS_DIR + "",
+	minify: mode === "production",
+	plugins: [
+		cleanDirectories,
+		scssToCss,
+		// bundleScriptAndStyleExtensions,
+		// copyOtherAssets,
+	],
+	drop: mode === "production" ? ["console", "window"] : [],
+	naming: {
+		asset: "assets/[name]-[hash].[ext]",
+	},
 };
 const buildResult = await build(buildConfig);
 
 if (mode == "development") {
-  const tryBuild = async () => {
-    try {
-      await build({ ...buildConfig, plugins: [scssToCss] });
-    } catch (error) {
-      console.log("Build failed. Error:", error);
-      console.log("Retrying in 5s");
-      sleep(5000).finally(async () => {
-        await tryBuild();
-      });
-    }
-  };
-  let subscription = watcher.subscribe(Directory.APP, async (_, events) => {
-    events.forEach(async (e) => {
-      if (
-        e.path.endsWith(".ts") ||
-        e.path.endsWith(".css") ||
-        e.path.endsWith(".scss")
-      ) {
-        await tryBuild();
-      }
-    });
-  });
+	const tryBuild = async () => {
+		try {
+			await build({ ...buildConfig, plugins: [scssToCss] });
+		} catch (error) {
+			console.log("Build failed. Error:", error);
+			console.log("Retrying in 5s");
+			sleep(5000).finally(async () => {
+				await tryBuild();
+			});
+		}
+	};
+	let subscription = watcher.subscribe(Directory.APP, async (_, events) => {
+		events.forEach(async (e) => {
+			if (
+				e.path.endsWith(".ts") ||
+				e.path.endsWith(".css") ||
+				e.path.endsWith(".scss")
+			) {
+				await tryBuild();
+			}
+		});
+	});
 
-  process.on("SIGINT", async () => {
-    await (await subscription).unsubscribe();
-    process.exit(0);
-  });
+	process.on("SIGINT", async () => {
+		await (await subscription).unsubscribe();
+		process.exit(0);
+	});
 }
 
 if (buildResult) {
-  setupTweego().finally(async () => {
-    await compileStory();
-  });
-  // const tweego = new Tweenode({ writeToLog: true })
+	setupTweego().finally(async () => {
+		await compileStory();
+		// Start the live server
+		await $`rapidpreview --port 3000 --path ./dist --file ./index.html`;
+	});
+	// const tweego = new Tweenode({ writeToLog: true })
 
-  async function compileStory() {
-    //ANCHOR: The only reason I'm manually using a terminal command is because `tweenode` occasionally errors out with no reasonable logs >~<
-    await $`cd .tweenode && ./tweego --head=../${
-      Directory.HEAD_CONTENT
-    } --module=../${Directory.VENDOR} ${
-      mode == "development" ? "-t" : ""
-    } --output=../${Directory.BUNDLED_STORY_NAME} ../${Directory.STORY} ../${
-      Directory.BUNDLED_SCRIPTS_DIR
-    }`; // ../${Directory.BUNDLED_STYLES_DIR}`;
-    await copyAssets();
+	async function compileStory() {
+		//ANCHOR: The only reason I'm manually using a terminal command is because `tweenode` occasionally errors out with no reasonable logs >~<
+		await $`cd .tweenode && ./tweego --head=../${
+			Directory.HEAD_CONTENT
+		} --module=../${Directory.VENDOR} ${
+			mode == "development" ? "-t" : ""
+		} --output=../${Directory.BUNDLED_STORY_NAME} ../${Directory.STORY} ../${
+			Directory.BUNDLED_SCRIPTS_DIR
+		}`; // ../${Directory.BUNDLED_STYLES_DIR}`;
+		await copyAssets();
 
-    // Copy over the assets
+		// Copy over the assets
 
-    // write(Directory.BUNDLED_STORY_NAME, blob())
-    // await tweego.process({
-    //   input: {
-    //     storyDir: Directory.STORY,
-    //     useTwineTestMode: mode == 'development' ? true : false,
-    //     htmlHead: Directory.HEAD_CONTENT,
-    //     modules: [Directory.VENDOR],
-    //     scripts: Directory.BUNDLED_SCRIPTS_DIR,
-    //     styles: Directory.BUNDLED_STYLES_DIR,
-    //   },
-    //   // output: { fileName: Directory.BUNDLED_STORY_NAME, mode: 'file' },
-    //   output: { mode: 'file', fileName: Directory.BUNDLED_STORY_NAME },
-    // })
-  }
+		// write(Directory.BUNDLED_STORY_NAME, blob())
+		// await tweego.process({
+		//   input: {
+		//     storyDir: Directory.STORY,
+		//     useTwineTestMode: mode == 'development' ? true : false,
+		//     htmlHead: Directory.HEAD_CONTENT,
+		//     modules: [Directory.VENDOR],
+		//     scripts: Directory.BUNDLED_SCRIPTS_DIR,
+		//     styles: Directory.BUNDLED_STYLES_DIR,
+		//   },
+		//   // output: { fileName: Directory.BUNDLED_STORY_NAME, mode: 'file' },
+		//   output: { mode: 'file', fileName: Directory.BUNDLED_STORY_NAME },
+		// })
+	}
 
-  if (mode == "development") {
-    const subscription = watcher.subscribe(Directory.OUTPUT, async () => {
-      await compileStory();
-      // // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
-      // await write(
-      //   `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
-      //   "Dummy"
-      // );
-    });
+	if (mode == "development") {
+		const subscription = watcher.subscribe(Directory.OUTPUT, async () => {
+			await compileStory();
+			// // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
+			// await write(
+			//   `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
+			//   "Dummy"
+			// );
+		});
 
-    // const subscription2 = watcher.subscribe(Directory.STORY, async () => {
-    //   await compileStory();
-    //   // // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
-    //   // await write(
-    //   //   `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
-    //   //   "Dummy"
-    //   // );
-    // });
+		// const subscription2 = watcher.subscribe(Directory.STORY, async () => {
+		//   await compileStory();
+		//   // // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
+		//   // await write(
+		//   //   `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
+		//   //   "Dummy"
+		//   // );
+		// });
 
-    process.on("SIGINT", async () => {
-      await (await subscription).unsubscribe();
-      // await (await subscription2).unsubscribe();
-      process.exit(0);
-    });
-  }
+		process.on("SIGINT", async () => {
+			await (await subscription).unsubscribe();
+			// await (await subscription2).unsubscribe();
+			process.exit(0);
+		});
+	}
 
-  await compileStory();
+	await compileStory();
 }
 
 export {};
