1714e09040d81a922e402b934f8d5cff8b40f349
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-16 15:05:54 +0100

    Tweaked the live server to solve some bugs...

--- .build/plugins.ts
+++ .build/plugins.ts
@@ -8,268 +8,268 @@ import { link, mkdir, rm } from "node:fs/promises";
 import { dirname } from "node:path";
 
 export const cleanDirectories: BunPlugin = {
-  name: "Clean Directories",
-  setup(build) {
-    build.onStart(async () => {
-      const cleanDir = async (dir: string) => {
-        rm(dir, { recursive: true, force: true })
-          .catch(() => {
-            console.log(`File at path, ${dir}, doesn't exist. Skipping.`);
-          })
-          .finally(async () => {
-            await mkdir(dir, { recursive: true });
-          });
-      };
+	name: "Clean Directories",
+	setup(build) {
+		build.onStart(async () => {
+			const cleanDir = async (dir: string) => {
+				rm(dir, { recursive: true, force: true })
+					.catch(() => {
+						console.log(`File at path, ${dir}, doesn't exist. Skipping.`);
+					})
+					.finally(async () => {
+						await mkdir(dir, { recursive: true });
+					});
+			};
 
-      await cleanDir(Directory.OUTPUT);
-      await cleanDir(Directory.BUNDLED_STORY);
-    });
-  },
+			await cleanDir(Directory.OUTPUT);
+			await cleanDir(Directory.BUNDLED_STORY);
+		});
+	},
 };
 
 async function processCSS(cssString: string, filePath?: string) {
-  const filename = filePath || "input.css";
+	const filename = filePath || "input.css";
 
-  // Lightning CSS automatically applies vendor prefixes based on the specified targets.
-  // It also minifies the output when `minify` is set to true.
-  const { code } = transform({
-    filename,
-    code: Buffer.from(cssString),
-    minify: mode == "production",
+	// Lightning CSS automatically applies vendor prefixes based on the specified targets.
+	// It also minifies the output when `minify` is set to true.
+	const { code } = transform({
+		filename,
+		code: Buffer.from(cssString),
+		minify: mode == "production",
 
-    // Define browser targets to control autoprefixing and feature transpilation.
-    targets: {
-      chrome: 80,
-      firefox: 80,
-      safari: 13,
-      edge: 80,
-    },
-  });
+		// Define browser targets to control autoprefixing and feature transpilation.
+		targets: {
+			chrome: 80,
+			firefox: 80,
+			safari: 13,
+			edge: 80,
+		},
+	});
 
-  // Return the transformed CSS as a string.
-  console.log(code.toString());
-  return code.toString();
+	// Return the transformed CSS as a string.
+	console.log(code.toString());
+	return code.toString();
 }
 
 /**
  * Also works with regular css files
  */
 export const scssToCss: BunPlugin = {
-  name: "SCSS To CSS",
-  setup(build) {
-    build.onLoad({ filter: /\.(scss|css)$/ }, async ({ path }) => {
-      const convertedCSS = path.endsWith(".scss")
-        ? (await sass.compileAsync(path)).css
-        : await file(path).text();
+	name: "SCSS To CSS",
+	setup(build) {
+		build.onLoad({ filter: /\.(scss|css)$/ }, async ({ path }) => {
+			const convertedCSS = path.endsWith(".scss")
+				? (await sass.compileAsync(path)).css
+				: await file(path).text();
 
-      const baseUrl = process.cwd() + Directory.ASSETS.replace(".", "");
+			const baseUrl = process.cwd() + Directory.ASSETS.replace(".", "");
 
-      // Regex explanation:
-      // url\((['"]?)    : Matches 'url(' followed by an optional quote (captured as group 1)
-      // \/              : Matches a literal slash indicating the start of an absolute path
-      // ([^'")]+)       : Captures one or more characters that are not a quote, closing parenthesis, or double quote (group 2)
-      // \1              : Matches the same quote character as captured in group 1 (if any)
-      // \)              : Matches the closing parenthesis ')'
-      const updatedCSS = convertedCSS.replace(
-        /url\((['"]?)\/([^'")]+)\1\)/g,
-        (_, quote, path) => {
-          return `url(${quote}${baseUrl}/${path}${quote})`;
-        }
-      );
+			// Regex explanation:
+			// url\((['"]?)    : Matches 'url(' followed by an optional quote (captured as group 1)
+			// \/              : Matches a literal slash indicating the start of an absolute path
+			// ([^'")]+)       : Captures one or more characters that are not a quote, closing parenthesis, or double quote (group 2)
+			// \1              : Matches the same quote character as captured in group 1 (if any)
+			// \)              : Matches the closing parenthesis ')'
+			const updatedCSS = convertedCSS.replace(
+				/url\((['"]?)\/([^'")]+)\1\)/g,
+				(_, quote, path) => {
+					return `url(${quote}${baseUrl}/${path}${quote})`;
+				}
+			);
 
-      // console.log(updatedCss);
+			// console.log(updatedCss);
 
-      return { contents: updatedCSS, loader: "css" };
-    });
-  },
+			return { contents: updatedCSS, loader: "css" };
+		});
+	},
 };
 
 const getFilePathsRecursivelyFromDirectory = async (
-  fileGlobPattern: string,
-  dirToSearch?: string
+	fileGlobPattern: string,
+	dirToSearch?: string
 ) => {
-  const glob = new Glob(fileGlobPattern);
-  const filePaths: string[] = [];
+	const glob = new Glob(fileGlobPattern);
+	const filePaths: string[] = [];
 
-  // Scans the current working directory and each of its sub-directories recursively
-  for await (const filePath of glob.scan(dirToSearch ?? ".")) {
-    filePaths.push(filePath);
-  }
-  return filePaths;
+	// Scans the current working directory and each of its sub-directories recursively
+	for await (const filePath of glob.scan(dirToSearch ?? ".")) {
+		filePaths.push(filePath);
+	}
+	return filePaths;
 };
 
 /** For moving all the bundled assets into the final directory*/
 export async function copyAssets() {
-  const filePaths = await getFilePathsRecursivelyFromDirectory(
-    "*",
-    Directory.BUNDLED_ASSETS
-  );
+	const filePaths = await getFilePathsRecursivelyFromDirectory(
+		"*",
+		Directory.BUNDLED_ASSETS
+	);
 
-  // console.log(filePaths);
-  filePaths.forEach(async (path) => {
-    await write(
-      Directory.BUNDLED_STORY_ASSETS + "/" + path,
-      file(Directory.BUNDLED_ASSETS + "/" + path)
-    );
-  });
+	// console.log(filePaths);
+	filePaths.forEach(async (path) => {
+		await write(
+			Directory.BUNDLED_STORY_ASSETS + "/" + path,
+			file(Directory.BUNDLED_ASSETS + "/" + path)
+		);
+	});
 }
 
 const trimFilePath = (path: string) => {
-  return "./" + path.replace(process.cwd(), "");
+	return "./" + path.replace(process.cwd(), "");
 };
 
 export const bundleScriptAndStyleExtensions: BunPlugin = {
-  name: "Bundle JS and CSS extensions",
-  setup(build) {
-    build.onStart(async () => {
-      const jsFilePaths = await getFilePathsRecursivelyFromDirectory(
-        "**/*.js",
-        Directory.SCRIPT_EXTENSIONS
-      );
-      const cssFilePaths = await getFilePathsRecursivelyFromDirectory(
-        "**/*.css",
-        Directory.STYLE_EXTENSIONS
-      );
+	name: "Bundle JS and CSS extensions",
+	setup(build) {
+		build.onStart(async () => {
+			const jsFilePaths = await getFilePathsRecursivelyFromDirectory(
+				"**/*.js",
+				Directory.SCRIPT_EXTENSIONS
+			);
+			const cssFilePaths = await getFilePathsRecursivelyFromDirectory(
+				"**/*.css",
+				Directory.STYLE_EXTENSIONS
+			);
 
-      const processContentOfFiles = async (
-        filePaths: string[],
-        fileType: "css" | "js"
-      ) => {
-        const scriptTranspiler = new Transpiler({
-          inline: true,
-          deadCodeElimination: true,
-          minifyWhitespace: true,
-        });
+			const processContentOfFiles = async (
+				filePaths: string[],
+				fileType: "css" | "js"
+			) => {
+				const scriptTranspiler = new Transpiler({
+					inline: true,
+					deadCodeElimination: true,
+					minifyWhitespace: true,
+				});
 
-        filePaths.forEach(async (path) => {
-          const combinedPath = `${
-            fileType == "css"
-              ? Directory.STYLE_EXTENSIONS
-              : Directory.SCRIPT_EXTENSIONS
-          }/${path}`;
-          let fileText = await file(combinedPath).text();
+				filePaths.forEach(async (path) => {
+					const combinedPath = `${
+						fileType == "css"
+							? Directory.STYLE_EXTENSIONS
+							: Directory.SCRIPT_EXTENSIONS
+					}/${path}`;
+					let fileText = await file(combinedPath).text();
 
-          // Processing
-          if (mode == "production") {
-            if (fileType == "css") {
-              fileText = await processCSS(fileText);
-            } else {
-              fileText = scriptTranspiler.transformSync(fileText);
-            }
-          }
+					// Processing
+					if (mode == "production") {
+						if (fileType == "css") {
+							fileText = await processCSS(fileText);
+						} else {
+							fileText = scriptTranspiler.transformSync(fileText);
+						}
+					}
 
-          const sanitizedPath = path.replace("/", "_");
-          write(
-            fileType == "css"
-              ? `${Directory.BUNDLED_STYLES_DIR}/0_${sanitizedPath}`
-              : `${Directory.BUNDLED_SCRIPTS_DIR}/0_${sanitizedPath}`,
-            fileText
-          );
-        });
-      };
+					const sanitizedPath = path.replace("/", "_");
+					write(
+						fileType == "css"
+							? `${Directory.BUNDLED_STYLES_DIR}/0_${sanitizedPath}`
+							: `${Directory.BUNDLED_SCRIPTS_DIR}/0_${sanitizedPath}`,
+						fileText
+					);
+				});
+			};
 
-      await processContentOfFiles(cssFilePaths, "css");
-      await processContentOfFiles(jsFilePaths, "js");
+			await processContentOfFiles(cssFilePaths, "css");
+			await processContentOfFiles(jsFilePaths, "js");
 
-      if (mode == "development") {
-        const scriptSubscription = watcher.subscribe(
-          Directory.SCRIPT_EXTENSIONS,
-          async (_, events) => {
-            events.forEach(async (e) => {
-              await processContentOfFiles([trimFilePath(e.path)], "js");
-            });
-          }
-        );
-        const styleSubscription = watcher.subscribe(
-          Directory.SCRIPT_EXTENSIONS,
-          async (_, events) => {
-            events.forEach(async (e) => {
-              await processContentOfFiles([trimFilePath(e.path)], "css");
-            });
-          }
-        );
-        process.on("SIGINT", async () => {
-          await (await scriptSubscription).unsubscribe();
-          await (await styleSubscription).unsubscribe();
-          process.exit(0);
-        });
-      }
-    });
-  },
+			if (mode == "development") {
+				const scriptSubscription = watcher.subscribe(
+					Directory.SCRIPT_EXTENSIONS,
+					async (_, events) => {
+						events.forEach(async (e) => {
+							await processContentOfFiles([trimFilePath(e.path)], "js");
+						});
+					}
+				);
+				const styleSubscription = watcher.subscribe(
+					Directory.SCRIPT_EXTENSIONS,
+					async (_, events) => {
+						events.forEach(async (e) => {
+							await processContentOfFiles([trimFilePath(e.path)], "css");
+						});
+					}
+				);
+				process.on("SIGINT", async () => {
+					await (await scriptSubscription).unsubscribe();
+					await (await styleSubscription).unsubscribe();
+					process.exit(0);
+				});
+			}
+		});
+	},
 };
 
 /**
  * UNUSED
  */
 export const copyOtherAssets: BunPlugin = {
-  name: "Copy Non-Code Assets (e.g audio, fonts, images, etc)",
-  setup(build) {
-    build.onStart(async () => {
-      const appStr = `${Directory.APP}/`.replace(`${Directory.ASSETS}/`, ""); // Yeah, don't think too much about this but with the default values, you're meant to get "app/"
+	name: "Copy Non-Code Assets (e.g audio, fonts, images, etc)",
+	setup(build) {
+		build.onStart(async () => {
+			const appStr = `${Directory.APP}/`.replace(`${Directory.ASSETS}/`, ""); // Yeah, don't think too much about this but with the default values, you're meant to get "app/"
 
-      // Skip scripts and styles
-      const assetPaths = await getFilePathsRecursivelyFromDirectory(
-        `!${appStr}**`, // Ignore all the files in the "app" directory (basically the styles and scripts)
-        Directory.ASSETS
-      );
+			// Skip scripts and styles
+			const assetPaths = await getFilePathsRecursivelyFromDirectory(
+				`!${appStr}**`, // Ignore all the files in the "app" directory (basically the styles and scripts)
+				Directory.ASSETS
+			);
 
-      const fastCopyFile = async (
-        originPath: string,
-        destinationPath: string
-      ) => {
-        const destFile = file(destinationPath);
-        const copyFunc = async () => {
-          // Ensure the destination directory exists
-          await mkdir(dirname(destinationPath), { recursive: true });
+			const fastCopyFile = async (
+				originPath: string,
+				destinationPath: string
+			) => {
+				const destFile = file(destinationPath);
+				const copyFunc = async () => {
+					// Ensure the destination directory exists
+					await mkdir(dirname(destinationPath), { recursive: true });
 
-          await link(originPath, destinationPath);
-        };
+					await link(originPath, destinationPath);
+				};
 
-        // Since trying to overwrite the link with spit out an error, we need to delete it first
-        if (await destFile.exists()) {
-          await destFile.delete().then(async () => {
-            await copyFunc();
-          });
-        } else {
-          await copyFunc();
-        }
-      };
+				// Since trying to overwrite the link with spit out an error, we need to delete it first
+				if (await destFile.exists()) {
+					await destFile.delete().then(async () => {
+						await copyFunc();
+					});
+				} else {
+					await copyFunc();
+				}
+			};
 
-      assetPaths.forEach(async (path) => {
-        const originPath = `${Directory.ASSETS}/${path}`;
-        const destinationPath = `${Directory.BUNDLED_STORY}/${path}`;
+			assetPaths.forEach(async (path) => {
+				const originPath = `${Directory.ASSETS}/${path}`;
+				const destinationPath = `${Directory.BUNDLED_STORY}/${path}`;
 
-        await fastCopyFile(originPath, destinationPath);
-      });
+				await fastCopyFile(originPath, destinationPath);
+			});
 
-      if (mode == "development") {
-        const subscription = watcher.subscribe(
-          Directory.ASSETS,
-          async (_, events) => {
-            events.forEach(async (e) => {
-              const path = trimFilePath(e.path);
-              const destinationPath = `${
-                Directory.BUNDLED_STORY
-              }/${path.replace(Directory.ASSETS.replace(".", ""), "")}`;
+			if (mode == "development") {
+				const subscription = watcher.subscribe(
+					Directory.ASSETS,
+					async (_, events) => {
+						events.forEach(async (e) => {
+							const path = trimFilePath(e.path);
+							const destinationPath = `${
+								Directory.BUNDLED_STORY
+							}/${path.replace(Directory.ASSETS.replace(".", ""), "")}`;
 
-              if (e.type == "create" || e.type == "update") {
-                if (!path.includes(appStr)) {
-                  await fastCopyFile(path, destinationPath);
-                }
-              } else {
-                // It's a deleted file so also delete it from the output
-                rm(destinationPath, {}).catch(() => {
-                  `File at path, ${path}, doesn't exist. Skipping.`;
-                });
-              }
-            });
-          }
-        );
-        process.on("SIGINT", async () => {
-          await (await subscription).unsubscribe();
-          process.exit(0);
-        });
-      }
-    });
-  },
+							if (e.type == "create" || e.type == "update") {
+								if (!path.includes(appStr)) {
+									await fastCopyFile(path, destinationPath);
+								}
+							} else {
+								// It's a deleted file so also delete it from the output
+								rm(destinationPath, {}).catch(() => {
+									`File at path, ${path}, doesn't exist. Skipping.`;
+								});
+							}
+						});
+					}
+				);
+				process.on("SIGINT", async () => {
+					await (await subscription).unsubscribe();
+					process.exit(0);
+				});
+			}
+		});
+	},
 };
--- build.ts
+++ build.ts
@@ -1,4 +1,4 @@
-import { build, write, $, sleep, type BuildConfig } from "bun";
+import { build, write, $, sleep, type BuildConfig, serve } from "bun";
 import {
 	bundleScriptAndStyleExtensions,
 	cleanDirectories,
@@ -10,21 +10,34 @@ import { mode } from "./.build/variables";
 import { Directory } from "./.build/enums";
 import watcher from "@parcel/watcher";
 import { setupTweego } from "tweenode";
+import { existsSync, symlinkSync } from "fs-extra";
 
 const buildConfig: BuildConfig = {
 	entrypoints: [Directory.SCRIPT_ENTRYPOINT + ""],
 	outdir: Directory.BUNDLED_SCRIPTS_DIR + "",
 	minify: mode === "production",
-	plugins: [
-		cleanDirectories,
-		scssToCss,
-		// bundleScriptAndStyleExtensions,
-		// copyOtherAssets,
-	],
+	plugins:
+		mode == "production"
+			? [
+					cleanDirectories,
+					scssToCss,
+					// bundleScriptAndStyleExtensions,
+					// copyOtherAssets,
+			  ]
+			: [
+					scssToCss,
+					// bundleScriptAndStyleExtensions,
+					// copyOtherAssets,
+			  ],
 	drop: mode === "production" ? ["console", "window"] : [],
 	naming: {
 		asset: "assets/[name]-[hash].[ext]",
 	},
+	// // Assets aren't copied during dev
+	// publicPath:
+	// 	mode === "development"
+	// 		? `./../${Directory.BUNDLED_SCRIPTS_DIR.replace("./", "")}/`
+	// 		: undefined,
 };
 const buildResult = await build(buildConfig);
 
@@ -58,11 +71,81 @@ if (mode == "development") {
 	});
 }
 
+// const RELOAD_TXT = `${Directory.BUNDLED_STORY}/reload.txt`;
 if (buildResult) {
 	setupTweego().finally(async () => {
-		await compileStory();
 		// Start the live server
-		await $`rapidpreview --port 3000 --path ./dist --file ./index.html`;
+		// await $`refreshin -p 3000 -d ./dist --no-browser`;
+		// // The sleep is just to avoid race issues with the html file
+		// await sleep(100).finally(async () => {
+		// 	// await $`bun ./dist/index.html`;
+		// 	await $`bunx reload -p 3000 -d ./dist`;
+		// 	// serve()
+		// });
+		if (mode == "development") {
+			// let calledTimes = 1;
+			let shouldCompile = true;
+			const subscription = watcher.subscribe(Directory.OUTPUT, async () => {
+				if (shouldCompile) {
+					shouldCompile = false;
+					await compileStory();
+					// console.log("I am called " + calledTimes++ + " times");
+					setTimeout(() => {
+						shouldCompile = true;
+					}, 1000);
+				}
+				// // A hacky way to prevent the live server from reloading multiple times over a short period of time
+				// if (shouldReloadServer) {
+				// 	shouldReloadServer = false;
+				// 	setTimeout(() => {
+				// 		shouldReloadServer = true;
+				// 	}, 1000);
+				// 	await $`touch ${RELOAD_TXT}`;
+				// }
+				// // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
+				// await write(
+				//   `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
+				//   "Dummy"
+				// );
+			});
+
+			// const subscription2 = watcher.subscribe(Directory.STORY, async () => {
+			//   await compileStory();
+			//   // // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
+			//   // await write(
+			//   //   `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
+			//   //   "Dummy"
+			//   // );
+			// });
+
+			process.on("SIGINT", async () => {
+				await (await subscription).unsubscribe();
+				// await (await subscription2).unsubscribe();
+				process.exit(0);
+			});
+			// await $`bunx reload -p 3000 -d ${Directory.BUNDLED_STORY}`;
+			// await $`bunx browser-sync start -s "dist" --files "dist" --no-open --reload-debounce 2000 --reload-delay 2000`;
+			// await $`bunx browser-sync start -s "dist" --files "dist" --no-open --reload-debounce 1500`;
+			// await $`bunx livereload dist/ -p 3000 -w 1000 -f 'index.html'`;
+			// await $`cd ./dist && bunx live-server --port=3000 --wait=1500 --no-browser`;
+			// write(RELOAD_TXT, "Used as a hacky debounce workaround");
+			// const target = Directory.BUNDLED_ASSETS;
+			// const linkPath = Directory.BUNDLED_STORY_ASSETS;
+
+			// // Only create the symlink if it doesn't already exist.
+			// if (!existsSync(linkPath)) {
+			// 	// On Windows, when linking directories, use the "junction" type for better compatibility.
+			// 	symlinkSync(target, linkPath, "junction");
+			// 	console.log(`Symlink created: ${linkPath} -> ${target}`);
+			// } else {
+			// 	console.log(`Symlink already exists at: ${linkPath}`);
+			// }
+			await $`[ ! -L "${Directory.BUNDLED_STORY_ASSETS}" ] && ln -s ../${Directory.BUNDLED_ASSETS}/ ${Directory.BUNDLED_STORY_ASSETS} || true`;
+			await $`echo "Symlink ready."`;
+			await $`bun run --bun rapidpreview --path ${Directory.BUNDLED_STORY} --port ${Directory.PORT}`;
+		} else {
+			await compileStory();
+		}
 	});
 	// const tweego = new Tweenode({ writeToLog: true })
 
@@ -75,7 +158,10 @@ if (buildResult) {
 		} --output=../${Directory.BUNDLED_STORY_NAME} ../${Directory.STORY} ../${
 			Directory.BUNDLED_SCRIPTS_DIR
 		}`; // ../${Directory.BUNDLED_STYLES_DIR}`;
-		await copyAssets();
+		if (mode === "production") await copyAssets();
+		// 		await $`if [ ! -L ${Directory.BUNDLED_STORY_ASSETS}" ]; then
+		//   ln -s ${Directory.BUNDLED_ASSETS} ${Directory.BUNDLED_STORY_ASSETS}
+		// fi`;
 
 		// Copy over the assets
 
@@ -94,33 +180,12 @@ if (buildResult) {
 		// })
 	}
 
-	if (mode == "development") {
-		const subscription = watcher.subscribe(Directory.OUTPUT, async () => {
-			await compileStory();
-			// // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
-			// await write(
-			//   `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
-			//   "Dummy"
-			// );
-		});
+	// let shouldReloadServer = true;
+	// if (mode == "development") {
 
-		// const subscription2 = watcher.subscribe(Directory.STORY, async () => {
-		//   await compileStory();
-		//   // // A hacky way to force the live reload server to respond to this change if it doesn't detect the `index.html` change
-		//   // await write(
-		//   //   `${Directory.BUNDLED_STORY}${Directory.STORY_MEDIA}/dummy.txt`,
-		//   //   "Dummy"
-		//   // );
-		// });
+	// }
 
-		process.on("SIGINT", async () => {
-			await (await subscription).unsubscribe();
-			// await (await subscription2).unsubscribe();
-			process.exit(0);
-		});
-	}
-
-	await compileStory();
+	// await compileStory();
 }
 
 export {};
