9878fb0a0fbc15628ac63f7e112c614c16ddab84
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-24 21:45:05 +0100

    Added a check to avoid animated scrolling for passage links

--- src/assets/app/scripts/story/passages/ui/passage_links.ts
+++ src/assets/app/scripts/story/passages/ui/passage_links.ts
@@ -9,7 +9,7 @@ import { nav, passageArea } from "./ui.module.css";
 let observer: MutationObserver | undefined;
 
 // This will copy all links in the current passage into a special container for ease of use.
-// It will also scroll the passage to the link's position.
+// Copied links, when clicked, also scroll the passage to the original link's position unless they are connected to other passages.
 // Copied links will be removed when te passage is refreshed or the original link is removed from the DOM.
 runOnPassageEnd((e) => {
 	const $bottomLinkContainer = $(convertToClass(nav)).empty();
@@ -51,10 +51,14 @@ runOnPassageEnd((e) => {
 						// Otherwise, we'd get some recursion issues >~<
 						e.stopPropagation();
 
-						$(convertToClass(passageArea)).animate(
-							{ scrollTop: distance },
-							750
-						);
+						const $passageArea = $(convertToClass(passageArea));
+						// No use scrolling if the link navigates to another passage
+						if (!$link.is("[data-passage]")) {
+							$passageArea.animate({ scrollTop: distance }, 750);
+						} else {
+							// Snap back to the top so the player doesn't miss anything on the new passage :D
+							$passageArea.scrollTop(0);
+						}
 					});
 					$link.wrap(div(""));
 
