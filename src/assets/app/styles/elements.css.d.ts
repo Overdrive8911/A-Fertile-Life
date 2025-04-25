2d4350c1d966001d64b6d093e16fe9d6a7d3f640
Overdrive8911 <wuchidiscord@gmail.com>
2025-04-25 21:19:26 +0100

    fix: Added some missing styling for native sugarcube elements

--- src/assets/app/styles/elements.css.d.ts
+++ src/assets/app/styles/elements.css.d.ts
@@ -1,1 +1,8 @@
-export {};
+declare const styles: {
+  readonly "saves": string;
+  readonly "uiDialogBody": string;
+  readonly "uiDialogClose": string;
+  readonly "uiDialogTitlebar": string;
+};
+export = styles;
+
