4e97cbc5e2fc6b73d4e46b2953990c448afa414e
Overdrive8911 <wuchidiscord@gmail.com>
2025-08-11 19:42:08 +0100

    wip(main-ui): make scrollbar visible on mobile protrait devices

--- src/components/main-ui.tsx
+++ src/components/main-ui.tsx
@@ -16,7 +16,7 @@ const ROUNDED_BORDER = "rounded-2xl";
 function MainUI() {
 	return (
 		// So that on mobile portrait mode, the 3 panels can be accessed by simply swiping
-		<div class="h-full w-full carousel grid grid-cols-[100vw_100vw_100vw] sm:grid-cols-[0.85fr_1.75fr_1fr] py-4 *:h-full *:[scroll-snap-align:start]">
+		<div class="h-full w-full carousel [scrollbar-width:auto] sm:[scrollbar-width:none] grid grid-cols-[100vw_100vw_100vw] sm:grid-cols-[0.85fr_1.75fr_1fr] py-4 *:h-full *:[scroll-snap-align:start]">
 			<LeftPanel />
 
 			<CenterPanel />
