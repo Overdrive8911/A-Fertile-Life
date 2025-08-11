576d948c11ba84b67f697185ecbadbb78ff92a8c
Overdrive8911 <wuchidiscord@gmail.com>
2025-08-11 16:48:33 +0100

    wip: wip

--- src/components/main-ui.tsx
+++ src/components/main-ui.tsx
@@ -1,9 +1,12 @@
 import { For, type JSX } from "solid-js";
 import { PassageDisplay } from "./../game/passages/passage-display";
 
+const RIGHT_AND_LEFT_PANEL_DIMENSION = "h-[97.5%] w-[75%] contain-strict";
+const ROUNDED_BORDER = "rounded-2xl";
+
 function MainUI() {
 	return (
-		<div class="h-full w-[300vw] sm:w-full grid grid-cols-3 sm:grid-cols-[1fr_2.5fr_1fr] gap-[7.5%] py-4 px-[2.5%] *:h-full">
+		<div class="h-full w-[300vw] sm:w-full grid grid-cols-3 sm:grid-cols-[0.85fr_1.75fr_1fr] py-4 *:h-full">
 			<LeftPanel />
 
 			<CenterPanel />
@@ -14,27 +17,121 @@ function MainUI() {
 }
 
 function LeftPanel() {
+	function TopPanel() {
+		function DigitalClock() {
+			return (
+				<div
+					class={`${ROUNDED_BORDER} p-1 bg-base-300 grid grid-rows-2 place-items-center w-3/4 mx-auto text-info`}
+				>
+					<div class="countdown font-mono text-2xl text-shadow-[2px_2px_1px] text-shadow-info/25">
+						<span style="--value:15;"></span>:
+						<span style="--value:30;" class="mr-2"></span> PM
+					</div>
+
+					<div class="countdown font-mono text-lg">
+						WED, <span style="--value:30;" class="mx-2"></span>
+						FEB
+					</div>
+				</div>
+			);
+		}
+
+		function Reminders() {
+			return (
+				<Block title="REMINDERS">
+					**Display a list of recent reminders here**
+				</Block>
+			);
+		}
+
+		function MapArea() {
+			return (
+				<div class={`${ROUNDED_BORDER} border border-primary`}>
+					MAP CANVAS HERE. TAP TO OPEN A MAGNIFIED VIEW.
+				</div>
+			);
+		}
+
+		return (
+			<div class="grid grid-rows-[0.75fr_1.25fr_1.5fr] gap-2">
+				<DigitalClock />
+
+				<Reminders />
+
+				<MapArea />
+			</div>
+		);
+	}
+
+	function BottomPanel() {
+		function Button(prop: {
+			handler?: (e: MouseEvent) => void;
+			children: JSX.Element;
+		}) {
+			return (
+				<button
+					type="button"
+					class="btn btn-primary btn-soft text-lg w-3/4"
+					onClick={prop.handler}
+				>
+					{prop.children}
+				</button>
+			);
+		}
+
+		return (
+			<div class="flex flex-col justify-end items-center gap-8 pb-4">
+				<Button>Save</Button>
+				<Button>Inventory</Button>
+				<Button>Settings</Button>
+				<Button>Restart</Button>
+			</div>
+		);
+	}
+
 	return (
-		<div class="w-1/4 h-full p-4">
-			<h2 class="text-xl font-bold mb-4">Left Panel</h2>
-			<p>Content for the left panel goes here.</p>
+		<div class="flex justify-center items-center">
+			<div
+				class={`${RIGHT_AND_LEFT_PANEL_DIMENSION} p-4 grid grid-rows-2 border border-primary ${ROUNDED_BORDER} text-center bg-base-200`}
+			>
+				<TopPanel />
+
+				<BottomPanel />
+			</div>
 		</div>
 	);
 }
 
 function CenterPanel() {
 	return (
-		<div class="grid grid-rows-[3.5fr_1fr] gap-6 *:border *:border-primary *:rounded-box *:p-4 *:bg-base-200">
+		<div
+			class={`grid grid-rows-[3.5fr_1fr] gap-6 px-1 *:border *:border-primary *:${ROUNDED_BORDER} *:p-4 *:bg-base-200`}
+		>
 			{/* Main passage display*/}
 			<main class="overflow-auto contain-strict">
 				<PassageDisplay />
 			</main>
 
 			{/* Buttons for interacting with the passage*/}
-			<div class="grid grid-cols-3 md:grid-cols-4 gap-4">
-				<For each={["Option 1", "Option 2", "Option 3", "Option 4"]}>
+			<div class="grid grid-cols-3 md:grid-cols-4 gap-4 contain-strict overflow-auto">
+				<For
+					each={[
+						"Option 1",
+						"Option 2",
+						"Option 3",
+						"Lorem Ipusm Dolomet",
+						"Option 5",
+						"Option 6",
+						"Pretty long text in box that should likely expand it, right?",
+						"Option 8",
+						"Option 9",
+						"Option 10",
+						"Option 11",
+						"Option 12",
+					]}
+				>
 					{(val) => (
-						<button type="button" class="btn btn-primary btn-soft">
+						<button type="button" class="btn btn-primary btn-soft min-h-fit">
 							{val}
 						</button>
 					)}
@@ -45,17 +142,6 @@ function CenterPanel() {
 }
 
 function RightPanel() {
-	function Block(prop: { title: string; children: JSX.Element }) {
-		return (
-			// To hide the ugly border on the last element, we use the `last:` variant
-			<div class="flex flex-col last:[&_div]:border-b-0">
-				<h2 class="text-xl font-bold">{prop.title}</h2>
-
-				<div class="grow border-b border-t border-primary">{prop.children}</div>
-			</div>
-		);
-	}
-
 	function StatusDisplay() {
 		return (
 			<Block title="STATS">
@@ -78,12 +164,35 @@ function RightPanel() {
 	}
 
 	return (
-		<div class="grid grid-rows-3 border border-primary rounded-box text-center bg-base-200">
-			<StatusDisplay />
+		<div class="flex justify-center items-center">
+			<div
+				class={`${RIGHT_AND_LEFT_PANEL_DIMENSION} grid grid-rows-3 border border-primary ${ROUNDED_BORDER} text-center bg-base-200 p-4 gap-4`}
+			>
+				<StatusDisplay />
 
-			<StatusEffects />
+				<StatusEffects />
 
-			<PlayerDisplay />
+				<PlayerDisplay />
+			</div>
+		</div>
+	);
+}
+
+function Block(prop: { title: string; children: JSX.Element }) {
+	// return (
+	// 	// To hide the ugly border on the last element, we use the `last:` variant
+	// 	<div class="flex flex-col last:[&_div]:border-b-0">
+	// 		<h2 class="text-xl font-bold">{prop.title}</h2>
+
+	// 		<div class="grow border-b border-t border-primary">{prop.children}</div>
+	// 	</div>
+	// );
+	//
+	return (
+		<div class={`flex flex-col border border-primary ${ROUNDED_BORDER}`}>
+			<h2 class="text-xl font-bold">{prop.title}</h2>
+
+			<div class="grow border-t border-primary p-2">{prop.children}</div>
 		</div>
 	);
 }
