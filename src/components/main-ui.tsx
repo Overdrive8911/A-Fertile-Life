b0cc52e7487c44ae3ac4e09e7b263376e1bcee17
Overdrive8911 <wuchidiscord@gmail.com>
2025-08-11 17:11:01 +0100

    wip: work on the map container

--- src/components/main-ui.tsx
+++ src/components/main-ui.tsx
@@ -4,6 +4,8 @@ import {
 	RotateCcwIcon,
 	SaveIcon,
 	SettingsIcon,
+	ZoomInIcon,
+	ZoomOutIcon,
 } from "lucide-solid";
 import { For, type JSX } from "solid-js";
 import { PassageDisplay } from "./../game/passages/passage-display";
@@ -59,9 +61,26 @@ function LeftPanel() {
 		}
 
 		function MapArea() {
+			const BUTTON_CLASS = "btn btn-primary p-0 size-full rounded-none";
+
 			return (
-				<div class={`${ROUNDED_BORDER} border border-primary`}>
-					MAP CANVAS HERE. TAP TO OPEN A MAGNIFIED VIEW.
+				<div
+					class={`${ROUNDED_BORDER} border border-primary grid grid-cols-[1fr_1.9rem] grid-rows-2 overflow-clip`}
+				>
+					{/* The map Canvas*/}
+					<div class="row-span-2">
+						MAP CANVAS HERE. TAP TO OPEN A MAGNIFIED VIEW.
+					</div>
+
+					{/*Zoom in button*/}
+					<button type="button" class={BUTTON_CLASS}>
+						<ZoomInIcon />
+					</button>
+
+					{/*Zoom out button*/}
+					<button type="button" class={BUTTON_CLASS}>
+						<ZoomOutIcon />
+					</button>
 				</div>
 			);
 		}
