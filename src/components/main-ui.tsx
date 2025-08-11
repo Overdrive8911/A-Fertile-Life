4b4c95d78b9746e1bbb4174a485ee5a7777c4456
Overdrive8911 <wuchidiscord@gmail.com>
2025-08-11 19:06:38 +0100

    wip: make the main ui a 3 panel carousel on mobile portrait devices

--- src/components/main-ui.tsx
+++ src/components/main-ui.tsx
@@ -15,7 +15,8 @@ const ROUNDED_BORDER = "rounded-2xl";
 
 function MainUI() {
 	return (
-		<div class="h-full w-[300vw] sm:w-full grid grid-cols-3 sm:grid-cols-[0.85fr_1.75fr_1fr] py-4 *:h-full">
+		// So that on mobile portrait mode, the 3 panels can be accessed by simply swiping
+		<div class="h-full w-full carousel grid grid-cols-[100vw_100vw_100vw] sm:grid-cols-[0.85fr_1.75fr_1fr] py-4 *:h-full *:[scroll-snap-align:start]">
 			<LeftPanel />
 
 			<CenterPanel />
