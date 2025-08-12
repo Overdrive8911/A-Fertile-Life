e81aee0816fc21aaf994158ed06343e639ad1120
Overdrive8911 <wuchidiscord@gmail.com>
2025-08-12 20:18:03 +0100

    chore: add node types

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solidPlugin(), tailwindcss()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+	plugins: [solidPlugin(), tailwindcss()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: "esnext",
+	},
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "src"),
+		},
+	},
 });
