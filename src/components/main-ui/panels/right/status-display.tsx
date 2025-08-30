334334f038744db4ae17d1f2af89d040f1452186
Overdrive8911 <wuchidiscord@gmail.com>
2025-08-30 04:55:28 +0100

    wip: add reactive values to the status meters

--- src/components/main-ui/panels/right/status-display.tsx
+++ src/components/main-ui/panels/right/status-display.tsx
@@ -1,3 +1,5 @@
+import { GAME_VARIABLES } from "~/App";
+import { Womb } from "~/game/pregnancy/classes/womb";
 import energyIcon from "~/media/img/icons/stats/energy.webp";
 import healthIcon from "~/media/img/icons/stats/heart.webp";
 import moodIcon from "~/media/img/icons/stats/mood.webp";
@@ -8,16 +10,39 @@ import { StatMeter } from "../../../meter";
 import { Block } from "../../shared/block";
 
 export function StatusDisplay() {
+	const player = () => GAME_VARIABLES.player;
+	const womb = () => player().womb;
+
 	return (
 		<Block title="STATS">
 			<div class="flex flex-col gap-2">
-				<StatMeter val={0.9} stat="Health" icon={healthIcon} />
-				<StatMeter val={0.69} stat="Energy" icon={energyIcon} />
-				<StatMeter val={0.87} stat="Mood" icon={moodIcon} />
-				<StatMeter val={0.72} stat="Fullness" icon={stomachIcon} />
-				<StatMeter val={0.89} stat="Womb Health" icon={uterusHpIcon} />
 				<StatMeter
-					val={0.23}
+					val={player().hp / player().maxHp}
+					stat="Health"
+					icon={healthIcon}
+				/>
+				<StatMeter
+					val={player().energy / 100}
+					stat="Energy"
+					icon={energyIcon}
+				/>
+				<StatMeter
+					val={player().mental.mood / 100}
+					stat="Mood"
+					icon={moodIcon}
+				/>
+				<StatMeter
+					val={player().fullness / 100}
+					stat="Fullness"
+					icon={stomachIcon}
+				/>
+				<StatMeter
+					val={womb().hp / womb().maxHp}
+					stat="Womb Health"
+					icon={uterusHpIcon}
+				/>
+				<StatMeter
+					val={womb().exp / Womb.getExpLimit(womb().lvl + 1)}
 					stat="Womb Exp"
 					icon={uterusExpIcon}
 					highColor="blue"
