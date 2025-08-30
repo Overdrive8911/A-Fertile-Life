c4b77b00bfd6720a460759cf7a9577cbb031044b
Overdrive8911 <wuchidiscord@gmail.com>
2025-08-30 22:15:12 +0100

    perf(modal): make modals unmount / remount when required
    the previous implementation just let them stack up in the background which led to notable performance degradation after a while

--- src/components/modal/generic-modal.tsx
+++ src/components/modal/generic-modal.tsx
@@ -1,5 +1,13 @@
-import type { JSX } from "solid-js";
+import { ReactiveMap } from "@solid-primitives/map";
+import { createSignal, type JSX, Show, type Signal } from "solid-js";
 import { Portal } from "solid-js/web";
+
+/** So we can have multiple dialogs */
+const signalMap = new ReactiveMap<string, Signal<boolean>>();
+
+function performAfterDelay(callback: () => void, delay = 300) {
+	setTimeout(() => callback(), delay);
+}
 
 /** Use to open the modal.
  * **<button class="btn" onclick="my_modal_2.showModal()">open modal</button>** */
@@ -10,32 +18,40 @@ function GenericModal(prop: {
 	/** Normally, modal's have a z-index of 999 */
 	"z-index"?: number | undefined;
 }) {
-	return (
-		<Portal>
-			<dialog
-				closedby="closerequest"
-				id={prop.modalId}
-				class="modal"
-				style={{ "z-index": prop["z-index"] ?? 999 }}
-			>
-				<div class={`modal-box max-h-[90vh] sm:max-h-[85vh] ${prop.class}`}>
-					<form method="dialog">
-						<button
-							type="submit"
-							class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-							aria-label="Close Modal"
-						>
-							✕
-						</button>
+	const signal = () => signalMap.get(prop.modalId);
 
-						<div>{prop.children}</div>
+	return (
+		<Show when={signal()?.[0]()}>
+			<Portal>
+				<dialog
+					closedby="closerequest"
+					id={prop.modalId}
+					class="modal"
+					style={{ "z-index": prop["z-index"] ?? 999 }}
+					onClose={(_) => {
+						// Give a small delay so the animation completes
+						performAfterDelay(() => signal()?.[1](false));
+					}}
+				>
+					<div class={`modal-box max-h-[90vh] sm:max-h-[85vh] ${prop.class}`}>
+						<form method="dialog">
+							<button
+								type="submit"
+								class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+								aria-label="Close Modal"
+							>
+								✕
+							</button>
+
+							<div>{prop.children}</div>
+						</form>
+					</div>
+					<form method="dialog" class="modal-backdrop">
+						<button type="submit">close</button>
 					</form>
-				</div>
-				<form method="dialog" class="modal-backdrop">
-					<button type="submit">close</button>
-				</form>
-			</dialog>
-		</Portal>
+				</dialog>
+			</Portal>
+		</Show>
 	);
 }
 
@@ -45,12 +61,32 @@ function getDialogElementFromId(id: string): HTMLDialogElement | null {
 	return dialog instanceof HTMLDialogElement ? dialog : null;
 }
 
+function getModalSignal(modalId: string) {
+	return (
+		signalMap.get(modalId) ??
+		// biome-ignore lint/style/noNonNullAssertion: <Can't be null>
+		signalMap.set(modalId, createSignal(false)).get(modalId)!
+	);
+}
+
 function showModal(modalId: string) {
-	getDialogElementFromId(modalId)?.show();
+	// Mount the dialog's contents first
+	const [modalSignal, setModalSignal] = getModalSignal(modalId);
+
+	if (!modalSignal()) {
+		setModalSignal(true);
+
+		performAfterDelay(() => getDialogElementFromId(modalId)?.show(), 10);
+	}
 }
 
 function closeModal(modalId: string) {
 	getDialogElementFromId(modalId)?.close();
+
+	// Unmount the dialog's contents
+	const [_, setModalSignal] = getModalSignal(modalId);
+
+	performAfterDelay(() => setModalSignal(false));
 }
 
 export { GenericModal, closeModal, showModal };
