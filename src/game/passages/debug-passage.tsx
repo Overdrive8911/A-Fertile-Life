915dba6689edcc4b96cf6149dabecda90aaf5574
Overdrive8911 <wuchidiscord@gmail.com>
2025-09-04 21:58:44 +0100

    wip(debug): add baby btn

--- src/game/passages/debug-passage.tsx
+++ src/game/passages/debug-passage.tsx
@@ -66,6 +66,18 @@ function NextPassage() {
 					);
 				})()}
 			</p>
+
+			<p>
+				<GenericButtonLink
+					onClick={(_) =>
+						GAME_ENGINE.setVars((s) =>
+							s.player.womb.tryCreatePregnancy(100, 100),
+						)
+					}
+				>
+					Baby?
+				</GenericButtonLink>
+			</p>
 		</>
 	);
 }
